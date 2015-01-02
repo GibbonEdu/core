163f0de2d0c9e5fa5c3117af315b3bdb13f37df9
Ross Parler <rparker@MBP00012-Ross-Parker-3.local>
2015-01-02 19:23:02 +0800

    Indonesian Translator Changes

--- CHANGEDB.php
+++ CHANGEDB.php
@@ -626,6 +626,7 @@ INSERT INTO `gibbonSetting` (`gibbonSystemSettingsID` ,`scope` ,`name` ,`nameDis
 ALTER TABLE `gibbonUnit` ADD `license` VARCHAR(50) NULL DEFAULT NULL AFTER `embeddable`, ADD `sharedPublic` ENUM('Y','N') NULL DEFAULT NULL AFTER `license`;end
 UPDATE gibboni18n SET maintainerName='Jasmine Chan & Charlie Chow' WHERE code='zh_HK';end
 ALTER TABLE `gibbonHook` CHANGE `type` `type` ENUM('Public Home Page','Student Profile','Unit') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;end
+UPDATE gibboni18n SET maintainerName='Dicky Widhyatmoko' WHERE code='id_ID';end
 ";
 
 ?>
