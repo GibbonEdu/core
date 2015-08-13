8db3e823a50874ed715cf69553cf9900556c5cf5
Ross Parker <rossdotparker@gmail.com>
2015-08-14 07:11:45 +0800

    House Logo Display Bug Fix

--- functions.php
+++ functions.php
@@ -273,7 +273,9 @@ function getMinorLinks($connection2, $guid, $cacheLoad) {
 		
 		//House logo
 		if (isset($_SESSION[$guid]["gibbonHouseIDLogo"]) AND isset($_SESSION[$guid]["gibbonHouseIDName"])) {
-			$return.=" . <img class='minorLinkIcon' title='" . $_SESSION[$guid]["gibbonHouseIDName"] . "' style='vertical-align: -75%; margin-left: 4px' src='" . $_SESSION[$guid]["absoluteURL"] . "/" . $_SESSION[$guid]["gibbonHouseIDLogo"] . "'/>" ; 
+			if ($_SESSION[$guid]["gibbonHouseIDLogo"]!="") {
+				$return.=" . <img class='minorLinkIcon' title='" . $_SESSION[$guid]["gibbonHouseIDName"] . "' style='vertical-align: -75%; margin-left: 4px' src='" . $_SESSION[$guid]["absoluteURL"] . "/" . $_SESSION[$guid]["gibbonHouseIDLogo"] . "'/>" ; 
+			}
 		}
 	}
 	
