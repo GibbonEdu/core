13bb607a7cc9297cdcbbff76a2cdd5cbc2ea7f33
rossdotparker <rossdotparker@gmail.com>
2015-10-07 17:01:18 +0800

    Data Points Commit 3
    Internal Assessment all done, External coming soon!

--- modules/Tracking/dataPoints.php
+++ modules/Tracking/dataPoints.php
@@ -53,14 +53,15 @@ else {
 				print sprintf(_('Data Points has been configured to allow you to export certain key assessment data. Please contact %1$s if you are not seeing the data you need here.'), "<a href='mailto:" . $_SESSION[$guid]["organisationAdministratorEmail"] . "'>" . $_SESSION[$guid]["organisationAdministratorName"] . "</a>") . "<br/>" ;
 			}
 			else { //Yes access, give link to settings.
-				print sprintf(_('Data Points has been configured to export certain key assessment data. Please take a look at %1$sTracking Settings%2$s to change what data points are included.'), "<a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/School Admin/trackingSettings.php'>", "</a>") . "<br/>" ;
+				print sprintf(_('Data Points has been configured to export certain key assessment data. Please take a look at %1$sTracking Settings%2$s to change what data points are included.'), "<a href='" . $_SESSION[$guid]["absoluteURL"] . "/index.php?q=/modules/School Admin/trackingSettings.php'>", "</a>") . ". Use the export button below to prepare your Data Points export for download.<br/>" ;
 			}
 			print "<br/>" ;
-			print _("Use the export button below to prepare your Data Points export for download.") ;
 		print "</p>" ;
-
-		print "<div class='linkTop'>" ;
-			print "<a href='" . $_SESSION[$guid]["absoluteURL"] . "/modules/" . $_SESSION[$guid]["module"] . "/dataPoints_contents.php'>" . _("Export to Excel") . " <img title='" . _('Export to Excel') . "' src='./themes/" . $_SESSION[$guid]["gibbonThemeName"] . "/img/download.png'/></a>" ;
+		print "<div class='warning'>" ;
+			print _("Warning, please note that this process is resource intensive, and may slow down access to the system for other users. Please be patient as the download might take a few minutes to prepare.") ;
+			print "<div class='linkTop'>" ;
+				print "<a href='" . $_SESSION[$guid]["absoluteURL"] . "/modules/" . $_SESSION[$guid]["module"] . "/dataPoints_contents.php'>" . _("Export to Excel") . " <img title='" . _('Export to Excel') . "' src='./themes/" . $_SESSION[$guid]["gibbonThemeName"] . "/img/download.png'/></a>" ;
+			print "</div>" ;
 		print "</div>" ;
 	}
 
--- modules/Tracking/moduleFunctions.php
+++ modules/Tracking/moduleFunctions.php
@@ -17,9 +17,16 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+//Takes a number, and returns an Excel-style column reference for it
+function num2alpha($n) {
+    for($r = ""; $n >= 0; $n = intval($n / 26) - 1)
+        $r = chr($n%26 + 0x41) . $r;
+    return $r;
+}
+
 function getColourArray() {
 	$return=array() ;
-	
+
 	$return[0]="54, 175, 56" ;
 	$return[1]="192, 89, 203" ;
 	$return[2]="30, 30, 200" ;
@@ -32,7 +39,7 @@ function getColourArray() {
 	$return[9]="86, 117, 57" ;
 	$return[10]="114, 66, 47" ;
 	$return[11]="93, 55, 98" ;
-	
+
 	return $return ;
 }
 
