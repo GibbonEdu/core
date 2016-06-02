3c3533ca2a6e4e40ba19ddec4761f94ffbb9aedd
rossdotparker <rossdotparker@gmail.com>
2016-06-02 17:10:49 +0800

    Finance Export Fixes

--- modules/Finance/expenses_manage_processBulkExportContents.php
+++ modules/Finance/expenses_manage_processBulkExportContents.php
@@ -40,10 +40,6 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/expenses_manage.ph
         echo __($guid, 'List of invoices or budget cycle have not been specified, and so this export cannot be completed.');
         echo '</div>';
     } else {
-        echo '<h1>';
-        echo __($guid, 'Expense Export');
-        echo '</h1>';
-
         try {
             $whereCount = 0;
             $whereSched = '(';
@@ -78,42 +74,73 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/expenses_manage.ph
 		$excel->getProperties()->setTitle('Expenses');
 		$excel->getProperties()->setSubject('Expense Export');
 		$excel->getProperties()->setDescription('Expense Export');
-		
-		
+
+        //Create border and fill style
+        $style_border = array('borders' => array('right' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'left' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'top' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'bottom' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e'))));
+        $style_head_fill = array('fill' => array('type' => PHPExcel_Style_Fill::FILL_SOLID, 'color' => array('rgb' => 'B89FE2')));
+
+        //Auto set column widths
+        for($col = 'A'; $col !== 'I'; $col++)
+            $excel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(0, 1, __($guid, 'Expense Number'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(0, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(0, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(1, 1, __($guid, 'Budget'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(1, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(1, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(2, 1, __($guid, 'Budget Cycle'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(2, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(2, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(3, 1, __($guid, 'Title'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(3, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(3, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(4, 1, __($guid, 'Status'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(4, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(4, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(5, 1, __($guid, 'Cost')." (".$_SESSION[$guid]['currency'].')');
+        $excel->getActiveSheet()->getStyleByColumnAndRow(5, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(5, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(6, 1, __($guid, 'Staff'));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(6, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(6, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(7, 1, __($guid, 'Timestamp')." (".$_SESSION[$guid]['currency'].')');
+        $excel->getActiveSheet()->getStyleByColumnAndRow(7, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(7, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->getStyle("1:1")->getFont()->setBold(true);
 
 
-        $count = 0;
+        $count = 1;
         while ($row = $result->fetch()) {
             ++$count;
  			//Column A
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(0, $r, $row['gibbonFinanceExpenseID']);
- 			//Column B
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(1, $r, $row['budget']);
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(0, $count, $row['gibbonFinanceExpenseID']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(0, $count)->applyFromArray($style_border);
+            //Column B
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(1, $count, $row['budget']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(1, $count)->applyFromArray($style_border);
  			//Column C
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(2, $r, $row['budgetCycle']);
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(2, $count, $row['budgetCycle']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(2, $count)->applyFromArray($style_border);
  			//Column D
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(3, $r, $row['title']);
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(3, $count, $row['title']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(3, $count)->applyFromArray($style_border);
  			//Column E
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(4, $r, $row['status']);
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(4, $count, $row['status']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(4, $count)->applyFromArray($style_border);
  			//Column F
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(5, $r, number_format($row['cost'], 2, '.', ','));
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(5, $count, number_format($row['cost'], 2, '.', ','));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(5, $count)->applyFromArray($style_border);
  			//Column G
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(6, $r, formatName('', $row['preferredName'], $row['surname'], 'Staff', true, true));
- 			//Column G
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(6, $r, $row['timestampCreator']);
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(6, $count, formatName('', $row['preferredName'], $row['surname'], 'Staff', true, true));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(6, $count)->applyFromArray($style_border);
+ 			//Column H
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(7, $count, $row['timestampCreator']);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(7, $count)->applyFromArray($style_border);
         }
         if ($count == 0) {
  			//Column A
-			$excel->getActiveSheet()->setCellValueByColumnAndRow(0, $r, __($guid, 'There are no records to display.'));
+			$excel->getActiveSheet()->setCellValueByColumnAndRow(0, $count, __($guid, 'There are no records to display.'));
         }
 	    $_SESSION[$guid]['financeExpenseExportIDs'] = null;
 		$excel->exportWorksheet();
--- modules/Finance/invoices_manage_processBulkExportContents.php
+++ modules/Finance/invoices_manage_processBulkExportContents.php
@@ -54,20 +54,20 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/invoices_manage.ph
 		$whereSched = substr($whereSched, 0, -4).')';
 
 		//SQL for billing schedule AND pending
-		$sql = "(SELECT gibbonFinanceInvoice.gibbonFinanceInvoiceID, surname, preferredName, gibbonPerson.gibbonPersonID, dob, gender, 
-				studentID, gibbonFinanceInvoice.invoiceTo, gibbonFinanceInvoice.status, gibbonFinanceInvoice.invoiceIssueDate, 
-				gibbonFinanceBillingSchedule.invoiceDueDate, paidDate, paidAmount, gibbonFinanceBillingSchedule.name AS billingSchedule, 
-				NULL AS billingScheduleExtra, notes, gibbonRollGroup.name AS rollGroup 
-			FROM gibbonFinanceInvoice 
-				JOIN gibbonFinanceBillingSchedule ON (gibbonFinanceInvoice.gibbonFinanceBillingScheduleID=gibbonFinanceBillingSchedule.gibbonFinanceBillingScheduleID) 
-				JOIN gibbonFinanceInvoicee ON (gibbonFinanceInvoice.gibbonFinanceInvoiceeID=gibbonFinanceInvoicee.gibbonFinanceInvoiceeID) 
-				JOIN gibbonPerson ON (gibbonFinanceInvoicee.gibbonPersonID=gibbonPerson.gibbonPersonID) 
-				LEFT JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID 
-					AND gibbonStudentEnrolment.gibbonSchoolYearID=gibbonFinanceInvoice.gibbonSchoolYearID) 
-				LEFT JOIN gibbonRollGroup ON (gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID) 
-			WHERE gibbonFinanceInvoice.gibbonSchoolYearID=:gibbonSchoolYearID 
-				AND billingScheduleType='Scheduled' 
-				AND gibbonFinanceInvoice.status='Pending' 
+		$sql = "(SELECT gibbonFinanceInvoice.gibbonFinanceInvoiceID, surname, preferredName, gibbonPerson.gibbonPersonID, dob, gender,
+				studentID, gibbonFinanceInvoice.invoiceTo, gibbonFinanceInvoice.status, gibbonFinanceInvoice.invoiceIssueDate,
+				gibbonFinanceBillingSchedule.invoiceDueDate, paidDate, paidAmount, gibbonFinanceBillingSchedule.name AS billingSchedule,
+				NULL AS billingScheduleExtra, notes, gibbonRollGroup.name AS rollGroup
+			FROM gibbonFinanceInvoice
+				JOIN gibbonFinanceBillingSchedule ON (gibbonFinanceInvoice.gibbonFinanceBillingScheduleID=gibbonFinanceBillingSchedule.gibbonFinanceBillingScheduleID)
+				JOIN gibbonFinanceInvoicee ON (gibbonFinanceInvoice.gibbonFinanceInvoiceeID=gibbonFinanceInvoicee.gibbonFinanceInvoiceeID)
+				JOIN gibbonPerson ON (gibbonFinanceInvoicee.gibbonPersonID=gibbonPerson.gibbonPersonID)
+				LEFT JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID
+					AND gibbonStudentEnrolment.gibbonSchoolYearID=gibbonFinanceInvoice.gibbonSchoolYearID)
+				LEFT JOIN gibbonRollGroup ON (gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID)
+			WHERE gibbonFinanceInvoice.gibbonSchoolYearID=:gibbonSchoolYearID
+				AND billingScheduleType='Scheduled'
+				AND gibbonFinanceInvoice.status='Pending'
 				AND $whereSched)";
 		$sql .= ' UNION ';
 		//SQL for Ad Hoc AND pending
@@ -87,26 +87,59 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/invoices_manage.ph
 		$excel->getProperties()->setTitle('Invoices');
 		$excel->getProperties()->setSubject('Invoice Export');
 		$excel->getProperties()->setDescription('Invoice Export');
-		
-		
+
+        //Create border and fill style
+        $style_border = array('borders' => array('right' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'left' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'top' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e')), 'bottom' => array('style' => PHPExcel_Style_Border::BORDER_THIN, 'color' => array('argb' => '766f6e'))));
+        $style_head_fill = array('fill' => array('type' => PHPExcel_Style_Fill::FILL_SOLID, 'color' => array('rgb' => 'B89FE2')));
+
+        //Auto set column widths
+        for($col = 'A'; $col !== 'I'; $col++)
+            $excel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(0, 1, __($guid, "Invoice Number"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(0, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(0, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(1, 1, __($guid, "Student"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(1, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(1, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(2, 1, __($guid, "Roll Group"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(2, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(2, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(3, 1, __($guid, "Invoice To"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(3, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(3, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(4, 1, __($guid, "DOB"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(4, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(4, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(5, 1, __($guid, "Gender"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(5, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(5, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(6, 1, __($guid, "Status"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(6, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(6, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(7, 1, __($guid, "Schedule"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(7, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(7, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(8, 1, __($guid, "Total Value") . '(' . $_SESSION[$guid]["currency"] .')');
+        $excel->getActiveSheet()->getStyleByColumnAndRow(8, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(8, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(9, 1, __($guid, "Issue Date"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(9, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(9, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(10, 1, __($guid, "Due Date"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(10, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(10, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(11, 1, __($guid, "Date Paid"));
+        $excel->getActiveSheet()->getStyleByColumnAndRow(11, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(11, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->setCellValueByColumnAndRow(12, 1, __($guid, "Amount Paid") . " (" . $_SESSION[$guid]["currency"] . ")" );
+        $excel->getActiveSheet()->getStyleByColumnAndRow(12, 1)->applyFromArray($style_border);
+        $excel->getActiveSheet()->getStyleByColumnAndRow(12, 1)->applyFromArray($style_head_fill);
 		$excel->getActiveSheet()->getStyle("1:1")->getFont()->setBold(true);
 
 		$r = 2;
 		$count = 0;
-	
+
 		while ($row=$result->fetch()) {
 			$count++ ;
 			//Column A
@@ -120,41 +153,47 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/invoices_manage.ph
 			else {
 				$excel->getActiveSheet()->setCellValueByColumnAndRow(0, $r, ltrim($row["gibbonFinanceInvoiceID"], "0"));
 			}
+            $excel->getActiveSheet()->getStyleByColumnAndRow(0, $r)->applyFromArray($style_border);
 			//Column B
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(1, $r, formatName("", htmlPrep($row["preferredName"]), htmlPrep($row["surname"]), "Student", true));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(1, $r)->applyFromArray($style_border);
 			//Column C
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(2, $r, $row["rollGroup"]);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(2, $r)->applyFromArray($style_border);
 			//Column D
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(3, $r, $row["invoiceTo"]);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(3, $r)->applyFromArray($style_border);
 			//Column E
-			if ($row["dob"]!="") {
-				$excel->getActiveSheet()->setCellValueByColumnAndRow(4, $r, dateConvertBack($guid, $row["dob"]));
-			}
+            $excel->getActiveSheet()->setCellValueByColumnAndRow(4, $r, dateConvertBack($guid, $row["dob"]));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(4, $r)->applyFromArray($style_border);
 			//Column F
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(5, $r, $row["gender"]);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(5, $r)->applyFromArray($style_border);
 			//Column G
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(6, $r, $row["status"]);
+            $excel->getActiveSheet()->getStyleByColumnAndRow(6, $r)->applyFromArray($style_border);
 			//Column H
 			if ($row["billingScheduleExtra"]!="")  {
 				$excel->getActiveSheet()->setCellValueByColumnAndRow(7, $r, $row["billingScheduleExtra"]);
 			}
-			else { 
+			else {
 				$excel->getActiveSheet()->setCellValueByColumnAndRow(7, $r, $row["billingSchedule"]);
 			}
+            $excel->getActiveSheet()->getStyleByColumnAndRow(7, $r)->applyFromArray($style_border);
 			//Column I
 			//Calculate total value
 			$totalFee=0 ;
 			$feeError = false ;
-			$dataTotal=array("gibbonFinanceInvoiceID"=>$row["gibbonFinanceInvoiceID"]); 
+			$dataTotal=array("gibbonFinanceInvoiceID"=>$row["gibbonFinanceInvoiceID"]);
 			if ($row["status"]=="Pending") {
-				$sqlTotal="SELECT gibbonFinanceInvoiceFee.fee AS fee, gibbonFinanceFee.fee AS fee2 
-					FROM gibbonFinanceInvoiceFee 
-						LEFT JOIN gibbonFinanceFee ON (gibbonFinanceInvoiceFee.gibbonFinanceFeeID=gibbonFinanceFee.gibbonFinanceFeeID) 
+				$sqlTotal="SELECT gibbonFinanceInvoiceFee.fee AS fee, gibbonFinanceFee.fee AS fee2
+					FROM gibbonFinanceInvoiceFee
+						LEFT JOIN gibbonFinanceFee ON (gibbonFinanceInvoiceFee.gibbonFinanceFeeID=gibbonFinanceFee.gibbonFinanceFeeID)
 					WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID" ;
 			}
 			else {
-				$sqlTotal="SELECT gibbonFinanceInvoiceFee.fee AS fee, NULL AS fee2 
-					FROM gibbonFinanceInvoiceFee 
+				$sqlTotal="SELECT gibbonFinanceInvoiceFee.fee AS fee, NULL AS fee2
+					FROM gibbonFinanceInvoiceFee
 					WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID" ;
 			}
 			if (is_null($resultTotal=$pdo->executeQuery($dataTotal, $sqlTotal)))
@@ -178,144 +217,26 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/invoices_manage.ph
 				$x .= number_format($totalFee, 2, ".", ",") ;
 				$excel->getActiveSheet()->setCellValueByColumnAndRow(8, $r, $x);
 			}
+            $excel->getActiveSheet()->getStyleByColumnAndRow(8, $r)->applyFromArray($style_border);
 			//Column J
-			if ($row["invoiceIssueDate"])
-				$excel->getActiveSheet()->setCellValueByColumnAndRow(9, $r, dateConvertBack($guid, $row["invoiceIssueDate"]));
+		    $excel->getActiveSheet()->setCellValueByColumnAndRow(9, $r, dateConvertBack($guid, $row["invoiceIssueDate"]));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(9, $r)->applyFromArray($style_border);
 			//Column K
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(10, $r, dateConvertBack($guid, $row["invoiceDueDate"]));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(10, $r)->applyFromArray($style_border);
 			//Column L
-			if ($row["paidDate"]!="") {
+            if ($row["paidDate"]!="")
 				$excel->getActiveSheet()->setCellValueByColumnAndRow(11, $r, dateConvertBack($guid, $row["paidDate"]));
-			}
+            else
+                $excel->getActiveSheet()->setCellValueByColumnAndRow(11, $r, '');
+            $excel->getActiveSheet()->getStyleByColumnAndRow(11, $r)->applyFromArray($style_border);
 			//Column M
 			$excel->getActiveSheet()->setCellValueByColumnAndRow(12, $r, number_format($row["paidAmount"], 2, ".", ","));
+            $excel->getActiveSheet()->getStyleByColumnAndRow(12, $r)->applyFromArray($style_border);
 			$r++;
 		}
-   		
+
 		$_SESSION[$guid]['financeInvoiceExportIDs'] = null;
 		$excel->exportWorksheet();
 	}
 }
-/*
-            if (! $feeError) {
-                if (substr($_SESSION[$guid]['currency'], 4) != '') {
-                    
-					$x = substr($_SESSION[$guid]['currency'], 4).' ';
-                }
-                $x .= number_format($totalFee, 2, '.', ',');
-            }
-
-
-
-
-
-
-
-			$r++;
-		}
-
-
-        $count = 0;
-/*        while ($row = $result->fetch()) {
-            ++$count;
-                //COLOR ROW BY STATUS!
-                echo '<tr>';
-            echo '<td>';
-            $invoiceNumber = getSettingByScope($connection2, 'Finance', 'invoiceNumber');
-            if ($invoiceNumber == 'Person ID + Invoice ID') {
-                echo ltrim($row['gibbonPersonID'], '0').'-'.ltrim($row['gibbonFinanceInvoiceID'], '0');
-            } elseif ($invoiceNumber == 'Student ID + Invoice ID') {
-                echo ltrim($row['studentID'], '0').'-'.ltrim($row['gibbonFinanceInvoiceID'], '0');
-            } else {
-                echo ltrim($row['gibbonFinanceInvoiceID'], '0');
-            }
-            echo '</td>';
-            echo '<td>';
-            echo formatName('', htmlPrep($row['preferredName']), htmlPrep($row['surname']), 'Student', true);
-            echo '</td>';
-            echo '<td>';
-            echo $row['rollGroup'];
-            echo '</td>';
-            echo '<td>';
-            echo $row['invoiceTo'];
-            echo '</td>';
-            echo '<td>';
-            if ($row['dob'] != '') {
-                echo dateConvertBack($guid, $row['dob']);
-            }
-            echo '</td>';
-            echo '<td>';
-            echo $row['gender'];
-            echo '</td>';
-            echo '<td>';
-            echo $row['status'];
-            echo '</td>';
-            echo '<td>';
-            if ($row['billingScheduleExtra'] != '') {
-                echo $row['billingScheduleExtra'];
-            } else {
-                echo $row['billingSchedule'];
-            }
-            echo '</td>';
-            echo '<td>';
-
-			//Calculate total value
-			$totalFee = 0;
-            $feeError = false;
-            try {
-                $dataTotal = array('gibbonFinanceInvoiceID' => $row['gibbonFinanceInvoiceID']);
-                if ($row['status'] == 'Pending') {
-                    $sqlTotal = 'SELECT gibbonFinanceInvoiceFee.fee AS fee, gibbonFinanceFee.fee AS fee2 FROM gibbonFinanceInvoiceFee LEFT JOIN gibbonFinanceFee ON (gibbonFinanceInvoiceFee.gibbonFinanceFeeID=gibbonFinanceFee.gibbonFinanceFeeID) WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID';
-                } else {
-                    $sqlTotal = 'SELECT gibbonFinanceInvoiceFee.fee AS fee, NULL AS fee2 FROM gibbonFinanceInvoiceFee WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID';
-                }
-                $resultTotal = $connection2->prepare($sqlTotal);
-                $resultTotal->execute($dataTotal);
-            } catch (PDOException $e) {
-                echo $e->getMessage();
-                echo '<i>Error calculating total</i>';
-                $feeError = true;
-            }
-            while ($rowTotal = $resultTotal->fetch()) {
-                if (is_numeric($rowTotal['fee2'])) {
-                    $totalFee += $rowTotal['fee2'];
-                } else {
-                    $totalFee += $rowTotal['fee'];
-                }
-            }
-            if ($feeError == false) {
-                if (substr($_SESSION[$guid]['currency'], 4) != '') {
-                    echo substr($_SESSION[$guid]['currency'], 4).' ';
-                }
-                echo number_format($totalFee, 2, '.', ',');
-            }
-            echo '</td>';
-            echo '<td>';
-            echo dateConvertBack($guid, $row['invoiceIssueDate']);
-            echo '</td>';
-            echo '<td>';
-            echo dateConvertBack($guid, $row['invoiceDueDate']);
-            echo '</td>';
-            echo '<td>';
-            if ($row['paidDate'] != '') {
-                echo dateConvertBack($guid, $row['paidDate']);
-            }
-            echo '</td>';
-            echo '<td>';
-            echo number_format($row['paidAmount'], 2, '.', ',');
-            echo '</td>';
-            echo '</tr>';
-        }
-        if ($count == 0) {
-            echo '<tr>';
-            echo '<td colspan=2>';
-            echo __($guid, 'There are no records to display.');
-            echo '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    }
-
-    $_SESSION[$guid]['financeInvoiceExportIDs'] = null;
-}
-*/
