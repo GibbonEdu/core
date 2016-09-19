28e50a73d485202c622f9e768ad944891b6a906e
Craig Rayner <craig@craigrayner.com>
2016-09-19 18:13:42 +1000

    Add Top of Page Anchor

--- src/themes/Bootstrap/views/default/sidebar/pageAnchors.php
+++ src/themes/Bootstrap/views/default/sidebar/pageAnchors.php
@@ -1,12 +1,17 @@
 <?php if ($this->session->notEmpty('pageAnchors')) { 
 	
-	$list = $this->startList('ul', 'moduleMenu')
-		->addHeader($this->h4('Page Anchors', array(), true));
-	foreach($this->session->get('pageAnchors') as $link=>$name)
-	{
-		$x = array('<a href="#'.$link.'">'.Gibbon\core\trans::__($name).'</a>');
+	$anchors = $this->session->get('pageAnchors');
+	if (! empty($anchors)) {
+		$list = $this->startList('ul', 'moduleMenu')
+			->addHeader($this->h4('Page Anchors', array(), true));
+		$x = array('<a href="#wrapOuter">'.Gibbon\core\trans::__('Top').'</a>');
 		$list->addListElement('%1$s', $x);
+		foreach($anchors as $link=>$name)
+		{
+			$x = array('<a href="#'.$link.'">'.Gibbon\core\trans::__($name).'</a>');
+			$list->addListElement('%1$s', $x);
+		}
+		$list->renderList($this);
 	}
-	$list->renderList($this);
 	$this->session->clear('pageAnchors');
 }
