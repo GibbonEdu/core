a8ad12841c9d50ad02e04a79b87cf5aa98cfbd41
Craig Rayner <craig@craigrayner.com>
2016-09-23 11:17:05 +1000

    Standardise CSS for forms.

--- src/themes/Bootstrap/views/form/style/standard.php
+++ src/themes/Bootstrap/views/form/style/standard.php
@@ -1,6 +1,6 @@
 <form<?php echo isset($el->method) ? ' method="'.$el->method.'"' : ' method="post"'; ?> action="<?php echo $el->getTarget(); ?>"<?php echo ! $el->isEmpty('role') ? ' role="'.$el->get('role').'"' : ' role="form"' ; ?><?php echo ! $el->isEmpty('id') ? ' id="'.$el->get('id').'"' : ' id="TheForm"' ;?><?php echo ! $el->isEmpty('class') ? ' class="'.$el->get('class').'"' : '' ;?><?php echo ! $el->isEmpty('enctype') ? ' enctype="'.$el->get('enctype').'"' : '' ;?><?php echo ! is_null($params->get('additional')) ? $params->get('additional') : '' ; ?>>
     <div class='container-fluid'>
-    	<div class='well standard-well'> 
+    	<div class='well'> 
     	<?php $this->render('form.style.elements', $el); ?>	
         </div>
     </div>
