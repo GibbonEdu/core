04803822730fe905d6ec0035789d4feae5579371
Craig Rayner <craig@craigrayner.com>
2016-09-29 10:15:39 +1000

    Feature: Remove SMS Settings from School Admin/Messenger Settings

--- src/modules/School Admin/messengerSettings.php
+++ src/modules/School Admin/messengerSettings.php
@@ -34,31 +34,6 @@ if ($this->getSecurity()->isActionAccessible()) {
 	$this->h2('Messenger Settings');
 	$form = $this->getForm(null, array('q' => '/modules/School Admin/messengerSettingsProcess.php'), true);
 
-	$form->addElement('h3', null, 'SMS Settings');
-	
-	$el = $form->addElement('info', null, 'Gibbon is designed to use the %1$sOne Way SMS%2$s gateway to send out SMS messages. This is a paid service, not affiliated with Gibbon, and you must create your own account with them before being able to send out SMSs using the Messenger module. It is possible that completing the fields below with details from other gateways may work.');
-	$el->valueParameters = array("<a href='http://onewaysms.com' target='_blank'>", '</a>');
-
-	$el = $form->addElement('text', null);
-	$el->injectRecord($this->config->getSetting('smsUsername', 'Messenger'));
-	$el->value = $this->htmlPrep($el->value);
-	$el->setMaxLength(50); 
-	
-	$el = $form->addElement('text', null);
-	$el->injectRecord($this->config->getSetting('smsPassword', 'Messenger'));
-	$el->value = $this->htmlPrep($el->value);
-	$el->setMaxLength(50); 
-
-	$el = $form->addElement('text', null);
-	$el->injectRecord($this->config->getSetting('smsURL', 'Messenger'));
-	$el->value = $this->htmlPrep($el->value);
-	$el->setMaxLength(250); 
-
-	$el = $form->addElement('text', null);
-	$el->injectRecord($this->config->getSetting('smsURLCredit', 'Messenger'));
-	$el->value = $this->htmlPrep($el->value);
-	$el->setMaxLength(250); 
-
 	$form->addElement('h3', null, 'Message Wall Settings');
 
 	$el = $form->addElement('select', null);
