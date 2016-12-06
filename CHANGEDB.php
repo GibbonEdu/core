103bd0835c08386fed65a01f2ab12a94572719c4
rossdotparker <rossdotparker@gmail.com>
2016-12-06 10:35:23 +0800

    System: added Odia as development language

--- CHANGEDB.php
+++ CHANGEDB.php
@@ -658,4 +658,5 @@ ALTER TABLE `gibbonFirstAid` CHANGE `gibbonCourseClassID` `gibbonCourseClassID` 
 UPDATE gibbonSetting SET value=(SELECT email FROM gibbonPerson WHERE gibbonPersonID=1) WHERE scope='System' AND name='organisationEmail' AND value='';end
 ALTER TABLE `gibbonCourse` ADD `map` ENUM('Y','N') NOT NULL DEFAULT 'Y' COMMENT 'Should this course be included in curriculum maps and other summaries?' AFTER `description`;end
 INSERT INTO `gibbonLanguage` (`gibbonLanguageID`, `name`) VALUES (NULL, 'Odia');
+INSERT INTO `gibboni18n` (`code`, `name`, `active`, `systemDefault`, `maintainerName`, `maintainerWebsite`, `dateFormat`, `dateFormatRegEx`, `dateFormatPHP`,`rtl`) VALUES ('in_OR', 'ଓଡ଼ିଆ - इंडिया', 'N', 'N', 'Saumya Kanta Swain', 'http://www.skswain.in', 'dd-mm-yyyy', '/^(0[1-9]|[12][0-9]|3[01])[-](0[1-9]|1[012])[-](19|20)\\\d\\\d$/i', 'd-m-Y', 'N');end
 ";
