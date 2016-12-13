14fb9e32f32058f18fd73829d771837c8d08e754
Sandra Kuipers <Sandra.Kuipers@tis.edu.mo>
2016-12-13 17:18:18 +0800

    Students: Clean up application links on delete

--- modules/Students/applicationForm_manage_deleteProcess.php
+++ modules/Students/applicationForm_manage_deleteProcess.php
@@ -70,6 +70,18 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/applicationForm_m
                 exit();
             }
 
+            // Clean up the links between this and other forms
+            try {
+                $data = array('gibbonApplicationFormID' => $gibbonApplicationFormID);
+                $sql = 'DELETE FROM gibbonApplicationFormLink WHERE gibbonApplicationFormID1=:gibbonApplicationFormID OR gibbonApplicationFormID2=:gibbonApplicationFormID';
+                $result = $connection2->prepare($sql);
+                $result->execute($data);
+            } catch (PDOException $e) {
+                $URL .= '&return=error2';
+                header("Location: {$URL}");
+                exit();
+            }
+
             $URLDelete = $URLDelete.'&return=success0';
             header("Location: {$URLDelete}");
         }
