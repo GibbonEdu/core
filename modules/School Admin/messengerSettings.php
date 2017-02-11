4dd02b443b1205059427b8fa09ccbb44192a5715
rossdotparker <rossdotparker@gmail.com>
2017-02-11 12:14:15 +0800

    Removed isRequired for SMS options.

--- modules/School Admin/messengerSettings.php
+++ modules/School Admin/messengerSettings.php
@@ -42,26 +42,26 @@ if (isActionAccessible($guid, $connection2, '/modules/School Admin/messengerSett
     $row = $form->addRow()->addHeading('SMS Settings');
 
     $row = $form->addRow()->addAlert( sprintf(__($guid, 'Gibbon is designed to use the %1$sOne Way SMS%2$s gateway to send out SMS messages. This is a paid service, not affiliated with Gibbon, and you must create your own account with them before being able to send out SMSs using the Messenger module. It is possible that completing the fields below with details from other gateways may work.'), "<a href='http://onewaysms.com' target='_blank'>", '</a>') );
-	
+
     $settingByScope = getSettingByScope($connection2, 'Messenger', 'smsUsername', true);
 	$row = $form->addRow();
     	$row->addLabel($settingByScope['name'], $settingByScope['nameDisplay'])->description($settingByScope['description']);
-		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value'])->isRequired();
+		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value']);
 
 	$settingByScope = getSettingByScope($connection2, 'Messenger', 'smsPassword', true);
 	$row = $form->addRow();
     	$row->addLabel($settingByScope['name'], $settingByScope['nameDisplay'])->description($settingByScope['description']);
-		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value'])->isRequired();
+		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value']);
 
 	$settingByScope = getSettingByScope($connection2, 'Messenger', 'smsURL', true);
 	$row = $form->addRow();
     	$row->addLabel($settingByScope['name'], $settingByScope['nameDisplay'])->description($settingByScope['description']);
-		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value'])->isRequired();
+		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value']);
 
 	$settingByScope = getSettingByScope($connection2, 'Messenger', 'smsURLCredit', true);
 	$row = $form->addRow();
     	$row->addLabel($settingByScope['name'], $settingByScope['nameDisplay'])->description($settingByScope['description']);
-		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value'])->isRequired();
+		$row->addTextField($settingByScope['name'])->setValue($settingByScope['value']);
 
 
 	$row = $form->addRow()->addHeading('Message Wall Settings');
