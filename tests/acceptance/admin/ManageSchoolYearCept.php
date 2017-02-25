4a17c0bc55d585bc8022cb67311d1096c1514f7a
Sandra Kuipers <sandra@skuipers.com>
2017-02-25 20:43:04 +0800

    Tests: Updated Manage School Year test to include all fields

--- tests/acceptance/admin/ManageSchoolYearCept.php
+++ tests/acceptance/admin/ManageSchoolYearCept.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('add, edit and delete a school year');
 $I->loginAsAdmin();
@@ -10,7 +10,7 @@ $I->see('Add School Year', '.trailEnd');
 
 $I->fillField('name', '2020-2021');
 $I->selectOption('status', 'Upcoming');
-$I->fillField('sequenceNumber', 99);
+$I->fillField('sequenceNumber', 100);
 $I->fillField('firstDay', '01/01/2020');
 $I->fillField('lastDay', '31/12/2020');
 $I->click('Submit');
@@ -23,14 +23,18 @@ $gibbonSchoolYearID = $I->grabFromCurrentUrl('/editID=(\d+)/');
 $I->amOnPage('/index.php?q=/modules/School Admin/schoolYear_manage_edit.php&gibbonSchoolYearID='.$gibbonSchoolYearID);
 $I->see('Edit School Year', '.trailEnd');
 
+$I->fillField('name', '1920-1921');
+$I->selectOption('status', 'Past');
 $I->fillField('sequenceNumber', 90);
-$I->fillField('firstDay', '01/01/2021');
-$I->fillField('lastDay', '31/12/2021');
+$I->fillField('firstDay', '01/01/1920');
+$I->fillField('lastDay', '31/12/1920');
 $I->click('Submit');
 
+$I->seeInField('name', '1920-1921');
+$I->seeOptionIsSelected('status', 'Past');
 $I->seeInField('sequenceNumber', 90);
-$I->seeInField('firstDay', '01/01/2021');
-$I->seeInField('lastDay', '31/12/2021');
+$I->seeInField('firstDay', '01/01/1920');
+$I->seeInField('lastDay', '31/12/1920');
 
 // Delete
 $I->amOnPage('/index.php?q=/modules/School Admin/schoolYear_manage_delete.php&gibbonSchoolYearID='.$gibbonSchoolYearID);
