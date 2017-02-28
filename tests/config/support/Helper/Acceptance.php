0c8fa47da7fe365bd4c32197f1bc0672e0b4158c
Sandra Kuipers <sandra@skuipers.com>
2017-02-28 15:42:23 +0800

    Tests: Added an Acceptance helper method for grabbing all values in a form

--- tests/config/support/Helper/Acceptance.php
+++ tests/config/support/Helper/Acceptance.php
@@ -6,5 +6,33 @@ namespace Helper;
 
 class Acceptance extends \Codeception\Module
 {
+    /**
+     * Grab all values from a form (generally for the purposes of restoring later)
+     *
+     * @param    string  $selector
+     * @return   [type]
+     */
+    public function grabAllFormValues($selector = '#content form') {
+        $elements = $this->getModule('PhpBrowser')->_findElements("$selector input, $selector textarea, $selector select");
 
+        $formValues = array();
+        foreach ($elements as $element) {
+            $type = $element->getAttribute('type');
+            if ($type == 'submit' || $type == 'button') continue;
+
+            $name = $element->getAttribute('name');
+            $value = $element->getAttribute('value');
+
+            switch($type) {
+                case 'checkbox':    if ($element->hasAttribute('checked')) {
+                                        $value = ($element->hasAttribute('value'))? $element->getAttribute('value') : 'on';
+                                    }
+                                    break;
+            }
+            
+            $formValues[$name] = $value;
+        }
+        
+        return $formValues;
+    }
 }
