e82ae25c67676f4f50c488ea82967903643ad042
Sandra Kuipers <sandra@skuipers.com>
2017-04-30 19:22:43 +0800

    System: Added notification events for three failed Login attempts

--- CHANGEDB.php
+++ CHANGEDB.php
@@ -759,5 +759,6 @@ INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `typ
 INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `type`, `scopes`, `active`) VALUES ('Finance Data Updates', 'Data Updater', 'Finance Data Updates', 'Core', 'All', 'Y');end
 INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `type`, `scopes`, `active`) VALUES ('Medical Form Updates', 'Data Updater', 'Medical Form Updates', 'Core', 'All', 'Y');end
 INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `type`, `scopes`, `active`) VALUES ('Personal Data Updates', 'Data Updater', 'Personal Data Updates', 'Core', 'All', 'Y');end
+INSERT INTO `gibbonNotificationEvent` (`event`, `moduleName`, `actionName`, `type`, `scopes`, `active`) VALUES ('Login - Failed', 'User Admin', 'Manage Users', 'Core', 'All', 'Y');end
 
 ";
