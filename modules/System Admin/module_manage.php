2141d62355511774970d5ee70927d5ee2d9e4ef4
James Peru Mmbono <jmsperu@gmail.com>
2017-07-29 15:42:08 +0300

    Update module_manage.php
    Added ability to upload module from module settings.

--- modules/System Admin/module_manage.php
+++ modules/System Admin/module_manage.php
@@ -267,3 +267,74 @@ if (isActionAccessible($guid, $connection2, '/modules/System Admin/module_manage
     }
 }
 ?>
+<?php  
+ if(isset($_POST["btn_zip"]))  
+ {  
+      $output = '';  
+      if($_FILES['zip_file']['name'] != '')  
+      {  
+           $file_name = $_FILES['zip_file']['name'];  
+           $array = explode(".", $file_name);  
+           $name = $array[0];  
+           $ext = $array[1];  
+           if($ext == 'zip')  
+           {  
+                $path = 'modules/';  
+                $location = $path . $file_name;  
+                if(move_uploaded_file($_FILES['zip_file']['tmp_name'], $location))  
+                {  
+                     $zip = new ZipArchive;  
+                     if($zip->open($location))  
+                     {  
+                          $zip->extractTo($path);  
+                          $zip->close();  
+                     }  
+                     $files = scandir($path . $name);  
+                     //$name is extract folder from zip file  
+                     foreach($files as $file)  
+                     {  
+                          $file_ext = end(explode(".", $file));  
+                          $allowed_ext = array('jpg', 'png');  
+                          if(in_array($file_ext, $allowed_ext))  
+                          {  
+                               $new_name = md5(rand()).'.' . $file_ext;  
+                               $output .= '<div class="col-md-6"><div style="padding:16px; border:1px solid #CCC;"><img src="upload/'.$new_name.'" width="170" height="240" /></div></div>';  
+                               copy($path.$name.'/'.$file, $path . $new_name);  
+                               unlink($path.$name.'/'.$file);  
+                          }       
+                     }  
+                     unlink($location);  
+                     rmdir($path . $name);  
+                }  
+           }  
+      }  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+                     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br />  
+           <div class="container" style="width:500px;">  
+                <h3 align="">Upload a module</h3><br />  
+                <form method="post" enctype="multipart/form-data">  
+                     <label>Please Select Zip File</label>  
+                     <input type="file" name="zip_file" />  
+                     <br />  
+                     <input type="submit" name="btn_zip" class="btn btn-info" value="Upload" />  
+                </form>  
+                <br />  
+                <?php  
+                if(isset($output))  
+                {  
+                     echo $output;  
+                }  
+                ?>  
+           </div>  
+           <br />  
+      </body>  
+ </html>  
