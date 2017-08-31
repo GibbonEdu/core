b21f8ec5032547ad7714634008b55d5f64b02aad
Sandra Kuipers <sandra@skuipers.com>
2017-08-31 15:26:41 +0800

    Students: fixed visual glitch with Application Form table widths

--- modules/Students/applicationForm.php
+++ modules/Students/applicationForm.php
@@ -384,7 +384,7 @@ if ($proceed == false) {
 
     for ($i = 1; $i < 3; ++$i) {
         $row = $table->addRow();
-        $row->addTextField('schoolName'.$i)->maxLength(50)->setSize(20);
+        $row->addTextField('schoolName'.$i)->maxLength(50)->setSize(18);
         $row->addTextField('schoolAddress'.$i)->maxLength(255)->setSize(20);
         $row->addTextField('schoolGrades'.$i)->maxLength(20)->setSize(8);
         $row->addTextField('schoolLanguage'.$i)->autocomplete($languages)->setSize(10);
@@ -728,7 +728,7 @@ if ($proceed == false) {
             $name = formatName('', $rowSibling['preferredName'], $rowSibling['surname'], 'Student');
 
             $row = $table->addRow();
-            $row->addTextField('siblingName'.$rowCount)->maxLength(50)->setSize(30)->setValue($name);
+            $row->addTextField('siblingName'.$rowCount)->maxLength(50)->setSize(26)->setValue($name);
             $row->addDate('siblingDOB'.$rowCount)->setSize(10)->setValue(dateConvertBack($guid, $rowSibling['dob']));
             $row->addTextField('siblingSchool'.$rowCount)->maxLength(50)->setSize(30)->setValue($_SESSION[$guid]['organisationName']);
             $row->addDate('siblingSchoolJoiningDate'.$rowCount)->setSize(10)->setValue(dateConvertBack($guid, $rowSibling['dateStart']));
@@ -740,7 +740,7 @@ if ($proceed == false) {
     // Add additional sibling rows up to 3
     for ($i = $rowCount; $i <= 3; ++$i) {
         $row = $table->addRow();
-        $nameField = $row->addTextField('siblingName'.$i)->maxLength(50)->setSize(30);
+        $nameField = $row->addTextField('siblingName'.$i)->maxLength(50)->setSize(26);
         $dobField = $row->addDate('siblingDOB'.$i)->setSize(10);
         $row->addTextField('siblingSchool'.$i)->maxLength(50)->setSize(30);
         $row->addDate('siblingSchoolJoiningDate'.$i)->setSize(10);
