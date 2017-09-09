011807328c15c649130e8dc351fe1f5d12303357
Sandra Kuipers <sandra@skuipers.com>
2017-09-09 14:18:07 +0800

    School Admin: Manage Departments OOification

--- modules/School Admin/department_manage_edit.php
+++ modules/School Admin/department_manage_edit.php
@@ -91,278 +91,83 @@ if (isActionAccessible($guid, $connection2, '/modules/School Admin/department_ma
 
             $row = $form->addRow();
                 $row->addLabel('type', 'Type');
-                $row->addSelect('type')->fromArray($types)->isRequired()->selected($values['type']);
+                $row->addTextField('type')->readOnly();
 
             $row = $form->addRow();
                 $row->addLabel('name', 'Name');
-                $row->addTextField('name')->maxLength(40)->isRequired()->setValue($values['name']);
+                $row->addTextField('name')->maxLength(40)->isRequired();
 
             $row = $form->addRow();
                 $row->addLabel('nameShort', 'Short Name');
-                $row->addTextField('nameShort')->maxLength(4)->isRequired()->setValue($values['nameShort']);
+                $row->addTextField('nameShort')->maxLength(4)->isRequired();
 
             $row = $form->addRow();
                 $row->addLabel('subjectListing', 'Subject Listing');
-                $row->addTextField('subjectListing')->maxLength(255)->setValue($values['subjectListing']);
+                $row->addTextField('subjectListing')->maxLength(255);
 
             $row = $form->addRow();
                $column = $row->addColumn()->setClass('');
                $column->addLabel('blurb', 'Blurb');
-               $column->addEditor('blurb', $guid)->setValue($values['blurb']);
+               $column->addEditor('blurb', $guid);
 
             $row = $form->addRow();
                 $row->addLabel('file', 'Logo')->description('125x125px jpg/png/gif');
                 $row->addFileUpload('file')
                     ->accepts('.jpg,.jpeg,.gif,.png')
                     ->append('<br/><br/>'.getMaxUpload($guid))
-                    ->addClass('right')
-                    ->setValue($values['logo']);
+                    ->addClass('right');
+
+            $form->addRow()->addHeading(__('Current Staff'));
+
+            $data = array('gibbonDepartmentID' => $gibbonDepartmentID);
+            $sql = "SELECT preferredName, surname, gibbonDepartmentStaff.* FROM gibbonDepartmentStaff JOIN gibbonPerson ON (gibbonDepartmentStaff.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonDepartmentID=:gibbonDepartmentID AND gibbonPerson.status='Full' ORDER BY surname, preferredName";
+
+            $results = $pdo->executeQuery($data, $sql);
+
+            if ($results->rowCount() == 0) {
+                $form->addRow()->addAlert(__('There are no records to display.'), 'error');
+            } else {
+                $form->addRow()->addContent('<b>'.__('Warning').'</b>: '.__('If you delete a member of staff, any unsaved changes to this record will be lost!'))->wrap('<i>', '</i>');
+
+                $table = $form->addRow()->addTable()->addClass('colorOddEven');
+
+                $header = $table->addHeaderRow();
+                $header->addContent(__('Name'));
+                $header->addContent(__('Role'));
+                $header->addContent(__('Action'));
+
+                while ($staff = $results->fetch()) {
+                    $row = $table->addRow();
+                    $row->addContent(formatName('', $staff['preferredName'], $staff['surname'], 'Staff', true, true));
+                    $row->addContent($staff['role']);
+                    $row->addContent("<a onclick='return confirm(\"".__($guid, 'Are you sure you wish to delete this record?')."\")' href='".$_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/department_manage_edit_staff_deleteProcess.php?address='.$_GET['q'].'&gibbonDepartmentStaffID='.$staff['gibbonDepartmentStaffID']."&gibbonDepartmentID=$gibbonDepartmentID'><img title='".__($guid, 'Delete')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/garbage.png'/></a>");
+                }
+            }
+
+            $form->addRow()->addHeading(__('New Staff'));
 
             $row = $form->addRow();
-                $row->addLabel('staff', 'Staff')->description('Use Control, Command and/or Shift to select multiple.');
+                $row->addLabel('staff', 'Staff');
                 $row->addSelectStaff('staff')->selectMultiple();
 
-            $form->toggleVisibilityByClass('roleLARow')->onSelect('type')->when('Learning Area');
-
-            $row = $form->addRow()->setClass('roleLARow');
-                $row->addLabel('roleLA', 'Role');
-                $row->addSelect('roleLA')->fromArray($typesLA)->selected($values['roleLA']);
-
-            $form->toggleVisibilityByClass('roleAdmin')->onSelect('type')->when('Administration');
-
-            $row = $form->addRow()->setClass('roleAdmin');
-                $row->addLabel('roleAdmin', 'Role');
-                $row->addSelect('roleAdmin')->fromArray($typesAdmin)->selected($values['roleAdmin']);
+            if ($values['type'] == 'Learning Area') {
+                $row = $form->addRow()->setClass('roleLARow');
+                    $row->addLabel('role', 'Role');
+                    $row->addSelect('role')->fromArray($typesLA);
+            } else {
+                $row = $form->addRow()->setClass('roleAdmin');
+                    $row->addLabel('role', 'Role');
+                    $row->addSelect('role')->fromArray($typesAdmin);
+            }
 
             $row = $form->addRow();
                 $row->addFooter();
                 $row->addSubmit();
 
+            $form->loadAllValuesFrom($values);
+
             echo $form->getOutput();
-
-
-            ?>
-			<form method="post" action="<?php echo $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/department_manage_editProcess.php?gibbonDepartmentID=$gibbonDepartmentID&address=".$_SESSION[$guid]['address'] ?>" enctype="multipart/form-data">
-				<table class='smallIntBorder fullWidth' cellspacing='0'>
-					<tr class='break'>
-						<td colspan=2>
-							<h3><?php echo __($guid, 'General Information') ?></h3>
-						</td>
-					</tr>
-					<tr>
-						<td style='width: 275px'>
-							<b><?php echo __($guid, 'Type') ?> *</b><br/>
-							<span class="emphasis small"><?php echo __($guid, 'This value cannot be changed.') ?></i><br/></span>
-						</td>
-						<td class="right">
-							<?php $type = $row['type']; ?>
-							<input readonly name="type" id="type" value="<?php echo $type ?>" type="text" class="standardWidth">
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<b><?php echo __($guid, 'Name') ?> *</b><br/>
-						</td>
-						<td class="right">
-							<input name="name" id="name" maxlength=40 value="<?php echo $row['name'] ?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var name2=new LiveValidation('name');
-								name2.add(Validate.Presence);
-							</script>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<b><?php echo __($guid, 'Short Name') ?> *</b><br/>
-						</td>
-						<td class="right">
-							<input name="nameShort" id="nameShort" maxlength=4 value="<?php echo $row['nameShort'] ?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var nameShort=new LiveValidation('nameShort');
-								nameShort.add(Validate.Presence);
-							</script>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<b><?php echo __($guid, 'Subject Listing') ?></b><br/>
-						</td>
-						<td class="right">
-							<input name="subjectListing" id="subjectListing" maxlength=255 value="<?php echo $row['subjectListing'] ?>" type="text" class="standardWidth">
-						</td>
-					</tr>
-					<tr>
-						<td colspan=2>
-							<b><?php echo __($guid, 'Blurb') ?></b>
-							<?php echo getEditor($guid,  true, 'blurb', $row['blurb'], 20) ?>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<b><?php echo __($guid, 'Logo') ?></b><br/>
-							<span class="emphasis small">125x125px jpg/png/gif</i><br/></span>
-							<?php if ($row['logo'] != '') { ?>
-							<span class="emphasis small"><?php echo __($guid, 'Will overwrite existing attachment.') ?></span>
-							<?php
-							}
-            				?>
-						</td>
-						<td class="right">
-							<?php
-                            if ($row['logo'] != '') {
-                                echo __($guid, 'Current attachment:')." <a href='".$_SESSION[$guid]['absoluteURL'].'/'.$row['logo']."'>".$row['logo'].'</a><br/><br/>';
-                            }
-            				?>
-							<input type="file" name="file" id="file"><br/><br/>
-							<?php
-                            echo getMaxUpload($guid);
-                            $ext = "'.png','.jpeg','.jpg','.gif'";
-                            ?>
-
-							<script type="text/javascript">
-								var file=new LiveValidation('file');
-								file.add( Validate.Inclusion, { within: [<?php echo $ext; ?>], failureMessage: "<?php echo __($guid, 'Illegal file type!') ?>", partialMatch: true, caseSensitive: false } );
-							</script>
-						</td>
-					</tr>
-					<tr class='break'>
-						<td colspan=2>
-							<h3><?php echo __($guid, 'Current Staff') ?></h3>
-						</td>
-					</tr>
-					<tr>
-						<td colspan=2>
-							<?php
-                            try {
-                                $data = array('gibbonDepartmentID' => $gibbonDepartmentID);
-                                $sql = "SELECT preferredName, surname, gibbonDepartmentStaff.* FROM gibbonDepartmentStaff JOIN gibbonPerson ON (gibbonDepartmentStaff.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonDepartmentID=:gibbonDepartmentID AND gibbonPerson.status='Full' ORDER BY surname, preferredName";
-                                $result = $connection2->prepare($sql);
-                                $result->execute($data);
-                            } catch (PDOException $e) {
-                                echo "<div class='error'>".$e->getMessage().'</div>';
-                            }
-
-							if ($result->rowCount() < 1) {
-								echo "<div class='error'>";
-								echo __($guid, 'There are no records to display.');
-								echo '</div>';
-							} else {
-								echo '<i><b>Warning</b>: If you delete a member of staff, any unsaved changes to this record will be lost!</i>';
-								echo "<table cellspacing='0' style='width: 100%'>";
-								echo "<tr class='head'>";
-								echo '<th>';
-								echo __($guid, 'Name');
-								echo '</th>';
-								echo '<th>';
-								echo __($guid, 'Role');
-								echo '</th>';
-								echo '<th>';
-								echo __($guid, 'Action');
-								echo '</th>';
-								echo '</tr>';
-
-								$count = 0;
-								$rowNum = 'odd';
-								while ($row = $result->fetch()) {
-									if ($count % 2 == 0) {
-										$rowNum = 'even';
-									} else {
-										$rowNum = 'odd';
-									}
-									++$count;
-
-									//COLOR ROW BY STATUS!
-									echo "<tr class=$rowNum>";
-									echo '<td>';
-									echo formatName('', $row['preferredName'], $row['surname'], 'Staff', true, true);
-									echo '</td>';
-									echo '<td>';
-									echo $row['role'];
-									echo '</td>';
-									echo '<td>';
-									echo "<a onclick='return confirm(\"".__($guid, 'Are you sure you wish to delete this record?')."\")' href='".$_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/department_manage_edit_staff_deleteProcess.php?address='.$_GET['q'].'&gibbonDepartmentStaffID='.$row['gibbonDepartmentStaffID']."&gibbonDepartmentID=$gibbonDepartmentID'><img title='".__($guid, 'Delete')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/garbage.png'/></a>";
-									echo '</td>';
-									echo '</tr>';
-								}
-								echo '</table>';
-							}
-							?>
-						</td>
-					</tr>
-					<tr class='break'>
-						<td colspan=2>
-							<h3><?php echo __($guid, 'New Staff') ?></h3>
-						</td>
-					</tr>
-					<tr>
-    					<td>
-    						<b>Staff</b><br/>
-    						<span class="emphasis small"><?php echo __($guid, 'Use Control, Command and/or Shift to select multiple.') ?></span>
-    					</td>
-    					<td class="right">
-    						<select name="staff[]" id="staff[]" multiple class='standardWidth' style="height: 150px">
-    							<?php
-                                try {
-                                    $dataSelect = array();
-                                    $sqlSelect = "SELECT * FROM gibbonPerson JOIN gibbonStaff ON (gibbonPerson.gibbonPersonID=gibbonStaff.gibbonPersonID) WHERE status='Full' ORDER BY surname, preferredName";
-                                    $resultSelect = $connection2->prepare($sqlSelect);
-                                    $resultSelect->execute($dataSelect);
-                                } catch (PDOException $e) {
-                                }
-    							while ($rowSelect = $resultSelect->fetch()) {
-    								echo "<option value='".$rowSelect['gibbonPersonID']."'>".formatName('', htmlPrep($rowSelect['preferredName']), htmlPrep($rowSelect['surname']), 'Staff', true, true).'</option>';
-    							}
-    							?>
-    						</select>
-    					</td>
-                    </tr>
-					<tr id='roleLARow'>
-						<td>
-							<b><?php echo __($guid, 'Role') ?></b><br/>
-						</td>
-						<td class="right">
-							<select name="role" id="role" class="standardWidth">
-								<?php
-                                if ($type == 'Learning Area') {
-                                    ?>
-									<option value="Coordinator"><?php echo __($guid, 'Coordinator') ?></option>
-									<option value="Assistant Coordinator"><?php echo __($guid, 'Assistant Coordinator') ?></option>
-									<option value="Teacher (Curriculum)"><?php echo __($guid, 'Teacher (Curriculum)') ?></option>
-									<option value="Teacher"><?php echo __($guid, 'Teacher') ?></option>
-									<option value="Other"><?php echo __($guid, 'Other') ?></option>
-									<?php
-
-                                } elseif ($type == 'Administration') {
-                                    ?>
-									<option value="Director"><?php echo __($guid, 'Director') ?></option>
-									<option value="Manager"><?php echo __($guid, 'Manager') ?></option>
-									<option value="Administrator"><?php echo __($guid, 'Administrator') ?></option>
-									<option value="Other"><?php echo __($guid, 'Other') ?></option>
-									<?php
-
-                                } else {
-                                    ?>
-									<option value="Other"><?php echo __($guid, 'Other') ?></option>
-									<?php
-                                }
-           	 					?>
-							</select>
-						</td>
-					</tr>
-
-					<tr>
-						<td>
-							<span class="emphasis small">* <?php echo __($guid, 'denotes a required field'); ?></span>
-						</td>
-						<td class="right">
-							<input type="submit" value="<?php echo __($guid, 'Submit'); ?>">
-						</td>
-					</tr>
-				</table>
-			</form>
-			<?php
-
         }
     }
 }
-?>
+
