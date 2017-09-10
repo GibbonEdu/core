724dbb72f474a0c33ef7bd04d485715e71142a15
Sandra Kuipers <sandra@skuipers.com>
2017-09-10 11:55:09 +0800

    Activities: replaced form Submit button with SearchSubmit

--- modules/Activities/activities_attendance_sheet.php
+++ modules/Activities/activities_attendance_sheet.php
@@ -66,7 +66,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/activities_atte
 
     $row = $form->addRow();
         $row->addFooter();
-        $row->addSubmit(__('Go'))->prepend(sprintf('<a href="%s" class="right">%s</a> &nbsp;', $_SESSION[$guid]['absoluteURL'].'/index.php?q='.$_GET['q'], __('Clear Form')));
+        $row->addSearchSubmit($gibbon->session);
 
     echo $form->getOutput();
 
