ed9021603aaccaaeee37d716838b9b67e6a5d7a1
Ross Parker <rossdotparker@gmail.com>
2017-09-24 14:55:04 +0800

    Form OOification: installer step 1 ooified

--- installer/install.php
+++ installer/install.php
@@ -90,32 +90,14 @@ $_SESSION[$guid]['stringReplacement'] = array();
 					<div id='content'>
 						<?php
                             //Get and set database variables (not set until step 1)
-                            $databaseServer = '';
-                            if (isset($_POST['databaseServer'])) {
-                                $databaseServer = $_POST['databaseServer'];
-                            }
-                            $databaseName = '';
-                            if (isset($_POST['databaseName'])) {
-                                $databaseName = $_POST['databaseName'];
-                            }
-                            $databaseUsername = '';
-                            if (isset($_POST['databaseUsername'])) {
-                                $databaseUsername = $_POST['databaseUsername'];
-                            }
-                            $databasePassword = '';
-                            if (isset($_POST['databasePassword'])) {
-                                $databasePassword = $_POST['databasePassword'];
-                            }
-                            $demoData = '';
-                            if (isset($_POST['demoData'])) {
-                                $demoData = $_POST['demoData'];
-                            }
+                            $databaseServer = (isset($_POST['databaseServer']))? $_POST['databaseServer'] : '';
+                            $databaseName = (isset($_POST['databaseName']))? $_POST['databaseName'] : '';
+                            $databaseUsername = (isset($_POST['databaseUsername']))? $_POST['databaseUsername'] : '';
+                            $databasePassword = (isset($_POST['databasePassword']))? $_POST['databasePassword'] : '';
+                            $demoData = (isset($_POST['demoData']))? $_POST['demoData'] : '';
 
                             //Set language
-                            $code = 'en_GB';
-                            if (isset($_POST['code'])) {
-                                $code = $_POST['code'];
-                            }
+                            $code = (isset($_POST['code']))? $_POST['code'] : 'en_GB';
                             putenv('LC_ALL='.$code);
                             setlocale(LC_ALL, $code);
                             bindtextdomain('gibbon', '../i18n');
@@ -158,107 +140,66 @@ $_SESSION[$guid]['stringReplacement'] = array();
 									$phpRequirement = $gibbon->getSystemRequirement('php');
 									$extensions = $gibbon->getSystemRequirement('extensions');
 
-                                    //Set language options
-                                    ?>
-									<form method="post" action="./install.php?step=1&guid=<?php echo $guid ?>">
-										<table class='smallIntBorder fullWidth' cellspacing='0'>
-											<tr class='break'>
-												<td colspan=3>
-													<h3><?php echo __($guid, 'System Requirements') ?></h3>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<b><?php printf($versionTitle, 'PHP'); ?></b><br/>
-													<span class="emphasis small">
-														<?php printf($versionMessage, __($guid, 'Gibbon').' v'.$version, 'PHP', $phpRequirement ); ?>
-													</span>
-												</td>
-												<td style="width:60px;padding-left:10px!important;">
-													<b><?php echo $phpVersion; ?></b>
-												</td>
-												<td class="right" style="width:60px;">
-													<?php echo (version_compare($phpVersion, $phpRequirement, '>='))? $trueIcon : $falseIcon; ?>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<b><?php echo __($guid, 'MySQL PDO Support'); ?></b><br/>
-												</td>
-												<td style="padding-left:10px!important;">
-													<?php echo (@extension_loaded('pdo_mysql'))? __($guid, 'Installed') : __($guid, 'Not Installed'); ?>
-												</td>
-												<td class="right">
-													<?php echo (@extension_loaded('pdo') && extension_loaded('pdo_mysql'))? $trueIcon : $falseIcon; ?>
-												</td>
-											</tr>
-											<?php
-									            if (!empty($extensions) && is_array($extensions)) {
-									                foreach ($extensions as $extension) {
-									                    $installed = @extension_loaded($extension);
-									                    ?>
-									                    <tr>
-									                        <td>
-									                            <b><?php echo __($guid, 'Extension').' '. $extension; ?></b><br/>
-									                        </td>
-									                        <td style="padding-left:10px!important;">
-									                            <?php echo ($installed)? __($guid, 'Installed') : __($guid, 'Not Installed'); ?>
-									                        </td>
-									                        <td colspan=2 class="right">
-									                            <?php echo ($installed)? $trueIcon : $falseIcon; ?>
-									                        </td>
-									                    </tr>
-									                    <?php
-									                }
-									            }
-									        ?>
-											<tr class='break'>
-												<td colspan=3>
-													<h3><?php echo __($guid, 'Language Settings') ?></h3>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<b><?php echo __($guid, 'System Language') ?> *</b><br/>
-												</td>
-												<td colspan=2 class="right" style='width: 275px'>
-													<select name="code" id="code" class="standardWidth">
-														<option value='nl_NL'>Dutch - Nederland</option>
-														<option selected value='en_GB'>English - United Kingdom</option>
-														<option value='en_US'>English - United States</option>
-														<option value='es_ES'>Español</option>
-														<option value='fr_FR'>Français - France</option>
-														<option value='it_IT'>Italiano - Italia</option>
-														<option value='ro_RO'>Română</option>
-                                                        <option value="sq_AL">Shqip - Shqipëri</option>
-                                                        <option value="vi_VN">Tiếng Việt - Việt Nam</option>
-														<option value='ar_SA'>العربية - المملكة العربية السعودية</option>
-                                                        <option value="th_TH">ภาษาไทย - ราชอาณาจักรไทย </option>
-                                                        <option value='zh_HK'>體字 - 香港</option>
-													</select>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<span class="emphasis small">* <?php echo __($guid, 'denotes a required field'); ?></span>
-												</td>
-												<td colspan=2 class="right">
-													<input type="submit" value="<?php echo __($guid, 'Submit'); ?>">
-												</td>
-											</tr>
-										</table>
-									</form>
-									<?php
+                                    $form = Form::create('action', "./install.php?step=1&guid=$guid");
 
+                                    $form->setClass('smallIntBorder fullWidth');
+
+                                    $form->addRow()->addHeading(__('System Requirements'));
+
+                                    $row = $form->addRow();
+                                        $row->addLabel('phpVersion', sprintf($versionTitle, 'PHP'))->description(sprintf($versionMessage, __($guid, 'Gibbon').' v'.$version, 'PHP', $phpRequirement));
+                                        $row->addTextField('phpVersion')->setValue($phpVersion)->readonly();
+                                        $row->addContent((version_compare($phpVersion, $phpRequirement, '>='))? $trueIcon : $falseIcon);
+
+                                    $row = $form->addRow();
+                                        $row->addLabel('pdoSupport', __($guid, 'MySQL PDO Support'));
+                                        $row->addTextField('pdoSupport')->setValue((@extension_loaded('pdo_mysql'))? __($guid, 'Installed') : __($guid, 'Not Installed'))->readonly();
+                                        $row->addContent((@extension_loaded('pdo') && extension_loaded('pdo_mysql'))? $trueIcon : $falseIcon);
+
+                                    if (!empty($extensions) && is_array($extensions)) {
+                                        foreach ($extensions as $extension) {
+                                            $installed = @extension_loaded($extension);
+                                            $row = $form->addRow();
+                                                $row->addLabel('extension', __($guid, 'Extension').' '. $extension);
+                                                $row->addTextField('extension')->setValue(($installed)? __($guid, 'Installed') : __($guid, 'Not Installed'))->readonly();
+                                                $row->addContent(($installed)? $trueIcon : $falseIcon);
+                                        }
+                                    }
+
+                                    $form->addRow()->addHeading(__('Language Settings'));
+
+                                    $languages = array(
+                                        'nl_NL' => 'Dutch - Nederland',
+                                        'en_GB' => 'English - United Kingdom',
+                                        'en_US' => 'English - United States',
+                                        'es_ES' => 'Español',
+                                        'fr_FR' => 'Français - France',
+                                        'it_IT' => 'Italiano - Italia',
+                                        'ro_RO' => 'Română',
+                                        'sq_AL' => 'Shqip - Shqipëri',
+                                        'vi_VN' => 'Tiếng Việt - Việt Nam',
+                                        'ar_SA' => 'العربية - المملكة العربية السعودية',
+                                        'th_TH' => 'ภาษาไทย - ราชอาณาจักรไทย',
+                                        'zh_HK' => '體字 - 香港');
+
+                                    $row = $form->addRow();
+                                        $row->addLabel('code', __('System Language'));
+                                        $row->addSelect('code')->fromArray($languages)->selected($code)->isRequired();
+
+                                    $row = $form->addRow();
+                                        $row->addFooter();
+                                        $row->addSubmit();
+
+                                    echo $form->getOutput();
                                 }
                             }
                             if ($step == 1) { //Set database options
-                                //START FORM
                                 $form = Form::create('action', "./install.php?step=2&guid=$guid");
 
                                 $form->setClass('smallIntBorder fullWidth');
 
                                 $form->addHiddenValue('code', $code);
+
                                 $form->addRow()->addHeading(__('Database Settings'));
 
                                 $row = $form->addRow();
