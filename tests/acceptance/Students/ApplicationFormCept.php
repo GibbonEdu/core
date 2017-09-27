1a3775ef5f39a04d624329a93d475bb4f925a78c
Sandra Kuipers <sandra@skuipers.com>
2017-09-27 11:18:17 +0800

    Tests: added file upload to Student Application Form test

--- tests/acceptance/Students/ApplicationFormCept.php
+++ tests/acceptance/Students/ApplicationFormCept.php
@@ -17,6 +17,7 @@ $newApplicationSettings = array_replace($originalApplicationSettings, array(
     'languageOptionsBlurb'        => 'Language Blurb Test',
     'languageOptionsLanguageList' => 'Esperanto,Latin,Klingon',
     'applicationFormRefereeLink'  => 'http://referee.test',
+    'requiredDocuments'           => 'FileUpload0,FileUpload1',
     'agreement'                   => 'Agreement Test',
 ));
 
@@ -176,9 +177,9 @@ $I->selectFromDropdown('gibbonYearGroupIDEntry', 2);
 // Check the agreement
 $I->checkOption('agreement');
 
-// Separate test?
-//$I->attachFile('file0', 'attachment.jpg');
-//$I->attachFile('file1', 'attachment.txt');
+// Attach required documents
+$I->attachFile('file0', 'attachment.jpg');
+$I->attachFile('file1', 'attachment.txt');
 
 $I->submitForm('#content form', $formValues, 'Submit');
 
@@ -204,6 +205,14 @@ $I->seeBreadcrumb('Delete Form');
 
 $I->click('Yes');
 $I->see('Your request was completed successfully.', '.success');
+
+// Delete Files ------------------------------------------------
+$filepath = $I->grabFromDatabase('gibbonApplicationFormFile', 'path', ['gibbonApplicationFormID' => $gibbonApplicationFormID, 'name' => 'FileUpload0']);
+$I->deleteFile('../'.$filepath);
+$filepath = $I->grabFromDatabase('gibbonApplicationFormFile', 'path', ['gibbonApplicationFormID' => $gibbonApplicationFormID, 'name' => 'FileUpload1']);
+$I->deleteFile('../'.$filepath);
+
+$I->deleteFromDatabase('gibbonApplicationFormFile', ['gibbonApplicationFormID' => $gibbonApplicationFormID]);
 
 // Restore Original Settings -----------------------------------
 
