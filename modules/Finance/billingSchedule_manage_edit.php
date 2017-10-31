bc33c92944d621e377fae5407eb2dd89b1c77b70
Ray Clark <raynichc@users.noreply.github.com>
2017-10-31 16:19:45 +0800

    Gibbon Hackathon Form OOification (#377)
    * Form Class: Added "fullWidth" to blockTemplate class in CustomBlocks form element.
    
    * Form Class: Added null check for row variable in getLabel()
    
    * Form Class: Increased readability of Select creation.
    
    * Form Class: Changed null check to empty()
    
    * Form Class: setRow now returns the instance of the editor
    
    * Form Class: Fixed elements with empty getName values overriding each other.
    
    * OOified Forms
    
    * OOified Form

--- modules/Finance/billingSchedule_manage_edit.php
+++ modules/Finance/billingSchedule_manage_edit.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Forms\Form;
+
 @session_start();
 
 if (isActionAccessible($guid, $connection2, '/modules/Finance/billingSchedule_manage_edit.php') == false) {
@@ -58,162 +60,62 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/billingSchedule_ma
             echo '</div>';
         } else {
             //Let's go!
-            $row = $result->fetch();
+            $resultRow = $result->fetch();
 
             if ($search != '') {
                 echo "<div class='linkTop'>";
                 echo "<a href='".$_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Finance/billingSchedule_manage.php&gibbonSchoolYearID=$gibbonSchoolYearID&search=$search'>".__($guid, 'Back to Search Results').'</a>';
                 echo '</div>';
             }
-            ?>
-			
-			<form method="post" action="<?php echo $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/billingSchedule_manage_editProcess.php?gibbonSchoolYearID=$gibbonSchoolYearID&search=$search" ?>">
-				<table class='smallIntBorder fullWidth' cellspacing='0'>	
-					<tr>
-						<td style='width: 275px'> 
-							<b><?php echo __($guid, 'School Year') ?> *</b><br/>
-							<span class="emphasis small"><?php echo __($guid, 'This value cannot be changed.') ?></span>
-						</td>
-						<td class="right">
-							<?php
-                            $yearName = '';
-							try {
-								$dataYear = array('gibbonSchoolYearID' => $gibbonSchoolYearID);
-								$sqlYear = 'SELECT * FROM gibbonSchoolYear WHERE gibbonSchoolYearID=:gibbonSchoolYearID';
-								$resultYear = $connection2->prepare($sqlYear);
-								$resultYear->execute($dataYear);
-							} catch (PDOException $e) {
-								echo "<div class='error'>".$e->getMessage().'</div>';
-							}
-							if ($resultYear->rowCount() == 1) {
-								$rowYear = $resultYear->fetch();
-								$yearName = $rowYear['name'];
-							}
-							?>
-							<input readonly name="yearName" id="yearName" maxlength=20 value="<?php echo $yearName ?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var yearName=new LiveValidation('yearName');
-								yearname2.add(Validate.Presence);
-							</script>
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Name') ?> *</b><br/>
-						</td>
-						<td class="right">
-							<input name="name" id="name" maxlength=100 value="<?php echo htmlPrep($row['name'])?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var name2=new LiveValidation('name');
-								name2.add(Validate.Presence);
-							</script>
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Active') ?> *</b><br/>
-							<span class="emphasis small"></span>
-						</td>
-						<td class="right">
-							<select name="active" id="active" class="standardWidth">
-								<option <?php if ($row['active'] == 'Y') { echo 'selected'; } ?> value="Y"><?php echo __($guid, 'Yes') ?></option>
-								<option <?php if ($row['active'] == 'N') { echo 'selected'; } ?> value="N"><?php echo __($guid, 'No') ?></option>
-							</select>
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Description') ?></b><br/>
-						</td>
-						<td class="right">
-							<textarea name='description' id='description' rows=5 style='width: 300px'><?php echo htmlPrep($row['description'])?></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Invoice Issue Date') ?> *</b><br/>
-							<span class="emphasis small"><?php echo __($guid, 'Intended issue date.').'<br/>'.__($guid, 'Format:') ?> <?php if ($_SESSION[$guid]['i18n']['dateFormat'] == '') {
-								echo 'dd/mm/yyyy';
-							} else {
-								echo $_SESSION[$guid]['i18n']['dateFormat'];
-							}
-							?><br/></span>
-						</td>
-						<td class="right">
-							<input name="invoiceIssueDate" id="invoiceIssueDate" maxlength=10 value="<?php echo dateConvertBack($guid, $row['invoiceIssueDate']) ?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var invoiceIssueDate=new LiveValidation('invoiceIssueDate');
-								invoiceIssueDate.add( Validate.Format, {pattern: <?php if ($_SESSION[$guid]['i18n']['dateFormatRegEx'] == '') {
-								echo "/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i";
-								} else {
-									echo $_SESSION[$guid]['i18n']['dateFormatRegEx'];
-								}
-											?>, failureMessage: "Use <?php if ($_SESSION[$guid]['i18n']['dateFormat'] == '') {
-									echo 'dd/mm/yyyy';
-								} else {
-									echo $_SESSION[$guid]['i18n']['dateFormat'];
-								}
-								?>." } ); 
-								invoiceIssueDate.add(Validate.Presence);
-							</script>
-							 <script type="text/javascript">
-								$(function() {
-									$( "#invoiceIssueDate" ).datepicker();
-								});
-							</script>
-						</td>
-					</tr>
-				
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Invoice Due Date') ?> *</b><br/>
-							<span class="emphasis small"><?php echo __($guid, 'Final Payment Date.').'<br/>'.__($guid, 'Format:') ?> <?php if ($_SESSION[$guid]['i18n']['dateFormat'] == '') {
-								echo 'dd/mm/yyyy';
-							} else {
-								echo $_SESSION[$guid]['i18n']['dateFormat'];
-							}
-            				?><br/></span>
-						</td>
-						<td class="right">
-							<input name="invoiceDueDate" id="invoiceDueDate" maxlength=10 value="<?php echo dateConvertBack($guid, $row['invoiceDueDate']) ?>" type="text" class="standardWidth">
-							<script type="text/javascript">
-								var invoiceDueDate=new LiveValidation('invoiceDueDate');
-								invoiceDueDate.add( Validate.Format, {pattern: <?php if ($_SESSION[$guid]['i18n']['dateFormatRegEx'] == '') {
-								echo "/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/i";
-								} else {
-									echo $_SESSION[$guid]['i18n']['dateFormatRegEx'];
-								}
-											?>, failureMessage: "Use <?php if ($_SESSION[$guid]['i18n']['dateFormat'] == '') {
-									echo 'dd/mm/yyyy';
-								} else {
-									echo $_SESSION[$guid]['i18n']['dateFormat'];
-								}
-								?>." } ); 
-								invoiceDueDate.add(Validate.Presence);
-							</script>
-							 <script type="text/javascript">
-								$(function() {
-									$( "#invoiceDueDate" ).datepicker();
-								});
-							</script>
-						</td>
-					</tr>
-				
-					<tr>
-						<td>
-							<span class="emphasis small">* <?php echo __($guid, 'denotes a required field'); ?></span>
-						</td>
-						<td class="right">
-							<input name="gibbonFinanceBillingScheduleID" id="gibbonFinanceBillingScheduleID" value="<?php echo $gibbonFinanceBillingScheduleID ?>" type="hidden">
-							<input type="hidden" name="address" value="<?php echo $_SESSION[$guid]['address'] ?>">
-							<input type="submit" value="<?php echo __($guid, 'Submit'); ?>">
-						</td>
-					</tr>
-				</table>
-			</form>
-			<?php
 
+            $yearName = '';
+            try {
+                $dataYear = array('gibbonSchoolYearID' => $gibbonSchoolYearID);
+                $sqlYear = 'SELECT * FROM gibbonSchoolYear WHERE gibbonSchoolYearID=:gibbonSchoolYearID';
+                $resultYear = $connection2->prepare($sqlYear);
+                $resultYear->execute($dataYear);
+            } catch (PDOException $e) {
+                echo "<div class='error'>".$e->getMessage().'</div>';
+            }
+            if ($resultYear->rowCount() == 1) {
+                $rowYear = $resultYear->fetch();
+                $yearName = $rowYear['name'];
+            }
+
+            $form = Form::create("edit", $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/billingSchedule_manage_editProcess.php?gibbonSchoolYearID=$gibbonSchoolYearID&search=$search");
+
+            $form->addHiddenValue("gibbonFinanceBillingScheduleID", $gibbonFinanceBillingScheduleID);
+            $form->addHiddenValue("address", $_SESSION[$guid]['address']);
+
+            $row = $form->addRow();
+                $row->addLabel("yearName", __("School Year"))->description(__("This value cannot be changed."));
+                $row->addTextField("yearName")->setValue($yearName)->readonly(true)->isRequired();
+
+            $row = $form->addRow();
+                $row->addLabel("name", __("Name"));
+                $row->addTextField("name")->setValue(htmlprep($resultRow['name']))->maxLength(100)->isRequired();
+
+            $row = $form->addRow();
+                $row->addLabel("active", __("Active"));
+                $row->addYesNo("active")->selected($resultRow['active'])->isRequired();
+
+            $row = $form->addRow();
+                $row->addLabel("description", __("Description"));
+                $row->addTextArea("description")->setValue(htmlPrep($resultRow['description']))->setRows(5);
+
+            $row = $form->addRow();
+                $row->addLabel("invoiceIssueDate", __('Invoice Issue Date'))->description(__($guid, 'Intended issue date.') . '<br/>' . (empty($_SESSION[$guid]['i18n']['dateFormat']) ? "dd/mm/yy" : $_SESSION[$guid]['i18n']['dateFormat']));
+                $row->addDate('invoiceIssueDate')->setValue(dateConvertBack($guid, $resultRow['invoiceIssueDate']))->isRequired();
+
+            $row = $form->addRow();
+                $row->addLabel('invoiceDueDate', __('Invoice Due Date'))->description(__($guid, 'Final Payment Date.') . '<br/>' . (empty($_SESSION[$guid]['i18n']['dateFormat']) ? "dd/mm/yy" : $_SESSION[$guid]['i18n']['dateFormat']));;
+                $row->addDate('invoiceDueDate')->setValue(dateConvertBack($guid, $resultRow['invoiceDueDate']))->isRequired();
+
+            $row = $form->addRow();
+                $row->addFooter();
+                $row->addSubmit();
+
+            print $form->getOutput();
         }
     }
 }
-?>
