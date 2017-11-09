4eff08dc2263b9e19d8e5f1493d97e51509e3ff3
Ross Parker <rossdotparker@gmail.com>
2017-11-09 21:48:02 +0800

    Timetable Admin: Manage Columns ooification 100% complete

--- modules/Timetable Admin/ttColumn_delete.php
+++ modules/Timetable Admin/ttColumn_delete.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Forms\Form;
+
 @session_start();
 
 if (isActionAccessible($guid, $connection2, '/modules/Timetable Admin/ttColumn_delete.php') == false) {
@@ -56,31 +58,18 @@ if (isActionAccessible($guid, $connection2, '/modules/Timetable Admin/ttColumn_d
             echo '</div>';
         } else {
             //Let's go!
-            $row = $result->fetch(); ?>
-			<form method="post" action="<?php echo $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/ttColumn_deleteProcess.php?gibbonTTColumnID=$gibbonTTColumnID" ?>">
-				<table class='smallIntBorder fullWidth' cellspacing='0'>	
-					<tr>
-						<td style='width: 275px'> 
-							<b><?php echo __($guid, 'Are you sure you want to delete this record?'); ?></b><br/>
-							<span style="font-size: 90%; color: #cc0000"><i><?php echo __($guid, 'This operation cannot be undone, and may lead to loss of vital data in your system. PROCEED WITH CAUTION!'); ?></span>
-						</td>
-						<td class="right">
-							
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<input type="hidden" name="address" value="<?php echo $_SESSION[$guid]['address'] ?>">
-							<input type="submit" value="<?php echo __($guid, 'Yes'); ?>">
-						</td>
-						<td class="right">
-							
-						</td>
-					</tr>
-				</table>
-			</form>
-			<?php
+            $form = Form::create('accessRecord', $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/ttColumn_deleteProcess.php?gibbonTTColumnID=$gibbonTTColumnID");
 
+            $form->addHiddenValue('address', $_SESSION[$guid]['address']);
+
+            $row = $form->addRow();
+                $column = $row->addColumn();
+                $column->addContent(__('Are you sure you want to delete this record?'))->wrap('<strong>','</strong>');
+                $column->addContent(__('This operation cannot be undone, and may lead to loss of vital data in your system. PROCEED WITH CAUTION!'))->wrap('<span style="color: #cc0000"><i>', '</i></span>');
+
+            $form->addRow()->addSubmit();
+
+            echo $form->getOutput();
         }
     }
 }
