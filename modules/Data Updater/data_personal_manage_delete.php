e0c2ecaff42cd32a3de724767a582718e588033f
Sandra Kuipers <sandra@skuipers.com>
2017-11-14 16:23:46 +0800

    Data Updater: Personal Data Updates OOification

--- modules/Data Updater/data_personal_manage_delete.php
+++ modules/Data Updater/data_personal_manage_delete.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Forms\PrefabFormFactory;
+
 @session_start();
 
 //Module includes
@@ -59,32 +61,9 @@ if (isActionAccessible($guid, $connection2, '/modules/Data Updater/data_personal
             echo '</div>';
         } else {
             //Let's go!
-            $row = $result->fetch(); ?>
-			<form method="post" action="<?php echo $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/data_personal_manage_deleteProcess.php?gibbonPersonUpdateID=$gibbonPersonUpdateID" ?>">
-				<table class='smallIntBorder fullWidth' cellspacing='0'>	
-					<tr>
-						<td> 
-							<b><?php echo __($guid, 'Are you sure you want to delete this record?'); ?></b><br/>
-							<span style="font-size: 90%; color: #cc0000"><i><?php echo __($guid, 'This operation cannot be undone, and may lead to loss of vital data in your system. PROCEED WITH CAUTION!'); ?></span>
-						</td>
-						<td class="right">
-							
-						</td>
-					</tr>
-					<tr>
-						<td> 
-							<input name="gibbonPersonUpdateID" id="gibbonPersonUpdateID" value="<?php echo $gibbonPersonUpdateID ?>" type="hidden">
-							<input type="hidden" name="address" value="<?php echo $_SESSION[$guid]['address'] ?>">
-							<input type="submit" value="<?php echo __($guid, 'Yes'); ?>">
-						</td>
-						<td class="right">
-							
-						</td>
-					</tr>
-				</table>
-			</form>
-			<?php
-
+            
+            $form = PrefabFormFactory::createDeleteForm($_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/data_personal_manage_deleteProcess.php?gibbonPersonUpdateID=".$gibbonPersonUpdateID);
+            echo $form->getOutput();
         }
     }
 }
