1c0827e32efcb5d1072dff66a6a5d62fd29231ca
Sandra Kuipers <sandra@skuipers.com>
2017-11-16 10:02:24 +0800

    Form Class: add generate password button to Password class

--- src/Forms/Input/Password.php
+++ src/Forms/Input/Password.php
@@ -28,17 +28,6 @@ namespace Gibbon\Forms\Input;
 class Password extends TextField
 {
     /**
-     * Adds the validation to indicate this password field is a confirmation for another field.
-     * @return self
-     */
-    public function addConfirmation($fieldName)
-    {
-        $this->addValidation('Validate.Confirmation', "match: '$fieldName'");
-
-        return $this;
-    }
-
-    /**
      * Attach the validation requirements for the system-wide password policy.
      * @param sqlConnection $pdo
      * @return self
@@ -64,6 +53,35 @@ class Password extends TextField
         if (!empty($minLength) && is_numeric($minLength)) {
             $this->addValidation('Validate.Length', 'minimum: '.$minLength.', failureMessage: "'.__('Does not meet password policy.').'"');
         }
+
+        return $this;
+    }
+
+    /**
+     * Adds a button to the field that uses JS to generate and insert a password into the form.
+     * @param Form $form
+     * @return self
+     */
+    public function addGeneratePasswordButton($form, $sourceField = 'passwordNew', $confirmField = 'passwordConfirm')
+    {
+        $button = $form->getFactory()->createButton(__('Generate'));
+        $button->addClass('generatePassword alignRight')
+            ->addData('source', $sourceField)
+            ->addData('confirm', $confirmField)
+            ->addData('alert', __('Copy this password if required:'));
+
+        $this->append($button->getOutput());
+
+        return $this;
+    }
+
+    /**
+     * Adds the validation to indicate this password field is a confirmation for another field.
+     * @return self
+     */
+    public function addConfirmation($fieldName)
+    {
+        $this->addValidation('Validate.Confirmation', "match: '$fieldName'");
 
         return $this;
     }
