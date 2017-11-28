cee1373b9aacda039055c21d55805d76c9d5b7b0
Sandra Kuipers <sandra@skuipers.com>
2017-11-28 12:50:20 +0800

    Activities: add optional Activity Type description to view activity

--- modules/Activities/activities_view_full.php
+++ modules/Activities/activities_view_full.php
@@ -62,10 +62,10 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/activities_view
                 try {
                     if ($dateType != 'Date') {
                         $data = array('gibbonSchoolYearID' => $_SESSION[$guid]['gibbonSchoolYearID'], 'gibbonActivityID' => $gibbonActivityID);
-                        $sql = "SELECT * FROM gibbonActivity WHERE gibbonSchoolYearID=:gibbonSchoolYearID AND active='Y' AND NOT gibbonSchoolYearTermIDList='' AND gibbonActivityID=:gibbonActivityID";
+                        $sql = "SELECT gibbonActivity.*, gibbonActivityType.description as activityTypeDescription FROM gibbonActivity LEFT JOIN gibbonActivityType ON (gibbonActivity.type=gibbonActivityType.name) WHERE gibbonSchoolYearID=:gibbonSchoolYearID AND active='Y' AND NOT gibbonSchoolYearTermIDList='' AND gibbonActivityID=:gibbonActivityID";
                     } else {
                         $data = array('gibbonSchoolYearID' => $_SESSION[$guid]['gibbonSchoolYearID'], 'gibbonActivityID' => $gibbonActivityID, 'listingStart' => $today, 'listingEnd' => $today);
-                        $sql = "SELECT * FROM gibbonActivity WHERE gibbonSchoolYearID=:gibbonSchoolYearID AND active='Y' AND listingStart<=:listingStart AND listingEnd>=:listingEnd AND gibbonActivityID=:gibbonActivityID";
+                        $sql = "SELECT gibbonActivity.*, gibbonActivityType.description as activityTypeDescription FROM gibbonActivity LEFT JOIN gibbonActivityType ON (gibbonActivity.type=gibbonActivityType.name) WHERE gibbonSchoolYearID=:gibbonSchoolYearID AND active='Y' AND listingStart<=:listingStart AND listingEnd>=:listingEnd AND gibbonActivityID=:gibbonActivityID";
                     }
                     $result = $connection2->prepare($sql);
                     $result->execute($data);
@@ -183,6 +183,14 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/activities_view
                     echo '</i>';
                     echo '</td>';
                     echo '</tr>';
+                    if (!empty($row['activityTypeDescription'])) {
+                        echo '<tr>';
+                        echo "<td style='text-align: justify; padding-top: 15px; width: 33%; vertical-align: top' colspan=3>";
+                        echo '<h2>'.$row['type'].'</h2>';
+                        echo $row['activityTypeDescription'];
+                        echo '</td>';
+                        echo '</tr>';
+                    }
                     if ($row['description'] != '') {
                         echo '<tr>';
                         echo "<td style='text-align: justify; padding-top: 15px; width: 33%; vertical-align: top' colspan=3>";
