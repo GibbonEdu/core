7ad0c1121e24c92dc00da417faead795ef0d70f5
Sandra Kuipers <sandra@skuipers.com>
2017-11-29 15:19:52 +0800

    Form Class: add comments to WebLink methods

--- src/Forms/Layout/WebLink.php
+++ src/Forms/Layout/WebLink.php
@@ -14,36 +14,68 @@ class WebLink extends Element
         parent::__construct($content);
     }
 
+    /**
+     * Sets the link href attribute.
+     * @param string $url
+     * @return self
+     */
     public function setURL($url)
     {
         return $this->setAttribute('href', $url);
     }
 
+    /**
+     * Gets the link href attribute.
+     * @return string
+     */
     public function getURL()
     {
         return $this->getAttribute('href');
     }
 
+    /**
+     * Sets the link target attribute.
+     * @param string $target
+     * @return self
+     */
     public function setTarget($target)
     {
         return $this->setAttribute('target', $target);
     }
 
+    /**
+     * Gets the link target attribute.
+     * @return string
+     */
     public function getTarget()
     {
         return $this->getAttribute('target');
     }
 
+    /**
+     * Sets the link rel attribute.
+     * @param string $rel
+     * @return self
+     */
     public function setRel($rel)
     {
         return $this->setAttribute('rel', $rel);
     }
 
+    /**
+     * Gets the link rel attribute.
+     * @return string
+     */
     public function getRel()
     {
         return $this->getAttribute('rel');
     }
 
+    /**
+     * Add a confirmation message to display on click.
+     * @param string $message
+     * @return self
+     */
     public function addConfirmation($message)
     {
         $this->setAttribute('onclick', "return confirm(\"".__($message)."\")");
@@ -51,6 +83,12 @@ class WebLink extends Element
         return $this;
     }
 
+    /**
+     * Adds a URL parameter to be appended to the link URL.
+     * @param string $name
+     * @param string $value
+     * @return self
+     */
     public function addParam($name, $value)
     {
         $this->params[$name] = $value;
@@ -58,6 +96,11 @@ class WebLink extends Element
         return $this;
     }
 
+    /**
+     * Adds an array of URL parameters to be appended to the link URL.
+     * @param array $values
+     * @return self
+     */
     public function addParams($values)
     {
         if (is_array($values)) {
@@ -67,6 +110,11 @@ class WebLink extends Element
         return $this;
     }
 
+    /**
+     * Adds an embedded element to output inside the link tag.
+     * @param OutputtableInterface $element
+     * @return self
+     */
     public function addEmbeddedElement($element)
     {
         if ($element instanceof OutputableInterface) {
@@ -76,6 +124,11 @@ class WebLink extends Element
         return $this;
     }
     
+    /**
+     * Sets an array of embedded elements to output inside the link tag.
+     * @param OutputtableInterface $element
+     * @return self
+     */
     public function setEmbeddedElements($elements)
     {
         $elements = is_array($elements)? $elements : array($elements);
@@ -87,6 +140,10 @@ class WebLink extends Element
         return $this;
     }
 
+    /**
+     * Gets the output of all embedded elements as a string.
+     * @return string
+     */
     public function getEmbeddedElements()
     {
         $output = '';
@@ -98,12 +155,15 @@ class WebLink extends Element
         return $output;
     }
 
+    /**
+     * Gets the HTML output for this form element.
+     * @return  string
+     */
     public function getElement()
     {
         if (!empty($this->params)) {
-            $url = $this->getURL();
-            $url .= (stripos($url, '?') !== false)? '&' : '?';
-            $this->setURL($url.http_build_query($this->params));
+            $separator = (stripos($this->getURL(), '?') === false)? '?' : '&';
+            $this->setURL($this->getURL().$separator.http_build_query($this->params));
         }
 
         return '<a ' . $this->getAttributeString() . '>' . $this->content . $this->getEmbeddedElements() . '</a>';
