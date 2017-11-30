9ed3c9b67b096d716ae8e6e3fdd7d20962638db1
Sandra Kuipers <sandra@skuipers.com>
2017-11-30 15:11:16 +0800

    Form Class: fix getLabelContext method in Time class

--- src/Forms/Input/Time.php
+++ src/Forms/Input/Time.php
@@ -57,27 +57,10 @@ class Time extends TextField
      * Adds time format to the label description
      * @return string|bool
      */
-    public function getLabelContext()
+    public function getLabelContext($label)
     {
-        if ($label = $this->getLabel()) {
-            if (stristr($label->getDescription(), 'Format') === false) {
-                return __('Format: hh:mm (24hr)');
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Adds time format to the label description
-     * @return string|bool
-     */
-    public function getLabelContext()
-    {
-        if ($label = $this->getLabel()) {
-            if (stristr($label->getDescription(), 'Format') === false) {
-                return __('Format: hh:mm (24hr)');
-            }
+        if (stristr($label->getDescription(), 'Format') === false) {
+            return __('Format: hh:mm (24hr)');
         }
 
         return false;
