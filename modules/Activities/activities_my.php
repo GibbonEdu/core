5408d951a9371e174959f1f5c04b19e7c07c8b20
Ross Parker <rossdotparker@gmail.com>
2018-01-03 21:49:49 +0800

    Revert "Revert "OOification of "My Activities" page (#441)""
    This reverts commit d6c6a34b2183c4883d5f15d93f00936735348672.

--- modules/Activities/activities_my.php
+++ modules/Activities/activities_my.php
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Forms\Form;
+use Gibbon\Forms\Prefab\BulkActionForm;
+use Gibbon\Forms\Layout\Element;
+
 @session_start();
 
 //Module includes
@@ -35,9 +39,9 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/activities_my.p
     try {
         $data = array('gibbonPersonID' => $_SESSION[$guid]['gibbonPersonID'], 'gibbonSchoolYearID' => $_SESSION[$guid]['gibbonSchoolYearID'], 'gibbonPersonID2' => $_SESSION[$guid]['gibbonPersonID'], 'gibbonSchoolYearID2' => $_SESSION[$guid]['gibbonSchoolYearID']);
         $sql = "(SELECT gibbonActivity.*, gibbonActivityStudent.status, NULL AS role FROM gibbonActivity JOIN gibbonActivityStudent ON (gibbonActivity.gibbonActivityID=gibbonActivityStudent.gibbonActivityID) WHERE gibbonActivityStudent.gibbonPersonID=:gibbonPersonID AND gibbonSchoolYearID=:gibbonSchoolYearID AND active='Y') UNION (SELECT gibbonActivity.*, NULL as status, gibbonActivityStaff.role FROM gibbonActivity JOIN gibbonActivityStaff ON (gibbonActivity.gibbonActivityID=gibbonActivityStaff.gibbonActivityID) WHERE gibbonActivityStaff.gibbonPersonID=:gibbonPersonID2 AND gibbonSchoolYearID=:gibbonSchoolYearID2 AND active='Y') ORDER BY name";
-        $result = $connection2->prepare($sql);
+         $result = $connection2->prepare($sql);
         $result->execute($data);
-    } catch (PDOException $e) {
+   } catch (PDOException $e) {
         echo "<div class='error'>".$e->getMessage().'</div>';
     }
 
@@ -47,76 +51,36 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/activities_my.p
         echo '</div>';
     } else {
         $highestAction = getHighestGroupedAction($guid, '/modules/Activities/activities_attendance.php', $connection2);
-        echo "<table cellspacing='0' style='width: 100%'>";
-        echo "<tr class='head'>";
-        echo '<th>';
-        echo __($guid, 'Activity');
-        echo '</th>';
         $options = getSettingByScope($connection2, 'Activities', 'activityTypes');
-        if ($options != '') {
-            echo '<th>';
-            echo __($guid, 'Type');
-            echo '</th>';
+        $form = Form::create('bulkAction',$_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/activities_my.php');
+        $table = $form->addRow()->addTable();
+
+        //Initialise headers
+        $header = $table->addHeaderRow();
+        $header->addContent(__('Activity'));
+        $header->addContent(__('Type'));
+        $header->addContent(__('Role'));
+        $header->addContent(__('Status'));
+        $header->addContent(__('Actions'));
+        
+        $class = true;
+        foreach($result->fetchAll() as $act)
+        {
+            $actRow = $table->addRow()->addClass($class == true? 'even' : 'odd');
+            $actRow->addContent($act['name']);
+            $actRow->addContent($act['type']);
+            $actRow->addContent($act['role']);
+            $actRow->addContent($act['status'] ? $act['status'] : "N/A");
+            $col = $actRow->addColumn()->addClass('inline');
+            $col->addWebLink()->setURL($_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_manage_enrolment.php&gibbonActivityID=' . $act['gibbonActivityID'] . '&search=&gibbonSchoolYearTermID=')->setEmbeddedElements(new Element('<img src="./themes/Default/img/config.png"/>'));
+            $col->addWebLink()->setURL($_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_my_full.php&gibbonActivityID=' . $act['gibbonActivityID'] . 'width=1000&height=550')->setEmbeddedElements(new Element('<img src="./themes/Default/img/plus.png"/>'));
+            $col->addWebLink()->setURL($_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_attendance.php&gibbonActivityID=' . $act['gibbonActivityID'])->setEmbeddedElements(new Element('<img src="./themes/Default/img/attendance.png"/>')); 
+            switch($class)
+            {
+                case true : $class = false; break;
+                case false : $class = true; break;
+            }
         }
-        echo '<th>';
-        echo __($guid, 'Role');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Status');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Actions');
-        echo '</th>';
-        echo '</tr>';
-
-        $count = 0;
-        $rowNum = 'odd';
-        while ($row = $result->fetch()) {
-            if ($count % 2 == 0) {
-                $rowNum = 'even';
-            } else {
-                $rowNum = 'odd';
-            }
-
-            ++$count;
-
-            //COLOR ROW BY STATUS!
-            echo "<tr class=$rowNum>";
-            echo '<td>';
-            echo $row['name'];
-            echo '</td>';
-            if ($options != '') {
-                echo '<td>';
-                echo trim($row['type']);
-                echo '</td>';
-            }
-            echo '<td>';
-            if ($row['role'] == '') {
-                echo 'Student';
-            } else {
-                echo __($guid, $row['role']);
-            }
-            echo '</td>';
-            echo '<td>';
-            if ($row['status'] != '') {
-                echo $row['status'];
-            } else {
-                echo '<i>'.__($guid, 'NA').'</i>';
-            }
-            echo '</td>';
-            echo '<td>';
-            if ($row['role'] == 'Organiser' && isActionAccessible($guid, $connection2, '/modules/Activities/activities_manage_enrolment.php')) {
-                echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_manage_enrolment.php&gibbonActivityID='.$row['gibbonActivityID']."&search=&gibbonSchoolYearTermID='><img title='".__($guid, 'Enrolment')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/config.png'/></a> ";
-            }
-
-            echo "<a class='thickbox' href='".$_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_my_full.php&gibbonActivityID='.$row['gibbonActivityID']."&width=1000&height=550'><img title='".__($guid, 'View Details')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/plus.png'/></a> ";
-
-            if ($highestAction == "Enter Activity Attendance" || ($highestAction == "Enter Activity Attendance_leader" && ($row['role'] == 'Organiser' || $row['role'] == 'Assistant' || $row['role'] == 'Coach'))) {
-                echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/activities_attendance.php&gibbonActivityID='.$row['gibbonActivityID']."'><img title='".__($guid, 'Attendance')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/attendance.png'/></a> ";
-            }
-            echo '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
+        echo $form->getOutput();
     }
 }
