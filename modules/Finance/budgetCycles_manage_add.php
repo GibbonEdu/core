7195ceeb2a8f28b322672b2521b204d9e82d7c55
Ross Parker <rossdotparker@gmail.com>
2018-01-16 17:08:15 +0800

    Finance: fixed missed OOification and improved existing OOification in Manage Budget Cycles

--- modules/Finance/budgetCycles_manage_add.php
+++ modules/Finance/budgetCycles_manage_add.php
@@ -18,6 +18,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
+use Gibbon\Forms\DatabaseFormFactory;
 
 @session_start();
 
@@ -41,68 +42,69 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/budgetCycles_manag
     }
 
     $form = Form::create('budgetCycle', $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module'].'/budgetCycles_manage_addProcess.php');
+    $form->setFactory(DatabaseFormFactory::create($pdo));
 
-    	$form->addHiddenValue("address", $_SESSION[$guid]['address']);
+	$form->addHiddenValue("address", $_SESSION[$guid]['address']);
 
-    	$row = $form->addRow();
-    		$row->addHeading(__("Basic Information"));
+	$row = $form->addRow();
+		$row->addHeading(__("Basic Information"));
 
-    	$row = $form->addRow();
-    		$row->addLabel("name", __("Name"))->description(__("Must be unique."));
-    		$row->addTextField("name")->isRequired()->maxLength(7);
+	$row = $form->addRow();
+		$row->addLabel("name", __("Name"))->description(__("Must be unique."));
+		$row->addTextField("name")->isRequired()->maxLength(7);
 
-    	$row = $form->addRow();
-    		$row->addLabel("status", "Status");
-    		$row->addSelect("status")->fromArray(array(__("Upcoming"), __("Current"), __("Past")));
+	$row = $form->addRow();
+		$row->addLabel("status", __("Status"));
+		$row->addSelect("status")->fromArray(array(__("Upcoming"), __("Current"), __("Past")));
 
-    	$row = $form->addRow();
-    		$row->addLabel("sequenceNumber", "Sequence Number")->description(__($guid, 'Must be unique. Controls chronological ordering.'));
-    		$row->addNumber("sequenceNumber")->isRequired()->maxLength(3);
+    $row = $form->addRow();
+        $row->addLabel('sequenceNumber', __('Sequence Number'))->description(__('Must be unique. Controls chronological ordering.'));
+        $row->addSequenceNumber('sequenceNumber', 'gibbonFinanceBudgetCycle')->isRequired()->maxLength(3);
 
-    	$row = $form->addRow();
-    		$row->addLabel("dateStart", "Start Date")->description(__('Format:').' ')->append($_SESSION[$guid]['i18n']['dateFormat']);
-    		$row->addDate("dateStart")->isRequired();
+	$row = $form->addRow();
+		$row->addLabel("dateStart", __("Start Date"))->description(__('Format:').' ')->append($_SESSION[$guid]['i18n']['dateFormat']);
+		$row->addDate("dateStart")->isRequired();
 
-    	$row = $form->addRow();
-    		$row->addLabel("dateEnd", "End Date")->description(__('Format:').' ')->append($_SESSION[$guid]['i18n']['dateFormat']);
-    		$row->addDate("dateEnd")->isRequired();
+	$row = $form->addRow();
+		$row->addLabel("dateEnd", __("End Date"))->description(__('Format:').' ')->append($_SESSION[$guid]['i18n']['dateFormat']);
+		$row->addDate("dateEnd")->isRequired();
 
-    	$row = $form->addRow();
-    		$row->addHeading(__("Budget Allocations"));
+	$row = $form->addRow();
+		$row->addHeading(__("Budget Allocations"));
 
 
-    	try {
-            $dataBudget = array();
-            $sqlBudget = 'SELECT * FROM gibbonFinanceBudget ORDER BY name';
-            $resultBudget = $connection2->prepare($sqlBudget);
-            $resultBudget->execute($dataBudget);
-        } catch (PDOException $e) {
-            echo "<div class='error'>".$e->getMessage().'</div>';
-        }
-		if ($resultBudget->rowCount() < 1) {
-			$row = $form->addRow();
-				$row->addAlert(__('There are no records to display.'), "error");
-		} else {
-			while ($rowBudget = $resultBudget->fetch()) {
-
-				$description = "";
-
-				if ($_SESSION[$guid]['currency'] != '') {
-                    $description = sprintf(__($guid, 'Numeric value in %1$s.'), $_SESSION[$guid]['currency']);
-                } else {
-                    $description = __($guid, 'Numeric value.');
-                }
-
-				$row = $form->addRow();
-					$row->addLabel('values[]', $rowBudget['name'])->description($description);
-					$row->addNumber("values[]")->maxLength(15)->decimalPlaces(2)->setValue("0.00");
-					$form->addHiddenValue("gibbonFinanceBudgetIDs[]", $rowBudget['gibbonFinanceBudgetID']);
-			}
-		}
-
+	try {
+        $dataBudget = array();
+        $sqlBudget = 'SELECT * FROM gibbonFinanceBudget ORDER BY name';
+        $resultBudget = $connection2->prepare($sqlBudget);
+        $resultBudget->execute($dataBudget);
+    } catch (PDOException $e) {
+        echo "<div class='error'>".$e->getMessage().'</div>';
+    }
+	if ($resultBudget->rowCount() < 1) {
 		$row = $form->addRow();
-			$row->addFooter();
-			$row->addSubmit();
+			$row->addAlert(__('There are no records to display.'), "error");
+	} else {
+		while ($rowBudget = $resultBudget->fetch()) {
+
+			$description = "";
+
+			if ($_SESSION[$guid]['currency'] != '') {
+                $description = sprintf(__($guid, 'Numeric value in %1$s.'), $_SESSION[$guid]['currency']);
+            } else {
+                $description = __($guid, 'Numeric value.');
+            }
+
+			$row = $form->addRow();
+				$row->addLabel('values[]', $rowBudget['name'])->description($description);
+				$row->addNumber("values[]")->maxLength(15)->decimalPlaces(2)->setValue("0.00");
+				$form->addHiddenValue("gibbonFinanceBudgetIDs[]", $rowBudget['gibbonFinanceBudgetID']);
+		}
+	}
+
+	$row = $form->addRow();
+		$row->addFooter();
+		$row->addSubmit();
 
     print $form->getOutput();
 }
