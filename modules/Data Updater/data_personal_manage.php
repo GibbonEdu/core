361d183a413bb07614e593cf9acb6b7f4fa552ff
Sandra Kuipers <sandra@skuipers.com>
2018-01-17 10:25:09 +0800

    Data Updater: fix link title on Edit actions

--- modules/Data Updater/data_personal_manage.php
+++ modules/Data Updater/data_personal_manage.php
@@ -130,7 +130,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Data Updater/data_personal
             echo '</td>';
             echo '<td>';
             if ($row['status'] == 'Pending') {
-                echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/data_personal_manage_edit.php&gibbonPersonUpdateID='.$row['gibbonPersonUpdateID']."' title='".__($guid, 'Edit')."'><img src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/config.png'/></a> ";
+                echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/data_personal_manage_edit.php&gibbonPersonUpdateID='.$row['gibbonPersonUpdateID']."'><img title='".__($guid, 'Edit')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/config.png'/></a> ";
                 echo "<a class='thickbox' href='".$_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/data_personal_manage_delete.php&gibbonPersonUpdateID='.$row['gibbonPersonUpdateID']."&width=650&height=135'><img title='".__($guid, 'Delete')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/garbage.png'/></a>";
             }
             echo '</td>';
