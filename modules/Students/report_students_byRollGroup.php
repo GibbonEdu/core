7f1973d5afff6bbb7acfc539501dd887e9adc47e
Ross Parker <rossdotparker@gmail.com>
2018-02-27 15:55:05 +0800

    Students: added View option to Students by Roll Group report, allowing Basic or Extended view

--- modules/Students/report_students_byRollGroup.php
+++ modules/Students/report_students_byRollGroup.php
@@ -40,10 +40,8 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_students_b
     echo __($guid, 'Choose Roll Group');
     echo '</h2>';
 
-    $gibbonRollGroupID = null;
-    if (isset($_GET['gibbonRollGroupID'])) {
-        $gibbonRollGroupID = $_GET['gibbonRollGroupID'];
-    }
+    $gibbonRollGroupID = (isset($_GET['gibbonRollGroupID']) ? $_GET['gibbonRollGroupID'] : null);
+    $view = (isset($_GET['view']) ? $_GET['view'] : 'Basic');
 
     $form = Form::create('action', $_SESSION[$guid]['absoluteURL'].'/index.php', 'get');
 
@@ -55,6 +53,10 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_students_b
     $row = $form->addRow();
         $row->addLabel('gibbonRollGroupID', __('Roll Group'));
         $row->addSelectRollGroup('gibbonRollGroupID', $_SESSION[$guid]['gibbonSchoolYearID'], true)->selected($gibbonRollGroupID)->placeholder()->isRequired();
+
+    $row = $form->addRow();
+        $row->addLabel('view', __('View'));
+        $row->addSelect('view')->fromArray(array('basic' => __('Basic'), 'Extended' =>__('Extended')))->selected($view)->isRequired();
 
     $row = $form->addRow();
         $row->addFooter();
@@ -120,7 +122,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_students_b
         }
 
         echo "<div class='linkTop'>";
-        echo "<a target='_blank' href='".$_SESSION[$guid]['absoluteURL'].'/report.php?q=/modules/'.$_SESSION[$guid]['module']."/report_students_byRollGroup_print.php&gibbonRollGroupID=$gibbonRollGroupID'>".__($guid, 'Print')."<img style='margin-left: 5px' title='".__($guid, 'Print')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/print.png'/></a>";
+        echo "<a target='_blank' href='".$_SESSION[$guid]['absoluteURL'].'/report.php?q=/modules/'.$_SESSION[$guid]['module']."/report_students_byRollGroup_print.php&gibbonRollGroupID=$gibbonRollGroupID&view=$view'>".__($guid, 'Print')."<img style='margin-left: 5px' title='".__($guid, 'Print')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/print.png'/></a>";
         echo '</div>';
 
         echo "<table class='mini' cellspacing='0' style='width: 100%'>";
@@ -131,28 +133,30 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_students_b
         echo '<th>';
         echo __($guid, 'Student');
         echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Gender');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Age').'<br/>';
-        echo "<span style='font-style: italic; font-size: 85%'>".__($guid, 'DOB').'</span>';
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Nationality');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Transport');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'House');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Locker');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Medical');
-        echo '</th>';
+        if ($view == 'Extended') {
+            echo '<th>';
+            echo __($guid, 'Gender');
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'Age').'<br/>';
+            echo "<span style='font-style: italic; font-size: 85%'>".__($guid, 'DOB').'</span>';
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'Nationality');
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'Transport');
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'House');
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'Locker');
+            echo '</th>';
+            echo '<th>';
+            echo __($guid, 'Medical');
+            echo '</th>';
+        }
         echo '</tr>';
 
         $count = 0;
@@ -173,86 +177,87 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_students_b
             echo '<td>';
             echo formatName('', $row['preferredName'], $row['surname'], 'Student', true);
             echo '</td>';
-            echo '<td>';
-            echo $row['gender'];
-            echo '</td>';
-            echo '<td>';
-            if (is_null($row['dob']) == false and $row['dob'] != '0000-00-00') {
-                echo getAge($guid, dateConvertToTimestamp($row['dob']), true).'<br/>';
-                echo "<span style='font-style: italic; font-size: 85%'>".dateConvertBack($guid, $row['dob']).'</span>';
-            }
-            echo '</td>';
-            echo '<td>';
-            if ($row['citizenship1'] != '') {
-                echo $row['citizenship1'].'<br/>';
-            }
-            if ($row['citizenship2'] != '') {
-                echo $row['citizenship2'].'<br/>';
-            }
-            echo '</td>';
-            echo '<td>';
-            echo $row['transport'];
-            echo '</td>';
-            echo '<td>';
-            if ($row['gibbonHouseID'] != '') {
-                try {
-                    $dataHouse = array('gibbonHouseID' => $row['gibbonHouseID']);
-                    $sqlHouse = 'SELECT * FROM gibbonHouse WHERE gibbonHouseID=:gibbonHouseID';
-                    $resultHouse = $connection2->prepare($sqlHouse);
-                    $resultHouse->execute($dataHouse);
-                } catch (PDOException $e) {
-                    echo "<div class='error'>".$e->getMessage().'</div>';
+            if ($view == 'Extended') {
+                echo '<td>';
+                echo $row['gender'];
+                echo '</td>';
+                echo '<td>';
+                if (is_null($row['dob']) == false and $row['dob'] != '0000-00-00') {
+                    echo getAge($guid, dateConvertToTimestamp($row['dob']), true).'<br/>';
+                    echo "<span style='font-style: italic; font-size: 85%'>".dateConvertBack($guid, $row['dob']).'</span>';
                 }
-                if ($resultHouse->rowCount() == 1) {
-                    $rowHouse = $resultHouse->fetch();
-                    echo $rowHouse['name'];
+                echo '</td>';
+                echo '<td>';
+                if ($row['citizenship1'] != '') {
+                    echo $row['citizenship1'].'<br/>';
                 }
-            }
-            echo '</td>';
-            echo '<td>';
-            echo $row['lockerNumber'];
-            echo '</td>';
-            echo '<td>';
-            try {
-                $dataForm = array('gibbonPersonID' => $row['gibbonPersonID']);
-                $sqlForm = 'SELECT * FROM gibbonPersonMedical WHERE gibbonPersonID=:gibbonPersonID';
-                $resultForm = $connection2->prepare($sqlForm);
-                $resultForm->execute($dataForm);
-            } catch (PDOException $e) {
-                echo "<div class='error'>".$e->getMessage().'</div>';
-            }
-
-            if ($resultForm->rowCount() == 1) {
-                $rowForm = $resultForm->fetch();
-                if ($rowForm['longTermMedication'] == 'Y') {
-                    echo '<b><i>'.__($guid, 'Long Term Medication').'</i></b>: '.$rowForm['longTermMedicationDetails'].'<br/>';
+                if ($row['citizenship2'] != '') {
+                    echo $row['citizenship2'].'<br/>';
                 }
-                $condCount = 1;
-                try {
-                    $dataConditions = array('gibbonPersonMedicalID' => $rowForm['gibbonPersonMedicalID']);
-                    $sqlConditions = 'SELECT * FROM gibbonPersonMedicalCondition WHERE gibbonPersonMedicalID=:gibbonPersonMedicalID';
-                    $resultConditions = $connection2->prepare($sqlConditions);
-                    $resultConditions->execute($dataConditions);
-                } catch (PDOException $e) {
-                    echo "<div class='error'>".$e->getMessage().'</div>';
-                }
-
-                while ($rowConditions = $resultConditions->fetch()) {
-                    echo '<b><i>'.__($guid, 'Condition')." $condCount</i></b> ";
-                    echo ': '.__($guid, $rowConditions['name']);
-
-                    $alert = getAlert($guid, $connection2, $rowConditions['gibbonAlertLevelID']);
-                    if ($alert != false) {
-                        echo " <span style='color: #".$alert['color']."; font-weight: bold'>(".__($guid, $alert['name']).' '.__($guid, 'Risk').')</span>';
-                        echo '<br/>';
-                        ++$condCount;
+                echo '</td>';
+                echo '<td>';
+                echo $row['transport'];
+                echo '</td>';
+                echo '<td>';
+                if ($row['gibbonHouseID'] != '') {
+                    try {
+                        $dataHouse = array('gibbonHouseID' => $row['gibbonHouseID']);
+                        $sqlHouse = 'SELECT * FROM gibbonHouse WHERE gibbonHouseID=:gibbonHouseID';
+                        $resultHouse = $connection2->prepare($sqlHouse);
+                        $resultHouse->execute($dataHouse);
+                    } catch (PDOException $e) {
+                        echo "<div class='error'>".$e->getMessage().'</div>';
+                    }
+                    if ($resultHouse->rowCount() == 1) {
+                        $rowHouse = $resultHouse->fetch();
+                        echo $rowHouse['name'];
                     }
                 }
-            } else {
-                echo '<i>'.__($guid, 'No medical data').'</i>';
-            }
+                echo '</td>';
+                echo '<td>';
+                echo $row['lockerNumber'];
+                echo '</td>';
+                echo '<td>';
+                try {
+                    $dataForm = array('gibbonPersonID' => $row['gibbonPersonID']);
+                    $sqlForm = 'SELECT * FROM gibbonPersonMedical WHERE gibbonPersonID=:gibbonPersonID';
+                    $resultForm = $connection2->prepare($sqlForm);
+                    $resultForm->execute($dataForm);
+                } catch (PDOException $e) {
+                    echo "<div class='error'>".$e->getMessage().'</div>';
+                }
 
-            echo '</td>';
+                if ($resultForm->rowCount() == 1) {
+                    $rowForm = $resultForm->fetch();
+                    if ($rowForm['longTermMedication'] == 'Y') {
+                        echo '<b><i>'.__($guid, 'Long Term Medication').'</i></b>: '.$rowForm['longTermMedicationDetails'].'<br/>';
+                    }
+                    $condCount = 1;
+                    try {
+                        $dataConditions = array('gibbonPersonMedicalID' => $rowForm['gibbonPersonMedicalID']);
+                        $sqlConditions = 'SELECT * FROM gibbonPersonMedicalCondition WHERE gibbonPersonMedicalID=:gibbonPersonMedicalID';
+                        $resultConditions = $connection2->prepare($sqlConditions);
+                        $resultConditions->execute($dataConditions);
+                    } catch (PDOException $e) {
+                        echo "<div class='error'>".$e->getMessage().'</div>';
+                    }
+
+                    while ($rowConditions = $resultConditions->fetch()) {
+                        echo '<b><i>'.__($guid, 'Condition')." $condCount</i></b> ";
+                        echo ': '.__($guid, $rowConditions['name']);
+
+                        $alert = getAlert($guid, $connection2, $rowConditions['gibbonAlertLevelID']);
+                        if ($alert != false) {
+                            echo " <span style='color: #".$alert['color']."; font-weight: bold'>(".__($guid, $alert['name']).' '.__($guid, 'Risk').')</span>';
+                            echo '<br/>';
+                            ++$condCount;
+                        }
+                    }
+                } else {
+                    echo '<i>'.__($guid, 'No medical data').'</i>';
+                }
+                echo '</td>';
+            }
             echo '</tr>';
         }
         if ($count == 0) {
