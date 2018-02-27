d3de62b4eae787856460b9fb848bb009e3d061af
Ross Parker <rossdotparker@gmail.com>
2018-02-27 08:39:35 +0800

    Messenger: added ability for uses with Manage Messages_all to resend on behalf of others

--- modules/Messenger/messenger_manage_report_processBulk.php
+++ modules/Messenger/messenger_manage_report_processBulk.php
@@ -44,6 +44,7 @@ if ($gibbonMessengerID == '' or $action != 'resend') { echo 'Fatal error loading
     if (isActionAccessible($guid, $connection2, '/modules/Messenger/messenger_manage_report.php') == false) {
         $URL .= '&return=error0';
         header("Location: {$URL}");
+        exit;
     } else {
         $gibbonMessengerReceiptIDs = array();
         if (isset($_POST['gibbonMessengerReceiptIDs'])) {
@@ -53,70 +54,80 @@ if ($gibbonMessengerID == '' or $action != 'resend') { echo 'Fatal error loading
         if (count($gibbonMessengerReceiptIDs) < 1) {
             $URL .= '&return=error1';
             header("Location: {$URL}");
+            exit;
         } else {
             $partialFail = false;
 
-            //Check message owner
+            //Check message exists
             try {
-                $data = array("gibbonMessengerID" => $gibbonMessengerID, "gibbonPersonID" => $_SESSION[$guid]['gibbonPersonID']);
-                $sql = "SELECT * FROM gibbonMessenger WHERE gibbonMessengerID=:gibbonMessengerID AND gibbonPersonID=:gibbonPersonID";
+                $data = array("gibbonMessengerID" => $gibbonMessengerID);
+                $sql = "SELECT * FROM gibbonMessenger WHERE gibbonMessengerID=:gibbonMessengerID";
                 $result = $connection2->prepare($sql);
                 $result->execute($data);
             } catch (PDOException $e) { }
 
             if ($result->rowCount() != 1) {
-                $partialFail = true;
+                $URL .= '&return=error0';
+                header("Location: {$URL}");
+                exit;
             } else {
                 $row = $result->fetch();
 
-                //Prep message
-                $emailCount = 0;
-                $bodyReminder = "<p style='font-style: italic; font-weight: bold'>" . __($guid, 'This is a reminder for an email that requires your action. Please look for the link in the email, and click it to confirm receipt and reading of this email.') ."</p>" ;
-                $bodyFin = "<p style='font-style: italic'>" . sprintf(__($guid, 'Email sent via %1$s at %2$s.'), $_SESSION[$guid]["systemName"], $_SESSION[$guid]["organisationName"]) ."</p>" ;
-                $mail=getGibbonMailer($guid);
-				$mail->SetFrom($_SESSION[$guid]["email"], $_SESSION[$guid]["preferredName"] . " " . $_SESSION[$guid]["surname"]);
-				$mail->CharSet="UTF-8";
-				$mail->Encoding="base64" ;
-				$mail->IsHTML(true);
-				$mail->Subject=__($guid, 'REMINDER:').' '.$row['subject'] ;
+                if ($row['gibbonPersonID'] != $_SESSION[$guid]['gibbonPersonID'] && $highestAction != 'Manage Messages_all') {
+                    $URL .= '&return=error0';
+                    header("Location: {$URL}");
+                    exit;
+                }
+                else {
+                    //Prep message
+                    $emailCount = 0;
+                    $bodyReminder = "<p style='font-style: italic; font-weight: bold'>" . __($guid, 'This is a reminder for an email that requires your action. Please look for the link in the email, and click it to confirm receipt and reading of this email.') ."</p>" ;
+                    $bodyFin = "<p style='font-style: italic'>" . sprintf(__($guid, 'Email sent via %1$s at %2$s.'), $_SESSION[$guid]["systemName"], $_SESSION[$guid]["organisationName"]) ."</p>" ;
+                    $mail=getGibbonMailer($guid);
+    				$mail->SetFrom($_SESSION[$guid]["email"], $_SESSION[$guid]["preferredName"] . " " . $_SESSION[$guid]["surname"]);
+    				$mail->CharSet="UTF-8";
+    				$mail->Encoding="base64" ;
+    				$mail->IsHTML(true);
+    				$mail->Subject=__($guid, 'REMINDER:').' '.$row['subject'] ;
 
-                //Scan through receipients
-                foreach ($gibbonMessengerReceiptIDs as $gibbonMessengerReceiptID) {
-                    //Check recipient status
-                    try {
-                        $dataRecipt = array("gibbonMessengerID" => $gibbonMessengerID, "gibbonMessengerReceiptID" => $gibbonMessengerReceiptID);
-                        $sqlRecipt = "SELECT * FROM gibbonMessengerReceipt WHERE gibbonMessengerID=:gibbonMessengerID AND gibbonMessengerReceiptID=:gibbonMessengerReceiptID";
-                        $resultRecipt = $connection2->prepare($sqlRecipt);
-                        $resultRecipt->execute($dataRecipt);
-                    } catch (PDOException $e) { }
+                    //Scan through receipients
+                    foreach ($gibbonMessengerReceiptIDs as $gibbonMessengerReceiptID) {
+                        //Check recipient status
+                        try {
+                            $dataRecipt = array("gibbonMessengerID" => $gibbonMessengerID, "gibbonMessengerReceiptID" => $gibbonMessengerReceiptID);
+                            $sqlRecipt = "SELECT * FROM gibbonMessengerReceipt WHERE gibbonMessengerID=:gibbonMessengerID AND gibbonMessengerReceiptID=:gibbonMessengerReceiptID";
+                            $resultRecipt = $connection2->prepare($sqlRecipt);
+                            $resultRecipt->execute($dataRecipt);
+                        } catch (PDOException $e) { }
 
-                    if ($resultRecipt->rowCount() != 1) {
-                        $partialFail = true;
-                    } else {
-                        $rowRecipt = $resultRecipt->fetch();
+                        if ($resultRecipt->rowCount() != 1) {
+                            $partialFail = true;
+                        } else {
+                            $rowRecipt = $resultRecipt->fetch();
 
-                        //Resend message
-                        $emailCount ++;
-                        $mail->ClearAddresses();
-						$mail->AddAddress($rowRecipt['contactDetail']);
-						//Deal with email receipt and body finalisation
-						if ($row['emailReceipt'] == 'Y') {
-							$bodyReadReceipt = "<a target='_blank' href='".$_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Messenger/messenger_emailReceiptConfirm.php&gibbonMessengerID=$gibbonMessengerID&gibbonPersonID=".$rowRecipt['gibbonPersonID']."&key=".$rowRecipt['key']."'>".$row['emailReceiptText']."</a>";
-							if (is_numeric(strpos($row['body'], '[confirmLink]'))) {
-								$bodyOut = $bodyReminder.str_replace('[confirmLink]', $bodyReadReceipt, $row['body']).$bodyFin;
-							}
-							else {
-								$bodyOut = $bodyReminder.$row['body'].$bodyReadReceipt.$bodyFin;
-							}
-						}
-						else {
-							$bodyOut = $bodyReminder.$row['body'].$bodyFin;
-						}
-						$mail->Body = $bodyOut ;
-						$mail->AltBody = emailBodyConvert($bodyOut);
-                        if(!$mail->Send()) {
-							$partialFail = TRUE ;
-						}
+                            //Resend message
+                            $emailCount ++;
+                            $mail->ClearAddresses();
+    						$mail->AddAddress($rowRecipt['contactDetail']);
+    						//Deal with email receipt and body finalisation
+    						if ($row['emailReceipt'] == 'Y') {
+    							$bodyReadReceipt = "<a target='_blank' href='".$_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Messenger/messenger_emailReceiptConfirm.php&gibbonMessengerID=$gibbonMessengerID&gibbonPersonID=".$rowRecipt['gibbonPersonID']."&key=".$rowRecipt['key']."'>".$row['emailReceiptText']."</a>";
+    							if (is_numeric(strpos($row['body'], '[confirmLink]'))) {
+    								$bodyOut = $bodyReminder.str_replace('[confirmLink]', $bodyReadReceipt, $row['body']).$bodyFin;
+    							}
+    							else {
+    								$bodyOut = $bodyReminder.$row['body'].$bodyReadReceipt.$bodyFin;
+    							}
+    						}
+    						else {
+    							$bodyOut = $bodyReminder.$row['body'].$bodyFin;
+    						}
+    						$mail->Body = $bodyOut ;
+    						$mail->AltBody = emailBodyConvert($bodyOut);
+                            if(!$mail->Send()) {
+    							$partialFail = TRUE ;
+    						}
+                        }
                     }
                 }
             }
