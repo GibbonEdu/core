2026065e8f2ab63405a10c16e2a37883fa016659
Sandra Kuipers <sandra@skuipers.com>
2018-03-10 10:09:07 +0800

    Library: Add and Edit Item OOification (#465)
    * Library: add ajax handling for custom fields in Catalog Add Item (WIP)
    
    * Form Class: move interfaces to their own files
    
    * Library: move google book loader js into module.js as a prototype
    
    * Library: rename catalog_add_ajax to catalog_fields_ajax (add & edit)
    
    * Library: implement ajax-loaded fields in catalog Add Item
    
    * Library: OOified catalog Edit Item
    
    * Library: remove debug code fro Add Item process

--- modules/Library/js/module.js
+++ modules/Library/js/module.js
@@ -15,3 +15,66 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
+function stopRKey(evt) {
+    var evt = (evt) ? evt : ((event) ? event : null); var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); if ((evt.keyCode == 13) && (node.type == "text")) { return false; }
+}
+
+$.prototype.loadGoogleBookData = function (settings) {
+    $(this).click(function () {
+        var isbn = $("#fieldISBN10").val() ? $("#fieldISBN10").val() : $("#fieldISBN13").val();
+
+        if (isbn) {
+            $.get(("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn), function (data) {
+                var obj = (data.constructor === String) ? jQuery.parseJSON(data) : data;
+
+                if (obj['totalItems'] == 0) {
+                    alert(settings.notFound);
+                } else {
+                    // SET FIELDS
+                    $("#name").val(obj['items'][0]['volumeInfo']['title']);
+                    var authors = '';
+                    for (var i = 0; i < obj['items'][0]['volumeInfo']['authors'].length; i++) {
+                        authors = authors + obj['items'][0]['volumeInfo']['authors'][i] + ', ';
+                    }
+                    $("#producer").val(authors.substring(0, (authors.length - 2)));
+                    $("#fieldPublisher").val(obj['items'][0]['volumeInfo']['publisher']);
+                    if (obj['items'][0]['volumeInfo']['publishedDate'].length == 10) {
+                        $("#fieldPublicationDate").val(obj['items'][0]['volumeInfo']['publishedDate'].substring(8, 10) + '/' + obj['items'][0]['volumeInfo']['publishedDate'].substring(5, 7) + '/' + obj['items'][0]['volumeInfo']['publishedDate'].substring(0, 4));
+                    } else if (obj['items'][0]['volumeInfo']['publishedDate'].length == 7) {
+                        $("#fieldPublicationDate").val(obj['items'][0]['volumeInfo']['publishedDate'].substring(5, 7) + '/' + obj['items'][0]['volumeInfo']['publishedDate'].substring(0, 4));
+                    } else if (obj['items'][0]['volumeInfo']['publishedDate'].length == 4) {
+                        $("#fieldPublicationDate").val(obj['items'][0]['volumeInfo']['publishedDate'].substring(0, 4));
+                    }
+                    $("#fieldDescription").val(obj['items'][0]['volumeInfo']['description']);
+                    if (obj['items'][0]['volumeInfo']['industryIdentifiers'][0]['type'] == 'ISBN_10') {
+                        $("#fieldISBN10").val(obj['items'][0]['volumeInfo']['industryIdentifiers'][0]['identifier']);
+                    }
+                    if (obj['items'][0]['volumeInfo']['industryIdentifiers'][1]['type'] == 'ISBN_13') {
+                        $("#fieldISBN13").val(obj['items'][0]['volumeInfo']['industryIdentifiers'][1]['identifier']);
+                    }
+                    $("#fieldPageCount").val(obj['items'][0]['volumeInfo']['pageCount']);
+                    var format = obj['items'][0]['volumeInfo']['printType'].toLowerCase();
+                    format = format.charAt(0).toUpperCase() + format.slice(1);
+                    $("#fieldFormat").val(format);
+                    $("#fieldLink").val(obj['items'][0]['volumeInfo']['infoLink']);
+                    var image = obj['items'][0]['volumeInfo']['imageLinks']['thumbnail'];
+                    if (image) {
+                        $("#imageType").val('Link');
+                        $("#imageLinkRow").slideDown("fast", $("#imageLinkRow").css("display", "table-row"));
+                        $("#imageLink").enable();
+                        $("#imageLink").val(image);
+                    }
+                    $("#fieldLanguage").val(obj['items'][0]['volumeInfo']['language']);
+                    var subjects = '';
+                    for (var i = 0; i < obj['items'][0]['volumeInfo']['categories'].length; i++) {
+                        subjects = subjects + obj['items'][0]['volumeInfo']['categories'][i] + ', ';
+                    }
+                    $("#fieldSubjects").val(subjects.substring(0, (subjects.length - 2)));
+                }
+            });
+        } else {
+            alert(settings.dataRequired);
+        }
+    });
+};
