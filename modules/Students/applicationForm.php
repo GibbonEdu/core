9526e06abe1a4874c7b8eb0cb0b0bfaac5f4b476
Sandra Kuipers <sandra@skuipers.com>
2018-04-25 11:17:23 +0800

    Students: add help text to parent Employer field in application form

--- modules/Students/applicationForm.php
+++ modules/Students/applicationForm.php
@@ -636,7 +636,7 @@ if ($proceed == false) {
                 $row->addTextField("parent{$i}profession")->maxLength(30)->loadFrom($application);
 
             $row = $form->addRow()->setClass("parentSection{$i}");
-                $row->addLabel("parent{$i}employer", __('Employer'));
+                $row->addLabel("parent{$i}employer", __('Employer'))->description(__('Employer company or organization name<br/>eg: Bank of China, MUST, etc.'));
                 $row->addTextField("parent{$i}employer")->isRequired()->maxLength(150)->loadFrom($application);
 
             // CUSTOM FIELDS FOR PARENTS
