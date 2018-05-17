3a5533308753d3f83ff62e678d3e2b1e8d43b3ef
Sandra Kuipers <sandra@skuipers.com>
2018-05-17 09:19:24 +0800

    System: add PDOStatement methods to Result contract, add comments

--- src/Contracts/Database/Result.php
+++ src/Contracts/Database/Result.php
@@ -21,17 +21,94 @@ namespace Gibbon\Contracts\Database;
 
 interface Result
 {
+    /**
+     * Returns the number of rows affected by the last SQL statement. 
+     * PDOStatement method. 
+     *
+     * @return integer
+     */
+    public function rowCount();
+
+    /**
+     * Does the result contain no database rows?
+     *
+     * @return bool
+     */
     public function isEmpty();
 
+    /**
+     * Does the result contain any database rows?
+     *
+     * @return bool
+     */
     public function isNotEmpty();
 
-    public function fetchAny();
+    /**
+     * Fetches the next row from a result set. 
+     * PDOStatement method. 
+     *
+     * @param integer|null $fetch_style
+     * @param integer|null $cursor_orientation
+     * @param integer|null $cursor_offset
+     * @return mixed
+     */
+    public function fetch($fetch_style = null, $cursor_orientation = null, $cursor_offset = null);
 
+    /**
+     * Returns an array containing all of the result set rows. 
+     * PDOStatement method. 
+     *
+     * @param integer|null        $fetch_style
+     * @param integer|string|null $fetch_argument
+     * @param array|null          $ctor_args
+     * @return array
+     */
+    public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = null);
+
+    /**
+     * Returns a single column from the next row of a result set. 
+     * PDOStatement method. 
+     *
+     * @param integer $column_number
+     * @return string
+     */
+    public function fetchColumn($column_number = 0);
+    
+    /**
+     * Fetches all as an array, grouped by key using the first column in the result set.
+     *
+     * @return array
+     */
     public function fetchGrouped();
 
-    public function fetchGroupedByUnique();
+    /**
+     * Fetches all as an array, grouped by key where the contents 
+     *
+     * @return array
+     */
+    public function fetchGroupedUnique();
 
+    /**
+     * Fetches all as an associative array of key => value pairs. The query may only have two columns.
+     *
+     * @return array
+     */
     public function fetchKeyPair();
 
+    /**
+     * Returns the number of rows affected by the last SQL statement. 
+     * PDOStatement method. 
+     *
+     * @param  integer    $mode
+     * @param  mixed|null $params
+     * @return boolean
+     */
+    public function setFetchMode($mode, $params = null);
+
+    /**
+     * Fetches all results and returns it as a DataSet object.
+     *
+     * @return array
+     */
     public function toDataSet();
 }
--- src/Database/Result.php
+++ src/Database/Result.php
@@ -22,43 +22,68 @@ namespace Gibbon\Database;
 use PDO;
 use PDOStatement;
 use Gibbon\Domain\DataSet;
-use Gibbon\Contracts\Database\Result;
+use Gibbon\Contracts\Database\Result as ResultContract;
 
 /**
- * Helper methods to improve the intent and readability of database code.
+ * Methods to improve the intent and readability of database code.
  */
-class Result extends PDOStatement implements Result
+class Result extends PDOStatement implements ResultContract
 {
+    /**
+     * Does the result contain no database rows?
+     *
+     * @return bool
+     */
     public function isEmpty()
     {
         return $this->rowCount() == 0;
     }
 
+    /**
+     * Does the result contain any database rows?
+     *
+     * @return bool
+     */
     public function isNotEmpty()
     {
         return $this->rowCount() > 0;
     }
 
-    public function fetchAny()
-    {
-        return $this->isNotEmpty()? $this->fetchAll() : array();
-    }
-
+    /**
+     * Fetches all as an array, grouped by key using the first column in the result set.
+     *
+     * @return array
+     */
     public function fetchGrouped()
     {
         return $this->isNotEmpty()? $this->fetchAll(PDO::FETCH_GROUP) : array();
     }
 
-    public function fetchGroupedByUnique()
+    /**
+     * Fetches all as an array, grouped by key where the contents 
+     *
+     * @return array
+     */
+    public function fetchGroupedUnique()
     {
         return $this->isNotEmpty()? $this->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_UNIQUE) : array();
     }
 
+    /**
+     * Fetches all as an associative array of key => value pairs. The query may only have two columns.
+     *
+     * @return array
+     */
     public function fetchKeyPair()
     {
         return $this->isNotEmpty()? $this->fetchAll(PDO::FETCH_KEY_PAIR) : array();
     }
 
+    /**
+     * Fetches all results and returns it as a DataSet object.
+     *
+     * @return array
+     */
     public function toDataSet()
     {
         return new DataSet($this->fetchAll());
