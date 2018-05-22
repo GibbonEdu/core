1802f2d73972f6915b5c96d79488822882fb9e21
Sandra Kuipers <sandra@skuipers.com>
2018-05-22 17:35:27 +0800

    Roll Groups: ooify the Roll Group table, add RollGroupGateway

--- modules/Roll Groups/rollGroups.php
+++ modules/Roll Groups/rollGroups.php
@@ -17,114 +17,46 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Tables\DataTable;
+use Gibbon\Services\Format;
+use Gibbon\Domain\School\RollGroupGateway;
+
 if (isActionAccessible($guid, $connection2, '/modules/Roll Groups/rollGroups.php') == false) {
     //Acess denied
     echo "<div class='error'>";
-    echo __($guid, 'You do not have access to this action.');
+    echo __('You do not have access to this action.');
     echo '</div>';
 } else {
     //Proceed!
     echo "<div class='trail'>";
-    echo "<div class='trailHead'><a href='".$_SESSION[$guid]['absoluteURL']."'>".__($guid, 'Home')."</a> > <a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_GET['q']).'/'.getModuleEntry($_GET['q'], $connection2, $guid)."'>".__($guid, getModuleName($_GET['q']))."</a> > </div><div class='trailEnd'>".__($guid, 'View Roll Groups').'</div>';
+    echo "<div class='trailHead'><a href='".$_SESSION[$guid]['absoluteURL']."'>".__('Home')."</a> > <a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_GET['q']).'/'.getModuleEntry($_GET['q'], $connection2, $guid)."'>".__($guid, getModuleName($_GET['q']))."</a> > </div><div class='trailEnd'>".__('View Roll Groups').'</div>';
     echo '</div>';
 
     echo '<p>';
-    echo __($guid, 'This page shows all roll groups in the current school year.');
+    echo __('This page shows all roll groups in the current school year.');
     echo '</p>';
 
-    try {
-        $data = array('gibbonSchoolYearID' => $_SESSION[$guid]['gibbonSchoolYearID']);
-        $sql = 'SELECT gibbonSchoolYear.gibbonSchoolYearID, gibbonRollGroupID, gibbonSchoolYear.name as yearName, gibbonRollGroup.name, gibbonRollGroup.nameShort, gibbonPersonIDTutor, gibbonPersonIDTutor2, gibbonPersonIDTutor3, gibbonSpace.name AS space, website FROM gibbonRollGroup JOIN gibbonSchoolYear ON (gibbonRollGroup.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID) LEFT JOIN gibbonSpace ON (gibbonRollGroup.gibbonSpaceID=gibbonSpace.gibbonSpaceID) WHERE gibbonSchoolYear.gibbonSchoolYearID=:gibbonSchoolYearID ORDER BY sequenceNumber, gibbonRollGroup.name';
-        $result = $connection2->prepare($sql);
-        $result->execute($data);
-    } catch (PDOException $e) {
-        echo "<div class='error'>".$e->getMessage().'</div>';
-    }
+    $gateway = $container->get(RollGroupGateway::class);
+    $rollGroups = $gateway->selectRollGroupsBySchoolYear($_SESSION[$guid]['gibbonSchoolYearID']);
+    
+    $formatTutorsList = function($row) use ($gateway) {
+        $tutors = $gateway->selectTutorsByRollGroup($row['gibbonRollGroupID'])->fetchAll();
+        if (count($tutors) > 1) $tutors[0]['surname'] .= ' ('.__('Main Tutor').')';
 
-    if ($result->rowCount() < 1) {
-        echo "<div class='error'>";
-        echo __($guid, 'There are no records to display.');
-        echo '</div>';
-    } else {
-        echo "<table cellspacing='0' style='width: 100%'>";
-        echo "<tr class='head'>";
-        echo '<th>';
-        echo __($guid, 'Name');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Form Tutors');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Room');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Students');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Website');
-        echo '</th>';
-        echo '<th>';
-        echo __($guid, 'Actions');
-        echo '</th>';
-        echo '</tr>';
+        return Format::listNames($tutors, 'Staff', false, true);
+    };
 
-        $count = 0;
-        $rowNum = 'odd';
-        while ($row = $result->fetch()) {
-            if ($count % 2 == 0) {
-                $rowNum = 'even';
-            } else {
-                $rowNum = 'odd';
-            }
+    $table = DataTable::create('rollGroups');
 
-            //COLOR ROW BY STATUS!
-            echo "<tr class=$rowNum>";
-            echo '<td>';
-            echo $row['name'];
-            echo '</td>';
-            echo '<td>';
-            try {
-                $dataTutor = array('gibbonPersonID1' => $row['gibbonPersonIDTutor'], 'gibbonPersonID2' => $row['gibbonPersonIDTutor2'], 'gibbonPersonID3' => $row['gibbonPersonIDTutor3']);
-                $sqlTutor = 'SELECT gibbonPersonID, surname, preferredName FROM gibbonPerson WHERE gibbonPersonID=:gibbonPersonID1 OR gibbonPersonID=:gibbonPersonID2 OR gibbonPersonID=:gibbonPersonID3';
-                $resultTutor = $connection2->prepare($sqlTutor);
-                $resultTutor->execute($dataTutor);
-            } catch (PDOException $e) {
-                echo "<div class='error'>".$e->getMessage().'</div>';
-            }
-            while ($rowTutor = $resultTutor->fetch()) {
-                echo formatName('', $rowTutor['preferredName'], $rowTutor['surname'], 'Staff', false, true);
-                if ($rowTutor['gibbonPersonID'] == $row['gibbonPersonIDTutor'] and $resultTutor->rowCount() > 1) {
-                    echo ' ('.__($guid, 'Main Tutor').')';
-                }
-                echo '<br/>';
-            }
-            echo '</td>';
-            echo '<td>';
-            echo $row['space'];
-            echo '</td>';
-            echo '<td>';
-            try {
-                $dataCount = array('gibbonRollGroupID' => $row['gibbonRollGroupID']);
-                $sqlCount = "SELECT gibbonPerson.gibbonPersonID FROM gibbonStudentEnrolment INNER JOIN gibbonPerson ON gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID WHERE gibbonRollGroupID=:gibbonRollGroupID AND status='Full' AND (dateStart IS NULL OR dateStart<='".date('Y-m-d')."') AND (dateEnd IS NULL  OR dateEnd>='".date('Y-m-d')."') ORDER BY preferredName, surname";
-                $resultCount = $connection2->prepare($sqlCount);
-                $resultCount->execute($dataCount);
-            } catch (PDOException $e) {
-                echo "<div class='error'>".$e->getMessage().'</div>';
-            }
-            echo $resultCount->rowCount();
-            echo '</td>';
-            echo '<td>';
-            if ($row['website'] != '') {
-                echo "<a target='_blank' href='".$row['website']."'>".$row['website'].'</a>';
-            }
-            echo '</td>';
-            echo '<td>';
-            echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/rollGroups_details.php&gibbonRollGroupID='.$row['gibbonRollGroupID']."'><img title='".__($guid, 'View')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/plus.png'/></a> ";
-            echo '</td>';
-            echo '</tr>';
+    $table->addColumn('name', __('Name'));
+    $table->addColumn('tutors', __('Form Tutors'))->format($formatTutorsList);
+    $table->addColumn('space', __('Room'));
+    $table->addColumn('students', __('Students'));
+    $table->addColumn('website', __('Website'))->format(Format::using('link', 'website'));
 
-            ++$count;
-        }
-        echo '</table>';
-    }
+    $actions = $table->addActionColumn()->addParam('gibbonRollGroupID');
+    $actions->addAction('view', __('View'))
+            ->setURL('/modules/Roll Groups/rollGroups_details.php');
+
+    echo $table->render($rollGroups->toDataSet());
 }
