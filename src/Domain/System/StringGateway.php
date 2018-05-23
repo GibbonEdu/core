1c076a743f2bef5966b72ef3ed25f5cdf2fc3929
Sandra Kuipers <sandra@skuipers.com>
2018-05-23 09:28:28 +0800

    System Admin: String Replacements table ooification

---
+++ src/Domain/System/StringGateway.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Domain\System;
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * String Repalcement Gateway
+ *
+ * @version v16
+ * @since   v16
+ */
+class StringGateway extends QueryableGateway
+{
+    use TableAware;
+
+    private static $tableName = 'gibbonString';
+
+    private static $searchableColumns = ['original', 'replacement'];
+    
+    /**
+     * Queries the list of strings for the Manage String Replacements page.
+     *
+     * @param QueryCriteria $criteria
+     * @return DataSet
+     */
+    public function queryStrings(QueryCriteria $criteria)
+    {
+        $query = $this
+            ->newQuery()
+            ->from($this->getTableName())
+            ->cols([
+                'gibbonStringID', 'original', 'replacement', 'mode', 'caseSensitive', 'priority'
+            ]);
+
+        return $this->runQuery($query, $criteria);
+    }
+}
