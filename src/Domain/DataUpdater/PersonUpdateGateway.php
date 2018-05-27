10de9f4e880f4eec491a6a4c0d83183beadcdd2c
Sandra Kuipers <sandra@skuipers.com>
2018-05-27 16:54:59 +0800

    Data Updater: table ooification (#534)
    * Tables: add CheckboxColumn and ExpandableColumn classes
    
    * Tables: move table Column classes into their own sub-namespace
    
    * Data Updater: Manage Family Updates table ooification
    
    * Data Updater: Manage Finance Updates table ooification
    
    * Data Updater: Manage Medical Updates table ooification
    
    * Data Updater: Manage Personal Updates table ooification

---
+++ src/Domain/DataUpdater/PersonUpdateGateway.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Domain\DataUpdater;
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * @version v16
+ * @since   v16
+ */
+class PersonUpdateGateway extends QueryableGateway
+{
+    use TableAware;
+
+    private static $tableName = 'gibbonPersonUpdate';
+
+    private static $searchableColumns = [''];
+    
+    /**
+     * @param QueryCriteria $criteria
+     * @return DataSet
+     */
+    public function queryDataUpdates(QueryCriteria $criteria)
+    {
+        $query = $this
+            ->newQuery()
+            ->from($this->getTableName())
+            ->cols([
+                'gibbonPersonUpdateID', 'gibbonPersonUpdate.status', 'gibbonPersonUpdate.timestamp', 'target.preferredName', 'target.surname', 'updater.title as updaterTitle', 'updater.preferredName as updaterPreferredName', 'updater.surname as updaterSurname'
+            ])
+            ->leftJoin('gibbonPerson AS target', 'target.gibbonPersonID=gibbonPersonUpdate.gibbonPersonID')
+            ->leftJoin('gibbonPerson AS updater', 'updater.gibbonPersonID=gibbonPersonUpdate.gibbonPersonIDUpdater');
+
+        return $this->runQuery($query, $criteria);
+    }
+}
