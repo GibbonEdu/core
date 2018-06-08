35209512f8d9953e7d0b467de38aaaed072d88bb
Sandra Kuipers <sandra@skuipers.com>
2018-06-08 12:11:34 +0800

    CLI: remove duplicate connections from photo CLI scripts

--- cli/staff_renameSchoolPhotos.php
+++ cli/staff_renameSchoolPhotos.php
@@ -19,11 +19,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 require getcwd().'/../config.php';
 require getcwd().'/../functions.php';
-//New PDO DB connection
-$pdo = new Gibbon\sqlConnection();
-$connection2 = $pdo->getConnection();
-
-@session_start();
 
 getSystemSettings($guid, $connection2);
 
