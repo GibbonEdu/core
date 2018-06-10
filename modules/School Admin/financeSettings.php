bf541a352e1a6ebf28224d97e67954444397af6f
Ross Parker <rossdotparker@gmail.com>
2018-06-10 22:44:12 +0800

    School Admin: fixed breadcrumb text issue in Manage Finance Settings

--- modules/School Admin/financeSettings.php
+++ modules/School Admin/financeSettings.php
@@ -28,7 +28,7 @@ if (isActionAccessible($guid, $connection2, '/modules/School Admin/financeSettin
 } else {
     //Proceed!
     echo "<div class='trail'>";
-    echo "<div class='trailHead'><a href='".$_SESSION[$guid]['absoluteURL']."'>".__($guid, 'Home')."</a> > <a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_GET['q']).'/'.getModuleEntry($_GET['q'], $connection2, $guid)."'>".__($guid, getModuleName($_GET['q']))."</a> > </div><div class='trailEnd'>".__($guid, 'Invoice & Receipt Settings').'</div>';
+    echo "<div class='trailHead'><a href='".$_SESSION[$guid]['absoluteURL']."'>".__($guid, 'Home')."</a> > <a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_GET['q']).'/'.getModuleEntry($_GET['q'], $connection2, $guid)."'>".__($guid, getModuleName($_GET['q']))."</a> > </div><div class='trailEnd'>".__($guid, 'Manage Finance Settings').'</div>';
     echo '</div>';
 
     if (isset($_GET['return'])) {
