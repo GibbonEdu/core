691717a385964342d7209d6e1a4470fbc860d943
Sandra Kuipers <sandra@skuipers.com>
2018-08-16 13:48:43 +0800

    User Admin: fix display of student name and status in Rollover (#590)
    * User Admin: fix display of student name and status in Rollover
    
    * Timetable Admin: fix PHP undefined index notice in Course Rollover

--- modules/Timetable Admin/course_rollover.php
+++ modules/Timetable Admin/course_rollover.php
@@ -172,11 +172,13 @@ if (isActionAccessible($guid, $connection2, '/modules/Timetable Admin/course_rol
                     $header->addContent(__('New Class'));
 
                 foreach ($currentCourses as $gibbonCourseClassID => $course) {
+                    $gibbonCourseClassIDNext = isset($course['gibbonCourseClassIDNext'])? $course['gibbonCourseClassIDNext'] : '';
+
                     $row = $table->addRow();
                         $row->addContent($course['course'].'.'.$course['class']);
                         $row->addSelect('gibbonCourseClassIDNext['.$gibbonCourseClassID.']')
                             ->fromArray($nextCourses)
-                            ->selected($course['gibbonCourseClassIDNext'])
+                            ->selected($gibbonCourseClassIDNext)
                             ->placeholder()
                             ->setClass('mediumWidth');
                 }
