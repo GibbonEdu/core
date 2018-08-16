eee257ab5b1c1e4f2f399f37adb087f2d85fd529
Sandra Kuipers <sandra@skuipers.com>
2018-08-16 13:48:43 +0800

    User Admin: fix display of student name and status in Rollover (#590)
    * User Admin: fix display of student name and status in Rollover
    
    * Timetable Admin: fix PHP undefined index notice in Course Rollover

--- modules/User Admin/rollover.php
+++ modules/User Admin/rollover.php
@@ -346,7 +346,7 @@ if (isActionAccessible($guid, $connection2, '/modules/User Admin/rollover.php') 
 
                                 $form->addHiddenValue($count."-enrolFull-gibbonPersonID", $student[0]);
                                 $row = $form->addRow();
-                                    $row->addColumn()->addContent(formatName('', $student[1], $student[2], 'Student', true));
+                                    $row->addColumn()->addContent(formatName('', $student[2], $student[1], 'Student', true));
                                     $row->addColumn()->addContent(__($student[3]));
                                     $column = $row->addColumn();
                                         $column->addCheckbox($count."-enrolFull-enrol")->setValue('Y')->checked('Y');
@@ -461,7 +461,7 @@ if (isActionAccessible($guid, $connection2, '/modules/User Admin/rollover.php') 
                         $row = $form->addRow();
                             $row->addColumn()->addContent(formatName('', $rowFinal['preferredName'], $rowFinal['surname'], 'Student', true));
                             $row->addColumn()->addContent(__($rowFinal['name']));
-                            $row->addColumn()->addContent(__('Expected'));
+                            $row->addColumn()->addContent(__('Full'));
                             $column = $row->addColumn();
                                 $column->addSelect($count."-final-status")->fromArray($statuses)->isRequired()->setClass('shortWidth floatNone')->selected('Left');
                     }
