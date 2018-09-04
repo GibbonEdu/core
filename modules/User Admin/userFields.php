8a0d29726a136b6fcb586530d4c1ad97389571d5
Sandra Kuipers <sandra@skuipers.com>
2018-09-04 09:05:00 +0800

    User Admin: fix user-readable type name in Manage User Custom Fields

--- modules/User Admin/userFields.php
+++ modules/User Admin/userFields.php
@@ -66,8 +66,18 @@ if (isActionAccessible($guid, $connection2, '/modules/User Admin/userFields.php'
         'role:other'   => __('Role').': '.__('Other'),
     ]);
 
+    $customFieldTypes = array(
+        'varchar' => __('Short Text'),
+        'text'    => __('Long Text'),
+        'date'    => __('Date'),
+        'url'     => __('Link'),
+        'select'  => __('Dropdown')
+    );
+
     $table->addColumn('name', __('Name'));
-    $table->addColumn('type', __('Type'));
+    $table->addColumn('type', __('Type'))->format(function ($row) use ($customFieldTypes) {
+        return isset($customFieldTypes[$row['type']])? $customFieldTypes[$row['type']] : '';
+    });
     $table->addColumn('active', __('Active'))->format(Format::using('yesNo', 'active'));
     $table->addColumn('roles', __('Role Categories'))
         ->notSortable()
