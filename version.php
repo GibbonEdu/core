2947faf7b9fc9735a26c903684ce9b59265d561f
Sandra Kuipers <sandra@skuipers.com>
2018-09-20 15:24:53 +0800

    System & Installer: add a check for mod_rewrite, ooified System Check (#629)
    * System & Installer: add a check for mod_rewrite, ooified System Check
    
    * System Check & Installer: silence errors on apache-related functions

--- version.php
+++ version.php
@@ -26,10 +26,11 @@ $version = '17.0.00';
  * System Requirements
  */
 $systemRequirements = array(
-    'php' 			=> '7.0.0',
-    'mysql' 		=> '5.6',
-    'extensions' 	=> array('gettext', 'mbstring', 'curl', 'zip', 'xml', 'gd'),
-    'settings' 		=> array(
+    'php'        => '7.0.0',
+    'mysql'      => '5.6',
+    'apache'     => array('mod_rewrite'),
+    'extensions' => array('gettext', 'mbstring', 'curl', 'zip', 'xml', 'gd'),
+    'settings'   => array(
                         array('max_input_vars', '>=', 5000),
                         array('max_file_uploads', '>=', 20),
                         array('allow_url_fopen', '==', 1),
