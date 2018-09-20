d882f7bfd09ef516900de73fb7d7a9441348d9d8
Sandra Kuipers <sandra@skuipers.com>
2018-09-20 15:13:38 +0800

    System Admin: move i18n files to submodule, add language installer (#628)
    * System Admin: add a tool to download & install i18n files through Manage Languages
    
    * System: remove all i18n files
    
    * System: add i18n directory as a git submodule
    
    * System: apply i18n submodule changes
    
    * System Admin: add a default message when no languages are installed

--- src/Tables/Renderer/PaginatedRenderer.php
+++ src/Tables/Renderer/PaginatedRenderer.php
@@ -68,7 +68,7 @@ class PaginatedRenderer extends SimpleRenderer implements RendererInterface
 
         $output .= parent::renderTable($table, $dataSet);
 
-        $output .= '</div></div><br/>';
+        $output .= '</div></div>';
 
         // Persist the bulk actions outside the AJAX-reloaded data table div
         $output .= $this->renderBulkActions($table);
@@ -98,6 +98,8 @@ class PaginatedRenderer extends SimpleRenderer implements RendererInterface
      */
     protected function renderHeader(DataTable $table, DataSet $dataSet) 
     {
+        if ($table->getMetaData('hidePagination') == true) return '';
+        
         $filterOptions = $table->getMetaData('filterOptions', []);
 
         $output = '<div class="flexRow">';
@@ -125,6 +127,8 @@ class PaginatedRenderer extends SimpleRenderer implements RendererInterface
      */
     protected function renderFooter(DataTable $table, DataSet $dataSet)
     {
+        if ($table->getMetaData('hidePagination') == true) return '';
+        
         $output = parent::renderFooter($table, $dataSet);
 
         if ($dataSet->getPageCount() > 1) {
