9eb99d17f8161cb980510367af7904711f613739
Sandra Kuipers <sandra@skuipers.com>
2018-09-27 20:06:42 +0800

    Messenger: optionally bcc all messenger emails to a set of recipients (#640)
    * Messenger: optionally bcc all messenger emails to a set of recipients
    
    * Changelog update

--- modules/School Admin/messengerSettings.php
+++ modules/School Admin/messengerSettings.php
@@ -84,7 +84,14 @@ if (isActionAccessible($guid, $connection2, '/modules/School Admin/messengerSett
 	$setting = getSettingByScope($connection2, 'Messenger', 'enableHomeScreenWidget', true);
 	$row = $form->addRow();
     	$row->addLabel($setting['name'], __($setting['nameDisplay']))->description(__($setting['description']));
-		$row->addYesNo($setting['name'])->selected($setting['value'])->isRequired();
+        $row->addYesNo($setting['name'])->selected($setting['value'])->isRequired();
+       
+    $row = $form->addRow()->addHeading(__('Miscellaneous'));
+
+	$setting = getSettingByScope($connection2, 'Messenger', 'messageBcc', true);
+	$row = $form->addRow();
+    	$row->addLabel($setting['name'], __($setting['nameDisplay']))->description(__($setting['description']));
+		$row->addTextArea($setting['name'])->setValue($setting['value'])->setRows(2);
 
 
 	$row = $form->addRow();
