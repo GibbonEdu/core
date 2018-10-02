4e251d6d55b5f37dcf150452c39c214aadef8f74
Sandra Kuipers <sandra@skuipers.com>
2018-10-02 16:24:05 +0800

    System: add the Twig template engine via Composer (#644)

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,21 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
 {
+    public static $files = array (
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Twig\\' => 5,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+        ),
         'P' => 
         array (
             'Psr\\Container\\' => 14,
@@ -30,6 +44,18 @@ class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
     );
 
     public static $prefixDirsPsr4 = array (
+        'Twig\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twig/twig/src',
+        ),
+        'Symfony\\Polyfill\\Mbstring\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
+        ),
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
@@ -53,11 +79,22 @@ class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'T' => 
+        array (
+            'Twig_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
