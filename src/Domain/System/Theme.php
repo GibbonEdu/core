1fa85a84109e307773725235d3186da52232e085
Sandra Kuipers <sandra@skuipers.com>
2018-10-03 17:06:14 +0800

    System: add Action, Module and Theme class structures

---
+++ src/Domain/System/Theme.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Domain\System;
+
+/**
+ * Gibbon Theme Model.
+ *
+ * @version v17
+ * @since   v17
+ */
+class Theme
+{
+    /**
+     * Get the theme name, used in the folder path and database record.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+
+    }
+
+    /**
+     * Returns the collection of stylesheets used by this theme.
+     *
+     * @return AssetBundle
+     */
+    public function stylesheets()
+    {
+        
+    }
+
+    /**
+     * Returns the collection of scripts used by this theme.
+     *
+     * @return AssetBundle
+     */
+    public function scripts()
+    {
+        
+    }
+}
