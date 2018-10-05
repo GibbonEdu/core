4e8715d248b69f94290adbcd86be5fe3fa8c0c5c
Koala Yeung <koalay@gmail.com>
2018-10-05 23:55:03 +0800

    View: improve AssetBundle and Page class
    * improved documentation and type hinting.
    * fix issues raised by phpcs.

--- src/View/AssetBundle.php
+++ src/View/AssetBundle.php
@@ -1,29 +1,43 @@
 <?php
-/*
-Gibbon, Flexible & Open School System
-Copyright (C) 2010, Ross Parker
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version >= 7.0
+ *
+ * @category File
+ * @package  Gibbon
+ * @author   Ross Parker <ross@rossparker.org>
+ * @license  GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link     https://gibbonedu.org/
+ */
 
 namespace Gibbon\View;
 
 /**
- * A collection of HTML assets (scripts & stylesheets) which can be registered for later use.
+ * A collection of HTML assets (scripts & stylesheets) which can be registered
+ * for later use.
  *
- * @version v17
- * @since   v17
+ * @category Class
+ * @package  Gibbon\View
+ * @author   Ross Parker <ross@rossparker.org>
+ * @license  GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @version  Release: v17
+ * @link     https://gibbonedu.org/
+ * @since    Release: v17 
  */
 class AssetBundle
 {
@@ -33,44 +47,59 @@ class AssetBundle
     /**
      * Register a named asset for later use. Name should be unique.
      *
-     * @param string $name      Unique identifier for this asset.
-     * @param string $src       URL, relative to the system absolutePath, or 
-     *                          inline content depending on `type` in `$options`
-     * @param array  $options   Options for rendering, includes these fields:
-     *                          - string `type`
-     *                                  'url' (default) for URL as $content.
-     *                                  'inline' for inline script or style as $content.
-     *                          - string `context`
-     *                                  The output location, eg: 'head', 'foot'
-     *                          - string 'media'
-     *                                  The media type (stylesheets only), eg: 'all', 'screen', 'print'
-     *                          - mixed `version`
-     *                                  The version number is appended to the asset URL for cache-busting.
-     *                          - mixed `weight`
-     *                                  Determines the execution order of assets.
+     * @param string $name    Unique identifier for this asset.
+     * @param string $src     URL, relative to the system absolutePath, or
+     *                        inline content depending on `type` in `$options`
+     * @param array  $options Options for rendering, includes these fields:
+     *
+     *                        string ['type']
+     *                        'url' (default) for URL as $content.
+     *                        'inline' for inline script or style as $content.
+     *
+     *                        string ['context']
+     *                        The output location, eg: 'head', 'foot'
+     *
+     *                        string ['media']
+     *                        The media type (stylesheets only),
+     *                        eg: 'all', 'screen', 'print'.
+     *
+     *                        mixed ['version']
+     *                        The version number is appended to the asset URL
+     *                        for cache-busting.
+     *
+     *                        int|null ['weight']
+     *                        Determines the execution order of assets.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function register($name, $src, array $options = [])
+    public function register(string $name, string $src, array $options = []): void
     {
-        $this->allAssets[$name] = array_replace([
-            'src'     => $src,
-            'type'    => 'url',
-            'context' => 'head',
-            'media'   => 'all',
-            'version' => null,
-            'weight'  => null,
-        ], $options);
+        $this->allAssets[$name] = array_replace(
+            [
+                'src' => $src,
+                'type' => 'url',
+                'context' => 'head',
+                'media' => 'all',
+                'version' => null,
+                'weight' => null,
+            ], $options
+        );
     }
 
     /**
      * Add an asset, optionally only providing the name of one previously registered.
      *
-     * @param string $name      Unique identifier for this asset.
-     * @param mixed  $src
-     * @param array  $options
-     * 
+     * @param string $name    Unique identifier for this asset.
+     * @param mixed  $src     Source string of the asset.
+     * @param array  $options Additional options for the registered asset.
+     *
      * @see register
+     *
+     * @return void
      */
-    public function add($name, $src = null, $options = [])
+    public function add(string $name, $src = null, array $options = []): void
     {
         if (!is_null($src)) {
             $this->register($name, $src, $options);
@@ -80,21 +109,29 @@ class AssetBundle
     }
 
     /**
-     * Get all assets sorted by weight, ascending. Optionally filter returned assets by context.
+     * Get all assets sorted by weight, ascending. Optionally filter returned
+     * assets by context.
      *
-     * @param string $context
+     * @param string $context Get assets of certain context.
+     *
      * @return array
      */
-    public function getAssets($context = null)
+    public function getAssets(string $context = null): array
     {
-        $usedAssets = array_intersect_key($this->allAssets, array_flip($this->usedAssetsByName));
+        $usedAssets = array_intersect_key(
+            $this->allAssets, array_flip($this->usedAssetsByName)
+        );
 
-        uasort($usedAssets, function($a, $b) {
-            return $a['weight'] <=> $b['weight'];
-        });
+        uasort(
+            $usedAssets, function ($a, $b) {
+                return $a['weight'] <=> $b['weight'];
+            }
+        );
 
-        return array_filter($usedAssets, function($item) use ($context) {
-            return empty($context) || $item['context'] == $context;
-        });
+        return array_filter(
+            $usedAssets, function ($item) use ($context) {
+                return empty($context) || $item['context'] == $context;
+            }
+        );
     }
 }
--- src/View/Page.php
+++ src/View/Page.php
@@ -1,31 +1,46 @@
 <?php
-/*
-Gibbon, Flexible & Open School System
-Copyright (C) 2010, Ross Parker
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
+/**
+ * Gibbon, Flexible & Open School System
+ * Copyright (C) 2010, Ross Parker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * PHP version >= 7.0
+ *
+ * @category File
+ * @package  Gibbon
+ * @author   Ross Parker <ross@rossparker.org>
+ * @license  GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @link     https://gibbonedu.org/
+ */
 
 namespace Gibbon\View;
 
+use Gibbon\Domain\System\Module;
+use Gibbon\Domain\System\Theme;
 use Gibbon\View\AssetBundle;
 
 /**
  * Holds the details for rendering the current page.
  *
- * @version v17
- * @since   v17
+ * @category Class
+ * @package  Gibbon\View
+ * @author   Ross Parker <ross@rossparker.org>
+ * @license  GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+ * @version  Release: v17
+ * @link     https://gibbonedu.org/
+ * @since    Release: v17 
  */
 class Page
 {
@@ -39,7 +54,7 @@ class Page
     protected $theme;
 
     /**
-     * These properties can be modified during the runtime of a script, 
+     * These properties can be modified during the runtime of a script,
      * and will be output at the end during template rendering.
      */
     protected $stylesheets;
@@ -50,9 +65,9 @@ class Page
     /**
      * Create a new page from a variable set of constructor params.
      *
-     * @param array $params
+     * @param array $params Essential parameters for building a page.
      */
-    public function __construct($params = [])
+    public function __construct(array $params = [])
     {
         $this->stylesheets = new AssetBundle();
         $this->scripts = new AssetBundle();
@@ -68,19 +83,23 @@ class Page
     /**
      * Get the HTML page title.
      *
-     * @return string
+     * @return string Page title
+     *
+     * @since Release: v17 
      */
-    public function getTitle()
+    public function getTitle(): string
     {
         return $this->title;
     }
 
     /**
-     * Get the current page address (?q=)
+     * Get the current page address (?q=).
      *
-     * @return string
+     * @return string Current page address.
+     *
+     * @since Release: v17 
      */
-    public function getAddress()
+    public function getAddress(): string
     {
         return $this->address;
     }
@@ -88,7 +107,9 @@ class Page
     /**
      * Get the action instance for the current page.
      *
-     * @return Action
+     * @return Action The action instance for the current page.
+     *
+     * @since Release: v17 
      */
     public function getAction()
     {
@@ -98,9 +119,11 @@ class Page
     /**
      * Get the module instance for the current page.
      *
-     * @return Module
+     * @return Module The module instance for the page.
+     *
+     * @since Release: v17 
      */
-    public function getModule()
+    public function getModule(): Module
     {
         return $this->module;
     }
@@ -109,27 +132,39 @@ class Page
      * Get the theme instance for the current page.
      *
      * @return Theme
+     *
+     * @since Release: v17 
      */
-    public function getTheme()
+    public function getTheme(): Theme
     {
         return $this->theme;
     }
 
     /**
-     * Get an array of all stylesheet asset used by this page (system, module & theme).
+     * Get an array of all stylesheet asset used by this page (system,
+     * module & theme).
+     *
+     * @param string|null $context Optional filter by context.
+     *                             Either 'head' or 'foot'.
      *
      * @return array
+     *
+     * @since Release: v17 
      */
-    public function getAllStylesheets($context = null)
+    public function getAllStylesheets(?string $context = null): array
     {
         $stylesheets = $this->stylesheets()->getAssets($context);
 
         if (!empty($this->getModule())) {
-            $stylesheets = array_replace($stylesheets, $this->getModule()->stylesheets()->getAssets($context));
+            $stylesheets = array_replace(
+                $stylesheets, $this->getModule()->stylesheets()->getAssets($context)
+            );
         }
 
         if (!empty($this->getTheme())) {
-            $stylesheets = array_replace($stylesheets, $this->getTheme()->stylesheets()->getAssets($context));
+            $stylesheets = array_replace(
+                $stylesheets, $this->getTheme()->stylesheets()->getAssets($context)
+            );
         }
 
         return $stylesheets;
@@ -138,19 +173,29 @@ class Page
     /**
      * Get an array of all script assets used by this page (system, module & theme).
      *
-     * @param string $context  Optionally filter by context.
+     * @param string|null $context Optionally filter by context.
+     *                             Either 'head' or 'foot'.
+     *
      * @return array
+     *
+     * @since Release: v17 
      */
-    public function getAllScripts($context = null)
+    public function getAllScripts(?string $context = null): array
     {
         $scripts = $this->scripts()->getAssets($context);
 
         if (!empty($this->getModule())) {
-            $scripts = array_replace($scripts, $this->getModule()->scripts()->getAssets($context));
+            $scripts = array_replace(
+                $scripts,
+                $this->getModule()->scripts()->getAssets($context)
+            );
         }
 
         if (!empty($this->getTheme())) {
-            $scripts = array_replace($scripts, $this->getTheme()->scripts()->getAssets($context));
+            $scripts = array_replace(
+                $scripts,
+                $this->getTheme()->scripts()->getAssets($context)
+            );
         }
 
         return $scripts;
@@ -159,9 +204,13 @@ class Page
     /**
      * Add user feedback as an error message displayed on this page.
      *
-     * @param string $text
+     * @param string $text Error message text.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function addError($text)
+    public function addError(string $text): void
     {
         $this->alerts['error'][] = $text;
     }
@@ -169,9 +218,13 @@ class Page
     /**
      * Add user feedback as a warning message displayed on this page.
      *
-     * @param string $text
+     * @param string $text Warning message text.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function addWarning($text)
+    public function addWarning(string $text): void
     {
         $this->alerts['warning'][] = $text;
     }
@@ -179,9 +232,13 @@ class Page
     /**
      * Add user feedback as an info message displayed on this page.
      *
-     * @param string $text
+     * @param string $text General notice message text.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function addMessage($text)
+    public function addMessage(string $text): void
     {
         $this->alerts['message'][] = $text;
     }
@@ -189,23 +246,29 @@ class Page
     /**
      * Get all alerts generated by this page, optionally by context.
      *
-     * @param string $context  Contexts: error, warning, message, code
-     * @param string $text
+     * @param string $context Contexts: error, warning, message, code
+     *
      * @return array
+     *
+     * @since Release: v17 
      */
-    public function getAlerts($context = null)
+    public function getAlerts(?string $context = null)
     {
-        return !empty($context) 
-            ? $this->alerts[$context] 
-            : $this->alerts;
+        return !empty($context)
+        ? $this->alerts[$context]
+        : $this->alerts;
     }
 
     /**
      * Add a section of raw HTML to the HEAD tag.
      *
-     * @param string $code
+     * @param string $code Raw HTML code to render in the HEAD region.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function addHeadExtra($code)
+    public function addHeadExtra($code): void
     {
         $this->extra['head'][] = $code;
     }
@@ -213,7 +276,11 @@ class Page
     /**
      * Add a section of raw HTML to bottom of the BODY tag.
      *
-     * @param string $code
+     * @param string $code Raw HTML code to render at the bottom of BODY region.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
     public function addFootExtra($code)
     {
@@ -223,9 +290,13 @@ class Page
     /**
      * Add a section of raw HTML to the page sidebar.
      *
-     * @param string $code
+     * @param string $code Raw HTML code to render in the page sidebar region.
+     *
+     * @return void
+     *
+     * @since Release: v17 
      */
-    public function addSidebarExtra($code)
+    public function addSidebarExtra($code): void
     {
         $this->extra['sidebar'][] = $code;
     }
@@ -233,22 +304,27 @@ class Page
     /**
      * Get all raw HTML code sections by context.
      *
-     * @param string $context  Contexts: head, foot, sidebar
+     * @param string $context Contexts: head, foot, sidebar
+     *
      * @return array
+     *
+     * @since Release: v17 
      */
-    public function getExtraCode($context)
+    public function getExtraCode($context): array
     {
-        return !empty($context) 
-            ? $this->extra[$context] 
-            : $this->extra;
+        return !empty($context)
+        ? $this->extra[$context]
+        : $this->extra;
     }
 
     /**
      * Returns the collection of stylesheets used by this page.
      *
-     * @return AssetBundle
+     * @return AssetBundle Assetbundle of stylesheets.
+     *
+     * @since Release: v17 
      */
-    public function stylesheets()
+    public function stylesheets(): AssetBundle
     {
         return $this->stylesheets;
     }
@@ -256,9 +332,11 @@ class Page
     /**
      * Returns the collection of scripts used by this page.
      *
-     * @return AssetBundle
+     * @return AssetBundle Assetbundle of javascripts.
+     *
+     * @since Release: v17 
      */
-    public function scripts()
+    public function scripts(): AssetBundle
     {
         return $this->scripts;
     }
