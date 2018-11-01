775b3fcd7f813034781852d0f03187d9950b5ff2
Koala Yeung <koalay@gmail.com>
2018-11-01 16:58:06 +0800

    Composer: migrate phpmailer as composer vendor library
    * migrate the requirement of PHPMailer 5.2.23 to composer vendor
      folder and manage by autoloader.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,9 +7,9 @@ namespace Composer\Autoload;
 class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
 {
     public static $files = array (
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
@@ -108,12 +108,24 @@ class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601::$classMap;
 
         }, null, ClassLoader::class);
     }
