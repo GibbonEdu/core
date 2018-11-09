4e1600af97e893cc83d154d2d96aea932e588dc4
Sandra Kuipers <sandra@skuipers.com>
2018-11-09 11:33:30 +0800

    System: add a temporary autoloader mapping for external modules

--- gibbon.php
+++ gibbon.php
@@ -48,6 +48,9 @@ if (!$gibbon->isInstalled() && !$gibbon->isInstalling()) {
 if (!empty($gibbon->session->get('module'))) {
     $moduleNamespace = preg_replace('/[^a-zA-Z0-9]/', '', $gibbon->session->get('module'));
     $autoloader->addPsr4('Gibbon\\Module\\'.$moduleNamespace.'\\', realpath(__DIR__).'/modules/'.$gibbon->session->get('module').'/src');
+
+    // Temporary backwards-compatibility for external modules (Query Builder)
+    $autoloader->addPsr4('Gibbon\\'.$moduleNamespace.'\\', realpath(__DIR__).'/modules/'.$gibbon->session->get('module'));
     $autoloader->register(true);
 }
 
