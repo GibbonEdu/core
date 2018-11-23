574ebea007979573d8637c55c49e7e45f0a745f9
Sandra Kuipers <sandra@skuipers.com>
2018-11-23 15:35:24 +0800

    Forms: add an option to specify the label for SelectGradeScaleGrade

--- src/Forms/DatabaseFormFactory.php
+++ src/Forms/DatabaseFormFactory.php
@@ -461,9 +461,10 @@ class DatabaseFormFactory extends FormFactory
         ), $params);
 
         $valueQuery = ($params['valueMode'] == 'id')? 'gibbonScaleGradeID as value' : 'value';
+        $labelQuery = ($params['labelMode'] == 'descriptor')? 'descriptor' : 'value';
 
         $data = array('gibbonScaleID' => $gibbonScaleID);
-        $sql = "SELECT {$valueQuery}, value as name, isDefault FROM gibbonScaleGrade WHERE gibbonScaleID=:gibbonScaleID ORDER BY sequenceNumber";
+        $sql = "SELECT {$valueQuery}, {$labelQuery} as name, isDefault FROM gibbonScaleGrade WHERE gibbonScaleID=:gibbonScaleID ORDER BY sequenceNumber";
         $results = $this->pdo->executeQuery($data, $sql);
 
         $grades = ($results->rowCount() > 0)? $results->fetchAll() : array();
