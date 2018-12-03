9573732d7497c31dd6ff8b7051166246f22629db
Craig Rayner <crayner@users.noreply.github.com>
2018-12-03 16:35:41 +1100

    System: deprecated getGibbonMailer and call GibbonMailer wrapper to remove duplicate code. (#744)
    * Ignore Editor Configuration Directory
    
    * PHPMailer wrapper updated for use with GMAIL, which requires SMTPSecure = 'ssl'
    Deprecated getGibbonMailer and call GibbonMailer wrapper to remove duplicate code.
    Refactored Messenger Post Process to use GibbonMailer.
    
    * Ensure that display_errors is off before issuing deprecated error, and restore display_errors.
    
    * Update .gitignore to remove .idea

--- modules/Messenger/messenger_postProcess.php
+++ modules/Messenger/messenger_postProcess.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Comms\GibbonMailer;
+
 include '../../gibbon.php';
 
 //Increase max execution time, as this stuff gets big
@@ -1937,7 +1939,7 @@ else {
 
 				//Set up email
 				$emailCount=0 ;
-				$mail=getGibbonMailer($guid);
+				$mail= new GibbonMailer($gibbon->session);
 				$mail->SMTPKeepAlive = true;
 				if ($emailReplyTo!="")
 					$mail->AddReplyTo($emailReplyTo, '');
