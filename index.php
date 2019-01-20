ef741b9072b6a265948cc28c4e74328709e0af23
Sandra Kuipers <sandra@skuipers.com>
2019-01-20 21:17:35 +0800

    System: fixed index_custom.php content not loading on first login

--- index.php
+++ index.php
@@ -553,7 +553,9 @@ if (!$session->has('address')) {
             ];
 
             $session->set('index_custom.php', $page->fetchFromFile('./index_custom.php', $globals));
-        } elseif ($session->has('index_custom.php')) {
+        }
+        
+        if ($session->exists('index_custom.php')) {
             $page->write($session->get('index_custom.php'));
         }
 
