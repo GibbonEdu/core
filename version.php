4a8847b79dd9f84ad7c261d27bd51a587caf211f
Sandra Kuipers <sandra@skuipers.com>
2019-01-20 16:51:16 +0800

    System: started v18.0.00

--- version.php
+++ version.php
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$version = '17.0.00';
+$version = '18.0.00';
 
 /**
  * System Requirements
