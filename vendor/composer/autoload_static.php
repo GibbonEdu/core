c8d851668fa4efff3db27ee57ce70fc3890ddb5c
Sandra Kuipers <sandra@skuipers.com>
2019-02-01 14:47:50 +0800

    System: restore PHPExcel in composer for current dependencies

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -252,6 +252,13 @@ class ComposerStaticInit3046e8c42bde92b8c2d990c2f6dd9601
                 0 => __DIR__ . '/..' . '/twig/twig/lib',
             ),
         ),
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
         'G' => 
         array (
             'Google_Service_' => 
