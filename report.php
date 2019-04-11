c9e0beee0d694d05244ecdf5929b293bbf9fdc42
Sandra Kuipers <sandra@skuipers.com>
2019-04-11 20:06:21 +0800

    Templates: update the printable report page to use a template (#841)

--- report.php
+++ report.php
@@ -17,120 +17,68 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-//Gibbon system-wide includes
-include './gibbon.php';
-include './version.php';
+use Gibbon\Services\Format;
 
-//Check to see if system settings are set from databases
-if ($_SESSION[$guid]['systemSettingsSet'] == false) {
+// Gibbon system-wide include
+require_once './gibbon.php';
+
+// Setup the Page and Session objects
+$page = $container->get('page');
+$session = $container->get('session');
+
+// Check to see if system settings are set from databases
+if (!$session->has('systemSettingsSet')) {
     getSystemSettings($guid, $connection2);
 }
-//If still false, show warning, otherwise display page
-if ($_SESSION[$guid]['systemSettingsSet'] == false) {
-    echo __('System Settings are not set: the system cannot be displayed');
-} else {
-    ?>
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
-		<head>
-			<title><?php echo $_SESSION[$guid]['organisationNameShort'].' - '.$_SESSION[$guid]['systemName'] ?></title>
-			<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-			<meta http-equiv="content-language" content="en"/>
-			<meta name="author" content="Ross Parker, International College Hong Kong"/>
-			<meta name="robots" content="none"/>
 
-			<?php
-            //Set theme
-            $themeCSS = "<link rel='stylesheet' type='text/css' href='./themes/Default/css/main.css' />";
-			if ($_SESSION[$guid]['i18n']['rtl'] == 'Y') {
-				$themeCSS .= "<link rel='stylesheet' type='text/css' href='./themes/Default/css/main_rtl.css' />";
-			}
-			$themeJS = "<script type='text/javascript' src='./themes/Default/js/common.js'></script>";
-			$_SESSION[$guid]['gibbonThemeID'] = '001';
-			$_SESSION[$guid]['gibbonThemeName'] = 'Default';
-
-			if ($_SESSION[$guid]['gibbonThemeIDPersonal'] != null) {
-				$dataTheme = array('gibbonThemeIDPersonal' => $_SESSION[$guid]['gibbonThemeIDPersonal']);
-				$sqlTheme = 'SELECT * FROM gibbonTheme WHERE gibbonThemeID=:gibbonThemeIDPersonal';
-			} else {
-				$dataTheme = array();
-				$sqlTheme = "SELECT * FROM gibbonTheme WHERE active='Y'";
-			}
-			$resultTheme = $connection2->prepare($sqlTheme);
-			$resultTheme->execute($dataTheme);
-			if ($resultTheme->rowCount() == 1) {
-				$rowTheme = $resultTheme->fetch();
-				$themeCSS = "<link rel='stylesheet' type='text/css' href='./themes/".$rowTheme['name']."/css/main.css' />";
-				if ($_SESSION[$guid]['i18n']['rtl'] == 'Y') {
-					$themeCSS .= "<link rel='stylesheet' type='text/css' href='./themes/".$rowTheme['name']."/css/main_rtl.css' />";
-				}
-				$themeJS = "<script type='text/javascript' src='./themes/".$rowTheme['name']."/js/common.js'></script>";
-				$_SESSION[$guid]['gibbonThemeID'] = $rowTheme['gibbonThemeID'];
-				$_SESSION[$guid]['gibbonThemeName'] = $rowTheme['name'];
-			}
-			echo $themeCSS;
-			echo $themeJS;
-
-            //Set module CSS & JS
-            $moduleCSS = "<link rel='stylesheet' type='text/css' href='./modules/".$_SESSION[$guid]['module']."/css/module.css' />";
-			$moduleJS = "<script type='text/javascript' src='./modules/".$_SESSION[$guid]['module']."/js/module.js'></script>";
-			echo $moduleCSS;
-			echo $moduleJS;
-
-            
-   			?>
-
-			<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico"/>
-			<script type="text/javascript" src="./lib/LiveValidation/livevalidation_standalone.compressed.js"></script>
-
-			<?php
-            if ($_SESSION[$guid]['analytics'] != '') {
-                echo $_SESSION[$guid]['analytics'];
-            }
-   			 ?>
-		</head>
-		<body style="background: none" class="print">
-			<div id="wrap-report" style="width:750px">
-				<div id="header-report">
-					<div style="width:400px; font-size: 100%; float: right">
-						<?php
-                        echo "<div style='padding-top: 10px'>";
-						echo "<p style='margin-bottom: 0; padding-bottom: 0'>";
-						echo sprintf(__('This printout contains information that is the property of %1$s. If you find this report, and do not have permission to read it, please return it to %2$s (%3$s). In the event that it cannot be returned, please destroy it.'), $_SESSION[$guid]['organisationName'], $_SESSION[$guid]['organisationAdministratorName'], $_SESSION[$guid]['organisationAdministratorEmail']);
-						echo '</p>';
-						echo '</div>';
-						?>
-					</div>
-					<div id="header-logo-report" style="text-align: right">
-						<img height='75px' width='300px' alt="Logo" src="<?php echo $_SESSION[$guid]['absoluteURL'].'/'.$_SESSION[$guid]['organisationLogo'];?>"/>
-					</div>
-				</div>
-				<div id="content-wrap-report" style="min-height: 500px">
-
-					<?php
-                    $_SESSION[$guid]['address'] = $_GET['q'];
-					$_SESSION[$guid]['module'] = getModuleName($_SESSION[$guid]['address']);
-					$_SESSION[$guid]['action'] = getActionName($_SESSION[$guid]['address']);
-
-					if (strstr($_SESSION[$guid]['address'], '..') != false) {
-						echo "<div class='error'>";
-						echo __('Illegal address detected: access denied.');
-						echo '</div>';
-					} else {
-						if (is_file('./'.$_SESSION[$guid]['address'])) {
-							include './'.$_SESSION[$guid]['address'];
-						} else {
-							include './error.php';
-						}
-					}
-					?>
-				</div>
-				<div id="footer-report">
-					<?php echo sprintf(__('Created by %1$s (%2$s) at %3$s on %4$s.'), $_SESSION[$guid]['username'], $_SESSION[$guid]['organisationNameShort'], date('H:i'), date($_SESSION[$guid]['i18n']['dateFormatPHP']));?>
-				</div>
-			</div>
-		</body>
-	</html>
-	<?php
+// If still false, show warning, otherwise display page
+if (!$session->has('systemSettingsSet')) {
+    exit(__('System Settings are not set: the system cannot be displayed'));
 }
-?>
+
+$address = $page->getAddress();
+
+if (empty($address)) {
+    $page->addWarning(__('There is no content to display'));
+} elseif ($page->isAddressValid($address) == false) {
+    $page->addError(__('Illegal address detected: access denied.'));
+} else {
+    // Pass these globals into the script of the included file, for backwards compatibility.
+    // These will be removed when we begin the process of ooifying action pages.
+    $globals = [
+        'guid'        => $guid,
+        'gibbon'      => $gibbon,
+        'version'     => $version,
+        'pdo'         => $pdo,
+        'connection2' => $connection2,
+        'autoloader'  => $autoloader,
+        'container'   => $container,
+        'page'        => $page,
+    ];
+
+    if (is_file('./'.$address)) {
+        $page->writeFromFile('./'.$address, $globals);
+    } else {
+        $page->writeFromFile('./error.php', $globals);
+    }
+}
+
+$page->addHeadExtra($session->get('analytics'));
+$page->stylesheets->add('theme-dev', 'resources/assets/css/theme.min.css');
+$page->stylesheets->add('core', 'resources/assets/css/core.min.css', ['weight' => 10]);
+
+$page->addData([
+    'isLoggedIn'                     => $session->has('username') && $session->has('gibbonRoleIDCurrent'),
+    'username'                       => $session->get('username'),
+    'gibbonThemeName'                => $session->get('gibbonThemeName'),
+    'organisationName'               => $session->get('organisationName'),
+    'organisationNameShort'          => $session->get('organisationNameShort'),
+    'organisationAdministratorName'  => $session->get('organisationAdministratorName'),
+    'organisationAdministratorEmail' => $session->get('organisationAdministratorEmail'),
+    'organisationLogo'               => $session->get('organisationLogo'),
+    'time'                           => Format::time(date('H:i:s')),
+    'date'                           => Format::date(date('Y-m-d')),
+    'rightToLeft'                    => $session->get('i18n')['rtl'] == 'Y',
+]);
+
+echo $page->render('report.twig.html');
