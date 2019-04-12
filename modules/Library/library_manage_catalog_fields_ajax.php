45a745d37dd1745bfb9cfa61e26d9c0b9c490935
Sandra Kuipers <sandra@skuipers.com>
2019-04-12 09:07:22 +0800

    Library: update the layout of nested details table in library items

--- modules/Library/library_manage_catalog_fields_ajax.php
+++ modules/Library/library_manage_catalog_fields_ajax.php
@@ -71,9 +71,9 @@ if (isActionAccessible($guid, $connection2, '/modules/Library/library_manage_cat
             $fieldName = 'field'.preg_replace('/ |\(|\)/', '', $field['name']);
             $fieldValue = isset($fieldsValues[$field['name']])? $fieldsValues[$field['name']] : '';
 
-            $row = $table->addRow();
-                $row->addLabel($fieldName, $field['name'])->description($field['description']);
-                $row->addCustomField($fieldName, $field)->setValue($fieldValue);
+            $row = $table->addRow()->addClass('flex flex-col sm:flex-row justify-between content-center p-0');
+                $row->addLabel($fieldName, $field['name'])->description($field['description'])->addClass('flex-grow sm:mb-0 border-transparent border-t-0 sm:border-gray-400');
+                $row->addCustomField($fieldName, $field)->setValue($fieldValue)->addClass('w-full max-w-full sm:max-w-xs flex justify-end items-center border-0 sm:border-b');
         }
 
         // Add Google Books data grabber
