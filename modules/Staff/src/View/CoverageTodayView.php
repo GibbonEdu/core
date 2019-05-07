839e71fe251a6efdf49e8a2053d46f8a8fb2d006
Sandra Kuipers <sandra@skuipers.com>
2019-05-07 14:21:10 +0800

    Staff: add the My Coverage page

---
+++ modules/Staff/src/View/CoverageTodayView.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Module\Staff\View;
+
+use Gibbon\View\Page;
+use Gibbon\Services\Format;
+use Gibbon\Domain\User\UserGateway;
+use Gibbon\Domain\Staff\StaffAbsenceGateway;
+use Gibbon\Domain\Staff\StaffCoverageGateway;
+use Gibbon\Module\Staff\View\StaffCard;
+use Gibbon\Tables\DataTable;
+use Gibbon\Domain\RollGroups\RollGroupGateway;
+
+
+/**
+ * CoverageTodayView
+ *
+ * @version v18
+ * @since   v18
+ */
+class CoverageTodayView
+{
+    protected $staffCoverageGateway;
+    protected $rollGroupGateway;
+    protected $userGateway;
+    protected $gibbonStaffCoverageID;
+
+    public function __construct(StaffCoverageGateway $staffCoverageGateway, RollGroupGateway $rollGroupGateway, UserGateway $userGateway)
+    {
+        $this->staffCoverageGateway = $staffCoverageGateway;
+        $this->rollGroupGateway = $rollGroupGateway;
+        $this->userGateway = $userGateway;
+    }
+
+    public function setCoverage($gibbonStaffCoverageID)
+    {
+        $this->gibbonStaffCoverageID = $gibbonStaffCoverageID;
+
+        return $this;
+    }
+
+    public function compose(Page $page)
+    {
+        $coverage = $this->staffCoverageGateway->getByID($this->gibbonStaffCoverageID);
+
+        $page->write('<details class="border  bg-white rounded-b -mt-5 px-4">');
+        $page->write('<summary class="py-4 text-gray-700 text-sm cursor-pointer">'.__('View Details').'</summary>');
+
+        // Coverage Request
+        $requester = $this->userGateway->getByID($coverage['gibbonPersonIDStatus']);
+        $page->writeFromTemplate('statusComment.twig.html', [
+            'name'    => Format::name($requester['title'], $requester['preferredName'], $requester['surname'], 'Staff', false, true),
+            'action'   => __('Requested Coverage'),
+            'photo'   => $requester['image_240'],
+            'date'    => Format::relativeTime($coverage['timestampStatus']),
+            'comment' => $coverage['notesStatus'],
+        ]);
+
+        // Attachment
+        if (!empty($coverage['attachmentType'])) {
+            $page->writeFromTemplate('statusComment.twig.html', [
+                'name'       => __('Attachment'),
+                'icon'       => 'internalAssessment',
+                'tag'        => 'dull',
+                'status'     => __($coverage['attachmentType']),
+                'attachment' => $coverage['attachmentType'] != 'Text' ? Format::link($coverage['attachmentContent']) : '',
+                'html'       => $coverage['attachmentType'] == 'Text' ? $coverage['attachmentContent'] : '',
+            ]);
+        }
+
+        // Roll Group Info
+
+        $rollGroups = $this->rollGroupGateway->selectRollGroupsByTutor($coverage['gibbonPersonID'])->toDataSet();
+
+        if (count($rollGroups) > 0) {
+            $table = DataTable::create('todaysCoverageTimetable');
+
+            $table->addColumn('name', __('Roll Group'))->context('primary');
+            $table->addColumn('spaceName', __('Location'))->context('primary');
+
+            $table->addActionColumn()
+                ->addParam('gibbonRollGroupID')
+                ->format(function ($values, $actions) {
+                    if ($values['attendance'] == 'Y') {
+                        $actions->addAction('attendance', __('Take Attendance'))
+                            ->setIcon('attendance')
+                            ->setURL('/modules/Attendance/attendance_take_byRollGroup.php');
+                    }
+
+                    $actions->addAction('view', __('View Details'))
+                        ->setURL('/modules/Roll Groups/rollGroups_details.php');
+                });
+
+            $page->write($table->render($rollGroups).'<br/>');
+        }
+
+        // Timetable Info
+        $timetable = $this->staffCoverageGateway->selectTimetableRowsByCoverageDate($this->gibbonStaffCoverageID, date('Y-m-d'))->toDataSet();
+
+        if (count($timetable) > 0) {
+            $table = DataTable::create('todaysCoverageTimetable');
+
+            $table->addColumn('period', __('Period'));
+            $table->addColumn('time', __('Time'))->format(Format::using('timeRange', ['timeStart', 'timeEnd']))->context('primary');
+            $table->addColumn('class', __('Class'))->format(Format::using('courseClassName', ['courseNameShort', 'className']))->context('secondary');
+            $table->addColumn('spaceName', __('Location'))->context('primary');
+
+            $table->addActionColumn()
+                ->addParam('gibbonCourseClassID')
+                ->format(function ($values, $actions) {
+                    if ($values['attendance'] == 'Y') {
+                        $actions->addAction('attendance', __('Take Attendance'))
+                            ->setIcon('attendance')
+                            ->setURL('/modules/Attendance/attendance_take_byCourseClass.php');
+                    }
+
+                    $actions->addAction('view', __('View Details'))
+                        ->setURL('/modules/Departments/department_course_class.php');
+                });
+
+            $page->write($table->render($timetable).'<br/>');
+        }
+
+        $page->write('</details>');
+    }
+}
