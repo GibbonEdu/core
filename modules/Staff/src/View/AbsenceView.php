22eb06e05beef42a73bb0be6814e1b8e7b9c5aff
Sandra Kuipers <sandra@skuipers.com>
2019-05-07 13:22:59 +0800

    Staff: add a View Absence Details page and AbsenceView templates

---
+++ modules/Staff/src/View/AbsenceView.php
@@ -0,0 +1,113 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Gibbon\Module\Staff\View;
+
+use Gibbon\View\Page;
+use Gibbon\Services\Format;
+use Gibbon\Domain\User\UserGateway;
+use Gibbon\Domain\Staff\StaffAbsenceGateway;
+use Gibbon\Domain\Staff\StaffCoverageGateway;
+
+
+/**
+ * AbsenceView
+ *
+ * @version v18
+ * @since   v18
+ */
+class AbsenceView
+{
+    protected $staffAbsenceGateway;
+    protected $staffCoverageGateway;
+    protected $userGateway;
+    protected $coverageView;
+
+    public function __construct(StaffAbsenceGateway $staffAbsenceGateway, StaffCoverageGateway $staffCoverageGateway, UserGateway $userGateway, CoverageView $coverageView)
+    {
+        $this->staffAbsenceGateway = $staffAbsenceGateway;
+        $this->staffCoverageGateway = $staffCoverageGateway;
+        $this->userGateway = $userGateway;
+        $this->coverageView = $coverageView;
+    }
+
+    public function setAbsence($gibbonStaffAbsenceID, $gibbonPersonIDViewing)
+    {
+        $this->gibbonStaffAbsenceID = $gibbonStaffAbsenceID;
+        $this->gibbonPersonIDViewing = $gibbonPersonIDViewing;
+
+        return $this;
+    }
+
+    public function compose(Page $page)
+    {
+        $absence = $this->staffAbsenceGateway->getAbsenceDetailsByID($this->gibbonStaffAbsenceID);
+        if (empty($absence)) return;
+
+        $person = $this->userGateway->getByID($absence['gibbonPersonIDCreator']);
+        $canViewConfidential = $absence['gibbonPersonIDApproval'] == $this->gibbonPersonIDViewing || $absence['gibbonPersonID'] == $this->gibbonPersonIDViewing;
+        
+        // Absence Details
+        $page->writeFromTemplate('statusComment.twig.html', [
+            'name'    => Format::name($person['title'], $person['preferredName'], $person['surname'], 'Staff', false, true),
+            'action'   => !empty($absence['gibbonPersonIDApproval'])? __('Requested Leave') : __('Submitted Leave'),
+            'photo'   => $person['image_240'],
+            'date'    => Format::relativeTime($absence['timestampCreator']),
+            'comment' => $absence['comment'],
+            'message' => $canViewConfidential && !empty($absence['commentConfidential']) ? __('Confidential Comment').': '.$absence['commentConfidential'] : '',
+        ]);
+
+        // Approval Details
+        if (!empty($absence['gibbonPersonIDApproval'])) {
+            $approver = $this->userGateway->getByID($absence['gibbonPersonIDApproval']);
+            $page->writeFromTemplate('statusComment.twig.html', [
+                'name'    => Format::name($approver['title'], $approver['preferredName'], $approver['surname'], 'Staff', false, true),
+                'action'  => $absence['status'] != 'Pending Approval' ? __($absence['status']) : '',
+                'photo'   => $approver['image_240'],
+                'date'    => Format::relativeTime($absence['timestampApproval']),
+                'status'  => __($absence['status']),
+                'tag'     => $this->getStatusColor($absence['status']),
+                'comment' => $canViewConfidential ? $absence['notesApproval'] : '',
+            ]);
+        }
+
+        $coverageList = $this->staffCoverageGateway->selectCoverageByAbsenceID($absence['gibbonStaffAbsenceID'])->fetchAll();
+        
+        // Coverage Details
+        if (!empty($coverageList)) {
+            foreach ($coverageList as $coverage) {
+                $this->coverageView->setCoverage($coverage['gibbonStaffCoverageID'])->compose($page);
+            }
+        }
+    }
+
+    protected function getStatusColor($status)
+    {
+        switch ($status) {
+            case 'Approved':
+                return 'success';
+
+            case 'Declined':
+                return 'error';
+
+            default:
+                return 'message';
+        }
+    }
+}
