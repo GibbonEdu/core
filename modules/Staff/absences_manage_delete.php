9914ac5ca2e935a1ac29c25b0d26c841c53aa4c1
Sandra Kuipers <sandra@skuipers.com>
2019-05-07 14:06:23 +0800

    Staff: add Manage Staff Absences page

---
+++ modules/Staff/absences_manage_delete.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Forms\Prefab\DeleteForm;
+use Gibbon\Domain\Staff\StaffAbsenceGateway;
+
+if (isActionAccessible($guid, $connection2, '/modules/Staff/absences_manage_delete.php') == false) {
+    // Access denied
+    $page->addError(__('You do not have access to this action.'));
+} else {
+    //Proceed!
+    $gibbonStaffAbsenceID = $_GET['gibbonStaffAbsenceID'] ?? '';
+    $staffAbsenceGateway = $container->get(StaffAbsenceGateway::class);
+    
+    if (empty($gibbonStaffAbsenceID)) {
+        $page->addError(__('You have not specified one or more required parameters.'));
+        return;
+    }
+
+    $values = $staffAbsenceGateway->getByID($gibbonStaffAbsenceID);
+
+    if (empty($gibbonStaffAbsenceID) || empty($values)) {
+        $page->addError(__('The specified record cannot be found.'));
+        return;
+    }
+
+    $form = DeleteForm::createForm($_SESSION[$guid]['absoluteURL'].'/modules/Staff/absences_manage_deleteProcess.php?gibbonStaffAbsenceID='.$gibbonStaffAbsenceID, true);
+    echo $form->getOutput();
+}
---
+++ modules/Staff/absences_manage_edit_edit.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Forms\Form;
+use Gibbon\Forms\DatabaseFormFactory;
+use Gibbon\Services\Format;
+use Gibbon\Domain\Staff\StaffAbsenceDateGateway;
+
+if (isActionAccessible($guid, $connection2, '/modules/Staff/absences_manage_edit.php') == false) {
+    // Access denied
+    $page->addError(__('You do not have access to this action.'));
+} else {
+    $gibbonStaffAbsenceID = $_GET['gibbonStaffAbsenceID'] ?? '';
+    $gibbonStaffAbsenceDateID = $_GET['gibbonStaffAbsenceDateID'] ?? '';
+
+    //Proceed!
+    $page->breadcrumbs
+        ->add(__('Manage Staff Absences'), 'absences_manage.php')
+        ->add(__('Edit Absence'), 'absences_manage_edit.php', ['gibbonStaffAbsenceID' => $gibbonStaffAbsenceID])
+        ->add(__('Edit'));
+
+    if (isset($_GET['return'])) {
+        returnProcess($guid, $_GET['return'], null, null);
+    }
+
+    if (empty($gibbonStaffAbsenceID) || empty($gibbonStaffAbsenceDateID)) {
+        $page->addError(__('You have not specified one or more required parameters.'));
+        return;
+    }
+
+    $staffAbsenceDateGateway = $container->get(StaffAbsenceDateGateway::class);
+    $values = $staffAbsenceDateGateway->getByID($gibbonStaffAbsenceDateID);
+
+    if (empty($values)) {
+        $page->addError(__('The specified record cannot be found.'));
+        return;
+    }
+
+    $form = Form::create('staffAbsenceEdit', $_SESSION[$guid]['absoluteURL'].'/modules/Staff/absences_manage_edit_editProcess.php');
+
+    $form->setFactory(DatabaseFormFactory::create($pdo));
+    $form->addHiddenValue('address', $_SESSION[$guid]['address']);
+    $form->addHiddenValue('gibbonStaffAbsenceID', $gibbonStaffAbsenceID);
+    $form->addHiddenValue('gibbonStaffAbsenceDateID', $gibbonStaffAbsenceDateID);
+   
+    $row = $form->addRow();
+        $row->addLabel('dateLabel', __('Date'));
+        $row->addTextField('dateLabel')->readonly()->setValue(Format::date($values['date']));
+
+    $row = $form->addRow();
+        $row->addLabel('allDay', __('When'));
+        $row->addCheckbox('allDay')
+            ->description(__('All Day'))
+            ->inline()
+            ->setClass()
+            ->setValue('Y')
+            ->checked($values['allDay'])
+            ->wrap('<div class="standardWidth floatRight">', '</div>');
+
+    $form->toggleVisibilityByClass('timeOptions')->onCheckbox('allDay')->whenNot('Y');
+
+    $row = $form->addRow()->addClass('timeOptions');
+        $row->addLabel('time', __('Time'));
+        $col = $row->addColumn('time')->addClass('right inline');
+        $col->addTime('timeStart')
+            ->setClass('shortWidth')
+            ->isRequired();
+        $col->addTime('timeEnd')
+            ->chainedTo('timeStart', false)
+            ->setClass('shortWidth')
+            ->isRequired();
+
+    $row = $form->addRow();
+        $row->addLabel('value', __('Value'));
+        $row->addNumber('value')->decimalPlaces(1)->maxLength(3)->minimum(0)->maximum(1);
+
+    $row = $form->addRow();
+        $row->addFooter();
+        $row->addSubmit();
+
+    $form->loadAllValuesFrom($values);
+
+    echo $form->getOutput();
+}
