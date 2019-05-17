6e002aa034a24a91a2bd7d42bee6c08b357fff51
Sandra Kuipers <sandra@skuipers.com>
2019-05-17 09:25:19 +0800

    Staff: add Manage Staff Coverage page (#881)
    * Staff: add Manage Staff Coverage page
    
    * Staff: fix changes to selectBy method in staff coverage
    
    * Staff: prevent adding coverage manually with no dates selected
    
    * Staff: tidy up some staff coverage scripts, add comments

---
+++ modules/Staff/coverage_manage_deleteProcess.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Gibbon, Flexible & Open School System
+Copyright (C) 2010, Ross Parker
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Domain\Staff\StaffCoverageGateway;
+use Gibbon\Domain\Staff\StaffCoverageDateGateway;
+
+require_once '../../gibbon.php';
+
+$gibbonStaffCoverageID = $_GET['gibbonStaffCoverageID'] ?? '';
+$search = $_POST['search'] ?? '';
+
+$URL = $_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/Staff/coverage_manage.php&search='.$search;
+
+if (isActionAccessible($guid, $connection2, '/modules/Staff/coverage_manage_delete.php') == false) {
+    $URL .= '&return=error0';
+    header("Location: {$URL}");
+} elseif (empty($gibbonStaffCoverageID)) {
+    $URL .= '&return=error1';
+    header("Location: {$URL}");
+    exit;
+} else {
+    // Proceed!
+    $staffCoverageGateway = $container->get(StaffCoverageGateway::class);
+    $staffCoverageDateGateway = $container->get(StaffCoverageDateGateway::class);
+    $values = $staffCoverageGateway->getByID($gibbonStaffCoverageID);
+
+    if (empty($values)) {
+        $URL .= '&return=error2';
+        header("Location: {$URL}");
+        exit;
+    }
+
+    $coverageDates = $staffCoverageDateGateway->selectDatesByCoverage($gibbonStaffCoverageID)->fetchAll();
+    $partialFail = false;
+
+    // Delete each date first
+    foreach ($coverageDates as $date) {
+        $partialFail &= !$staffCoverageDateGateway->delete($date['gibbonStaffCoverageDateID']);
+    }
+
+    // Then delete the coverage itself
+    $partialFail &= $staffCoverageGateway->delete($gibbonStaffCoverageID);
+
+    $URL .= $partialFail
+        ? '&return=warning1'
+        : '&return=success0';
+
+    header("Location: {$URL}");
+}
