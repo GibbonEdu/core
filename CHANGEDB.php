8a79a5031149069809ff6de2c86c08e0a3c0d9c2
Sandra Kuipers <sandra@skuipers.com>
2019-08-20 11:00:37 +0800

    v18.0.01

--- CHANGEDB.php
+++ CHANGEDB.php
@@ -904,7 +904,7 @@ UPDATE `gibbonAction` SET category='User Settings' WHERE name='Data Updater Sett
 UPDATE `gibbonAction` SET name='Manage Custom Fields' WHERE name='Manage User Custom Fields' AND gibbonModuleID=(SELECT gibbonModuleID FROM gibbonModule WHERE name='User Admin');end
 ";
 
-//v19.0.00
+//v18.0.01
 ++$count;
-$sql[$count][0] = '19.0.00';
+$sql[$count][0] = '18.0.01';
 $sql[$count][1] = "";
