da973a21fdc86c71739d172e54bc42eaee20a398
Sandra Kuipers <sandra@skuipers.com>
2019-08-20 11:00:37 +0800

    v18.0.01

--- version.php
+++ version.php
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$version = '19.0.00';
+$version = '18.0.01';
 
 /**
  * System Requirements
