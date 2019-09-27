53284c41ba7d1d0b24162ff8991a9a12a0bf5c33
Ross Parker <rossdotparker@gmail.com>
2019-09-27 21:38:00 +0800

    Students: post-merge report tweaks to facilitate additional module integration tweak

--- modules/Students/report_student_medicalSummary.php
+++ modules/Students/report_student_medicalSummary.php
@@ -38,7 +38,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/report_student_me
     $viewMode = $_REQUEST['format'] ?? '';
     $choices = $_POST['gibbonPersonID'] ?? [];
     //If $choices is blank, check to see if session is being used to inject gibbonPersonID list
-    if (count($choices) == 0) {
+    if (count($choices) == 0 && !empty($_SESSION[$guid]['report_student_medicalSummary.php_choices'])) {
         $choices = $_SESSION[$guid]['report_student_medicalSummary.php_choices'];
     }
     $gibbonSchoolYearID = $gibbon->session->get('gibbonSchoolYearID');
