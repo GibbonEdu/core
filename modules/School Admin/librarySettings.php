d60431ab9aeede2d1a428efbb3ba421a08c847cb
Ross Parker <rossdotparker@gmail.com>
2019-11-12 20:14:33 +0800

    School Admin: fixed Browse Library BG Image field length bug

--- modules/School Admin/librarySettings.php
+++ modules/School Admin/librarySettings.php
@@ -51,7 +51,7 @@ if (isActionAccessible($guid, $connection2, '/modules/School Admin/activitySetti
     $setting = getSettingByScope($connection2, 'Library', 'browseBGImage', true);
     $row = $form->addRow();
         $row->addLabel($setting['name'], __($setting['nameDisplay']))->description(__($setting['description']));
-        $row->addTextField($setting['name'])->setValue($setting['value'])->maxLength(6);
+        $row->addTextField($setting['name'])->setValue($setting['value'])->maxLength(255);
 
     $row = $form->addRow();
         $row->addFooter();
