22b7174f8a63857522bcb92582a469a498346100
leonardofreitas <wwwlfticombr@gmail.com>
2020-04-11 18:07:04 -0300

    applicationForm - staff and students

--- modules/Staff/applicationForm_manage_edit.php
+++ modules/Staff/applicationForm_manage_edit.php
@@ -178,7 +178,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Staff/applicationForm_mana
 
                 $row = $form->addRow();
                     $row->addLabel('officialName', __('Official Name'))->description(__('Full name as shown in ID documents.'));
-                    $row->addTextField('officialName')->required()->maxLength(150)->setTitle('Please enter full name as shown in ID documents');
+                    $row->addTextField('officialName')->required()->maxLength(150)->setTitle(__('Please enter full name as shown in ID documents'));
 
                 $row = $form->addRow();
                     $row->addLabel('nameInCharacters', __('Name In Characters'))->description(__('Chinese or other character-based name.'));
@@ -219,7 +219,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Staff/applicationForm_mana
                         $row->addSelectCountry('citizenship1')->required();
                     }
 
-                $countryName = (isset($_SESSION[$guid]['country']))? $_SESSION[$guid]['country'].' ' : '';
+                $countryName = (isset($_SESSION[$guid]['country']))? __($_SESSION[$guid]['country']).' ' : '';
                 $row = $form->addRow();
                     $row->addLabel('citizenship1Passport', __('Citizenship Passport Number'))->description('');
                     $row->addTextField('citizenship1Passport')->maxLength(30);
