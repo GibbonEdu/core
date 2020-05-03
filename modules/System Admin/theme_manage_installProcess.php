a8fb9750f809d87fdae1eb2ff4f3efd8c01d1175
Leonardo Freitas da Silva Pereira <leonardofreitas.lfti@gmail.com>
2020-05-02 21:55:55 -0300

    System Admin: Theme OOification (#1030)
    * OOification manage theme
    
    * Update version number of ThemeGateway
    
    * fixed comment
    
    Co-authored-by: leonardofreitas <wwwlfticombr@gmail.com>

--- modules/System Admin/theme_manage_installProcess.php
+++ modules/System Admin/theme_manage_installProcess.php
@@ -17,6 +17,8 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Domain\System\ThemeGateway;
+
 include '../../gibbon.php';
 
 //Get URL from calling page, and set returning URL
@@ -26,12 +28,9 @@ if (isActionAccessible($guid, $connection2, '/modules/System Admin/theme_manage_
     $URL .= '&return=error0';
     header("Location: {$URL}");
 } else {
-    $themeName = null;
-    if (isset($_GET['name'])) {
-        $themeName = $_GET['name'];
-    }
+    $themeName = $_GET['name'] ?? '';
 
-    if ($themeName == null or $themeName == '') {
+    if ($themeName == '') {
         $URL .= '&return=error1';
         header("Location: {$URL}");
     } else {
@@ -44,34 +43,20 @@ if (isActionAccessible($guid, $connection2, '/modules/System Admin/theme_manage_
                 $URL .= '&return=error1';
                 header("Location: {$URL}");
             } else {
+                $themeGateway = $container->get(ThemeGateway::class);
                 //Check for existence of theme
-                try {
-                    $dataModule = array('name' => $name);
-                    $sqlModule = 'SELECT * FROM gibbonTheme WHERE name=:name';
-                    $resultModule = $connection2->prepare($sqlModule);
-                    $resultModule->execute($dataModule);
-                } catch (PDOException $e) {
-                    $URL .= '&return=error2';
-                    header("Location: {$URL}");
-                    exit();
-                }
+                $data = array('name' => $name);
+                $theme = $themeGateway->selectBy($data)->rowCount();
 
-                if ($resultModule->rowCount() > 0) {
+                if ($theme > 0) {
                     $URL .= '&return=error3';
                     header("Location: {$URL}");
                 } else {
                     //Insert new theme row
-                    try {
-                        $dataModule = array('name' => $name, 'description' => $description, 'version' => $version, 'author' => $author, 'url' => $url);
-                        $sqlModule = "INSERT INTO gibbonTheme SET name=:name, description=:description, active='N', version=:version, author=:author, url=:url";
-                        $resultModule = $connection2->prepare($sqlModule);
-                        $resultModule->execute($dataModule);
-                    } catch (PDOException $e) {
-                        $URL .= '&return=error2';
-                        header("Location: {$URL}");
-                        exit();
-                    }
+                    $dataTheme = array('name' => $name, 'description' => $description, 'version' => $version, 'author' => $author, 'url' => $url);
+                    $themeGateway->insert($dataTheme);
 
+                    //Success 1
                     $URL .= '&return=success1';
                     header("Location: {$URL}");
                 }
