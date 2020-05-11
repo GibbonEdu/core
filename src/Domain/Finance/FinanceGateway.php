d2a6a0380bbf54bf50bc8ca79f7481b7e7f6f626
Jim Speir <jimbo80982@gmail.com>
2020-05-11 17:03:36 +0100

    OOifying budgetCycles and adding adding a gateway for the module

---
+++ src/Domain/Finance/FinanceGateway.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Gibbon\Domain\Finance;
+
+use Gibbon\Domain\QueryableGateway;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\DataSet;
+
+class FinanceGateway extends QueryableGateway
+{
+    use TableAware;
+    private static $primaryKey = 'gibbonFinanceBudgetID';
+    private static $tableName = 'gibbonFinanceBudget';
+    private static $searchableColumns = [];
+
+    public function queryFinanceCycles(QueryCriteria $criteria)
+    {
+
+       $query = $this
+            ->newQuery()
+            ->from('gibbonFinanceBudgetCycle')
+            ->orderBy(
+                [
+                'sequenceNumber DESC'
+                ]
+            )
+            ->cols(
+              [
+                'gibbonFinanceBudgetCycleID',
+                'name',
+                'status',
+                'dateStart',
+                'dateEnd',
+                'sequenceNumber',
+                "IF(dateStart > CURRENT_TIMESTAMP(),'Y','N') as inFuture",
+                "IF(dateEnd < CURRENT_TIMESTAMP(),'Y','N') as inPast"
+              ]
+            );
+
+        $criteria->addFilterRules(
+          [
+            'status' => function($query,$status){
+              return $query
+                ->where('gibbonFinanceBudgetCycle.status = :status')
+                ->bindValue('status',$status);
+            },
+            'inPast' => function($query,$inPast) {
+              return $query
+                ->where('inPast = :inPast')
+                ->bindValue('inPast',$inPast);
+            },
+            'inFuture' => function($query,$inFuture) {
+              return $query
+                ->where('inFuture = :inFuture')
+                ->bindValue('inFuture',$inFuture);
+            }
+          ]
+        );
+
+        return $this->runQuery($query, $criteria);
+    }
+
+}
+
+?>
