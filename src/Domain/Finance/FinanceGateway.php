36ca58af080392ff73bdf4d8033b9e560a0985ff
Jim Speir <jimbo80982@gmail.com>
2020-05-29 13:57:43 +0100

    Finance: oofified fees_manage.php (#1041)
    * - OOified fees_manage.php DataTable
    - Added a gateway for Finance
    - Slightly amended display preference on the DataTable so that active
    fees are higher than inactive ones.
    - Using Format::currency for currency formatting instead of inline
    statement.
    - Using addTitle to provide a title for the form on the page and the
    DataTable.
    - Using expandable column for description instead of a JS script.
    - PSR2 compliant
    
    * Added displayLabel(), moved string into description()
    
    Co-authored-by: Sandra Kuipers <sandra@skuipers.com>

--- src/Domain/Finance/FinanceGateway.php
+++ src/Domain/Finance/FinanceGateway.php
@@ -14,6 +14,56 @@ class FinanceGateway extends QueryableGateway
     private static $tableName = 'gibbonFinanceBudget';
     private static $searchableColumns = [];
 
+  public function queryFees(QueryCriteria $criteria)
+    {
+
+        $query = $this
+            ->newQuery()
+            ->from('gibbonFinanceFee')
+            ->join('left', 'gibbonFinanceFeeCategory', 'gibbonFinanceFee.gibbonFinanceFeeCategoryID = gibbonFinanceFeeCategory.gibbonFinanceFeeCategoryID')
+            ->orderBy(
+                [
+                'gibbonFinanceFee.active',
+                'gibbonFinanceFee.name'
+                  ]
+            )
+            ->cols(
+                [
+                'gibbonFinanceFee.name',
+                'gibbonFinanceFee.nameShort',
+                'gibbonFinanceFeeCategory.name as category',
+                'gibbonFinanceFee.fee',
+                'gibbonFinanceFee.gibbonFinanceFeeID',
+                'gibbonFinanceFee.gibbonSchoolYearID',
+                'gibbonFinanceFee.active',
+                'gibbonFinanceFee.description'
+                ]
+          );
+    
+          $criteria->addFilterRules(
+            [
+               'status' => function ($query, $status) {
+                return $query
+                ->where('gibbonFinanceFee.active = :status')
+                ->bindValue('status', $status);
+            },
+            'search' => function ($query, $search) {
+                return $query
+                ->where('(gibbonFinanceFee.name LIKE :search OR gibbonFinanceFeeCategory.name LIKE :search)')
+                ->bindValue('search', '%'.$search.'%');
+            },
+            'gibbonSchoolYearID' => function ($query, $gibbonSchoolYearID) {
+                return $query
+                ->where('gibbonFinanceFee.gibbonSchoolYearID = :gibbonSchoolYearID')
+                ->bindValue('gibbonSchoolYearID', $gibbonSchoolYearID);
+            }
+            ]
+        );
+    
+      return $this->runQuery($query, $criteria);
+    }  
+    
+                  
    public function queryFinanceBudget(QueryCriteria $criteria)
     {
         $query = $this
