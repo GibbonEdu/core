a82dd45e924aef5d39007b200d8f4eaf606d9f17
Jim Speir <jimbo80982@gmail.com>
2020-05-29 13:17:55 +0100

    Finance: OOifying Budget Cycles (#1039)
    * OOifying budgetCycles and adding adding a gateway for the module
    
    * Reversing the sequenceNumber ordering
    
    * Now complying to PSR-2
    
    * Tidied up unused try/catch block
    
    Co-authored-by: Sandra Kuipers <sandra@skuipers.com>

--- modules/Finance/budgetCycles_manage.php
+++ modules/Finance/budgetCycles_manage.php
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Domain\Finance\FinanceGateway;
+use Gibbon\Tables\DataTable;
+use Gibbon\Services\Format;
+
 if (isActionAccessible($guid, $connection2, '/modules/Finance/budgetCycles_manage.php') == false) {
     //Acess denied
     echo "<div class='error'>";
@@ -30,76 +34,27 @@ if (isActionAccessible($guid, $connection2, '/modules/Finance/budgetCycles_manag
         returnProcess($guid, $_GET['return']);
     }
 
-    try {
-        $data = array();
-        $sql = 'SELECT * FROM gibbonFinanceBudgetCycle ORDER BY sequenceNumber';
-        $result = $connection2->prepare($sql);
-        $result->execute($data);
-    } catch (PDOException $e) {
-        echo "<div class='error'>".$e->getMessage().'</div>';
-    }
-
-    echo "<div class='linkTop'>";
-    echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module']."/budgetCycles_manage_add.php'>".__('Add')."<img style='margin-left: 5px' title='".__('Add')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/page_new.png'/></a>";
-    echo '</div>';
-
-    if ($result->rowcount() < 1) {
-        echo "<div class='error'>";
-        echo __('There are no records to display.');
-        echo '</div>';
-    } else {
-        echo "<table cellspacing='0' style='width: 100%'>";
-        echo "<tr class='head'>";
-        echo '<th>';
-        echo __('Sequence');
-        echo '</th>';
-        echo '<th>';
-        echo __('Name');
-        echo '</th>';
-        echo '<th>';
-        echo __('Dates');
-        echo '</th>';
-        echo '<th>';
-        echo __('Status');
-        echo '</th>';
-        echo '<th>';
-        echo __('Actions');
-        echo '</th>';
-        echo '</tr>';
-
-        $count = 0;
-        $rowNum = 'odd';
-        while ($row = $result->fetch()) {
-            if ($count % 2 == 0) {
-                $rowNum = 'even';
-            } else {
-                $rowNum = 'odd';
-            }
-
-            //COLOR ROW BY STATUS!
-            echo "<tr class=$rowNum>";
-            echo '<td>';
-            echo $row['sequenceNumber'];
-            echo '</td>';
-            echo '<td>';
-            echo $row['name'];
-            echo '</td>';
-            echo '<td>';
-            if ($row['dateStart'] != null and $row['dateEnd'] != null) {
-                echo dateConvertBack($guid, $row['dateStart']).' - '.dateConvertBack($guid, $row['dateEnd']);
-            }
-            echo '</td>';
-            echo '<td>';
-            echo $row['status'];
-            echo '</td>';
-            echo '<td>';
-            echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/budgetCycles_manage_edit.php&gibbonFinanceBudgetCycleID='.$row['gibbonFinanceBudgetCycleID']."'><img title='".__('Edit')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/config.png'/></a> ";
-            echo "<a class='thickbox' href='".$_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/budgetCycles_manage_delete.php&gibbonFinanceBudgetCycleID='.$row['gibbonFinanceBudgetCycleID']."&width=650&height=135'><img title='".__('Delete')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/garbage.png'/></a> ";
-            echo '</td>';
-            echo '</tr>';
-
-            ++$count;
-        }
-        echo '</table>';
-    }
+    $gateway = $container->get(FinanceGateway::class);
+    $criteria = $gateway->newQueryCriteria(true)
+                        ->fromPOST();
+    $budgetCycles = $gateway->queryFinanceCycles($criteria);
+    $table = DataTable::createPaginated('cycles', $criteria);
+    $table->addHeaderAction('add', __('Add'))
+      ->setURL('/modules/Finance/budgetCycles_manage_add.php');
+    $table->addColumn('sequenceNumber', __('Sequence'));
+    $table->addColumn('name', __('Name'));
+    $table->addColumn('dates', __('Dates'))
+          ->format(function ($cycle) {
+            return Format::dateRange($cycle['dateStart'], $cycle['dateEnd']);
+          });
+    $table->addColumn('status', __('Status'));
+    $actions = $table->addActionColumn()
+        ->addParam('gibbonFinanceBudgetCycleID')
+                     ->format(function ($cycle, $actions) {
+                        $actions->addAction('edit', __('Edit'))
+                               ->setURL('/modules/Finance/budgetCycles_manage_edit.php');
+                        $actions->addAction('delete', __('Delete'))
+                          ->setURL('/modules/Finance/budgetCycles_manage_delete.php');
+                     });
+    echo $table->render($budgetCycles);
 }
--- src/Domain/Finance/FinanceGateway.php
+++ src/Domain/Finance/FinanceGateway.php
@@ -14,20 +14,64 @@ class FinanceGateway extends QueryableGateway
     private static $tableName = 'gibbonFinanceBudget';
     private static $searchableColumns = [];
 
+
+    public function queryFinanceCycles(QueryCriteria $criteria)
+    {
+
+        $query = $this
+            ->newQuery()
+            ->from('gibbonFinanceBudgetCycle')
+            ->cols(
+                [
+                'gibbonFinanceBudgetCycleID',
+                'name',
+                'status',
+                'dateStart',
+                'dateEnd',
+                'sequenceNumber',
+                "IF(dateStart > CURRENT_TIMESTAMP(),'Y','N') as inFuture",
+                "IF(dateEnd < CURRENT_TIMESTAMP(),'Y','N') as inPast"
+                ]
+            );
+
+        $criteria->addFilterRules(
+            [
+            'status' => function ($query, $status) {
+                return $query
+                ->where('gibbonFinanceBudgetCycle.status = :status')
+                ->bindValue('status', $status);
+            },
+            'inPast' => function ($query, $inPast) {
+                return $query
+                ->where('inPast = :inPast')
+                ->bindValue('inPast', $inPast);
+            },
+            'inFuture' => function ($query, $inFuture) {
+                return $query
+                ->where('inFuture = :inFuture')
+                ->bindValue('inFuture', $inFuture);
+            }
+            ]
+        );
+      return $this->runQuery($query, $criteria);
+    }
+
+
     public function queryExpenseApprovers(QueryCriteria $criteria)
     {
         $query = $this
         ->newQuery()
-        ->from('gibbonFinanceExpenseApprover')
-        ->innerJoin('gibbonPerson', 'gibbonPerson.gibbonPersonID = gibbonFinanceExpenseApprover.gibbonPersonID')
-        ->where("gibbonPerson.status = 'Full'")
         ->cols([
           'gibbonPerson.title',
           'gibbonPerson.preferredName',
           'gibbonPerson.surname',
           'gibbonFinanceExpenseApprover.sequenceNumber',
           'gibbonFinanceExpenseApprover.gibbonFinanceExpenseApproverID'
-        ]);
+        ])
+        ->from('gibbonFinanceExpenseApprover')
+        ->innerJoin('gibbonPerson', 'gibbonPerson.gibbonPersonID = gibbonFinanceExpenseApprover.gibbonPersonID')
+        ->where("gibbonPerson.status = 'Full'");
+        
         return $this->runQuery($query, $criteria);
     }
 }
