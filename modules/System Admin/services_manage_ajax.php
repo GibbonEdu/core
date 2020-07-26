6726038d94246e40763f69e634729b2133886ad4
Sandra Kuipers <sandra@skuipers.com>
2020-07-26 19:56:57 +0800

    System Admin: post-merge tweaks to System Overview & Manage Services

--- modules/System Admin/services_manage_ajax.php
+++ modules/System Admin/services_manage_ajax.php
@@ -50,12 +50,12 @@ if (isActionAccessible($guid, $connection2, '/modules/System Admin/services_mana
     $result = json_decode($response->getBody(), true);
 
     if ($response->getStatusCode() != 200) {
-        echo Format::alert('Failed to connect to gibbonedu.com server.').' '.__('Please contact support@gibbonedu.com if the problem persists.');
+        echo Format::alert(__('Failed to connect to gibbonedu.com server.').' '.__('Please contact support@gibbonedu.com if the problem persists.'));
         return;
     }
 
     if ($result['access'] == false) {
-        echo Format::alert('Failed to authenticate service key. Check that your organization name and key are entered correctly.').' '.__('Please contact support@gibbonedu.com if the problem persists.');
+        echo Format::alert(__('Failed to authenticate service key. Check that your organization name and key are entered correctly.').' '.__('Please contact support@gibbonedu.com if the problem persists.'));
         return;
     }
 
