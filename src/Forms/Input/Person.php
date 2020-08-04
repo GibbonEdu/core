6ac1012ab211d50609e52fd577ae5b3afc55edd3
Sandra Kuipers <sandra@skuipers.com>
2020-08-04 12:52:48 +0800

    Forms: fix user select overlapping labels

--- src/Forms/Input/Person.php
+++ src/Forms/Input/Person.php
@@ -46,10 +46,10 @@ class Person extends Select
         $this->addClass('personSelect');
 
         $output = '';
-        $output .= '<div class="w-full flex justify-end items-center pl-24 lg:pl-0">';
+        $output .= '<div class="flex-1 flex justify-end items-center pl-24 lg:pl-0">';
         if ($this->displayPhoto) {
 
-            $output .= '<div id="'.$this->getID().'Photo" class="flex-none relative w-20 h-20 z-10 -ml-24 mr-4 rounded-full bg-gray-200 border border-solid border-gray-400 bg-no-repeat">';
+            $output .= '<div id="'.$this->getID().'Photo" class="flex-none relative w-20 h-20 z-10  mr-4 rounded-full bg-gray-200 border border-solid border-gray-400 bg-no-repeat">';
             $output .= '<div id="'.$this->getID().'Count" class="hidden badge"></div>';
             $output .= '</div>';
 
