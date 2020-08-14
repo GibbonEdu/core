f961589d225519ebb4f3f3c87b3299ae65d14caf
Sandra Kuipers <sandra@skuipers.com>
2020-08-14 14:48:27 +0800

    Individual Needs: fix description of Reason field in Manage Investigations

--- modules/Individual Needs/investigations_manage_edit.php
+++ modules/Individual Needs/investigations_manage_edit.php
@@ -106,7 +106,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Individual Needs/investiga
             		//Reason
                     $row = $form->addRow();
                         $column = $row->addColumn();
-                        $column->addLabel('reason', __('Reason'))->description(__('Why should this student\'s individual needs should be investigated?'));;
+                        $column->addLabel('reason', __('Reason'))->description(__('Why should this student\'s individual needs be investigated?'));;
                     	$column->addTextArea('reason')->setRows(5)->setClass('fullWidth')->required()->readonly(!$canEdit || $investigation['status'] != 'Referral');
 
                     //Strategies Tried
