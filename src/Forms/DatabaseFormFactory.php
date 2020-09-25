8772ac65209bd8be69fc1d53ddfb6112fc90b28f
Sandra Kuipers <sandra@skuipers.com>
2020-09-25 14:02:15 +0800

    Forms: add an optional courseFilter to the addSelectClass method

--- src/Forms/DatabaseFormFactory.php
+++ src/Forms/DatabaseFormFactory.php
@@ -139,6 +139,24 @@ class DatabaseFormFactory extends FormFactory
 
         $classes = array();
 
+        if (!empty($gibbonPersonID) && !empty($params['courseFilter'])) {
+            $data = ['gibbonSchoolYearID' => $gibbonSchoolYearID, 'gibbonPersonID' => $gibbonPersonID, 'courseFilter' => '%'.$params['courseFilter'].'%'];
+            $sql = "SELECT gibbonCourseClass.gibbonCourseClassID, CONCAT(gibbonCourse.nameShort, '.', gibbonCourseClass.nameShort) AS class 
+                FROM gibbonCourse
+                JOIN gibbonCourseClass ON (gibbonCourse.gibbonCourseID=gibbonCourseClass.gibbonCourseID)
+                JOIN gibbonCourseClassPerson ON (gibbonCourseClass.gibbonCourseClassID=gibbonCourseClassPerson.gibbonCourseClassID )
+                WHERE gibbonSchoolYearID=:gibbonSchoolYearID
+                AND gibbonCourse.name LIKE :courseFilter
+                AND gibbonCourseClassPerson.gibbonPersonID=:gibbonPersonID
+                AND NOT role LIKE '% - Left%'
+                ORDER BY class";
+
+            $result = $this->pdo->select($sql, $data);
+            if ($result->rowCount() > 0) {
+                $classes[$params['courseFilter']] = $result->fetchAll(\PDO::FETCH_KEY_PAIR);
+            }
+        }
+
         if (!empty($gibbonPersonID) && !empty($params['departments'])) {
             $data = ['gibbonSchoolYearID' => $gibbonSchoolYearID, 'gibbonPersonID' => $gibbonPersonID, 'gibbonDepartmentIDList' => $params['departments']];
             $sql = "SELECT gibbonCourseClass.gibbonCourseClassID, CONCAT(gibbonCourse.nameShort, '.', gibbonCourseClass.nameShort) AS class 
