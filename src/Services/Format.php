698ea607947bd1b493fb692aaf809d510cdf580d
Ashton Power <48048360+powertashton@users.noreply.github.com>
2020-10-09 10:53:08 +0800

    Format: add a nameLinked method (#1120)
    * Update Format.php
    
    * Update CHANGELOG.txt
    
    * Update Format.php
    
    * Update Format.php
    
    * Remove parenthesis...
    
    * Update Format.php
    
    * Fixed enrolled spelling
    
    * Fix tag reverts...

--- src/Services/Format.php
+++ src/Services/Format.php
@@ -584,7 +584,39 @@ class Format
 
         return trim($output, ' ');
     }
-
+    
+    /**
+     * Formats a linked name based on roleCategory
+     * @param string $gibbonPersonID
+     * @param string $title
+     * @param string $preferredName
+     * @param string $surname
+     * @param string $roleCategory
+     * @param bool $reverse
+     * @param bool $informal
+     * @return string
+     */
+    public static function nameLinked($gibbonPersonID, $title, $preferredName, $surname, $roleCategory = 'Other', $reverse = false, $informal = false, $params = [])
+    {
+        $name = self::name($title, $preferredName, $surname, $roleCategory, $reverse, $informal);
+        if ($roleCategory == 'Staff') {
+            $url = static::$settings['absoluteURL'].'/index.php?q=/modules/Staff/staff_view_details.php&gibbonPersonID='.$gibbonPersonID;
+            if (!empty($params)) {
+                $url .= '&'.http_build_query($params);
+            }
+            $output = self::link($url, $name);
+        } elseif ($roleCategory == 'Student') {
+            $url = static::$settings['absoluteURL'].'/index.php?q=/modules/Students/student_view_details.php&gibbonPersonID='.$gibbonPersonID;
+            if (!empty($params)) {
+                $url .= '&'.http_build_query($params);
+            }
+            $output = self::link($url, $name);
+        } else {
+            $output = $name;
+        }
+        return $output;
+    }
+    
     /**
      * Formats a list of names from an array containing standard title, preferredName & surname fields.
      *
