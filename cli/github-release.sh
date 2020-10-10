1c3c5ae23e250b5e7b8d0da8c9eefac5a75326af
Koala Yeung <koala@pixelactionstudio.com>
2020-10-10 19:16:39 +0800

    Customize release asset label
    * Allow user to label asset uploaded with github-release

--- cli/github-release.sh
+++ cli/github-release.sh
@@ -26,7 +26,8 @@ EOF
 #   GITHUB_API_TOKEN   OAuth2 Token for releasing. See "Personal Access Tokens" in GitHub Docs.
 #   GITHUB_REPO_SLUG   GitHub repository slug string (i.e. "username/reponame").
 #   TAG                The tag name of the release to create.
-#   DISTPATH           The path where the assets locate.
+#   DISTPATH           The path where the asset(s) locate.
+#   LABEL              The label for the released asset(s).
 #
 function readopts {
     # Read all options as variables
@@ -52,7 +53,8 @@ function readopts {
 
     # Remove all options from $@
     shift $((OPTIND -1))
-    DISTPATH=$1
+    DISTPATH="$1"
+    LABEL="$2"
 
     if [ -z "$GITHUB_API_TOKEN" ]; then
         echo "flag -k is not set"
@@ -98,7 +100,7 @@ function release_create {
     eval $(echo "$RESPONSE" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
     if [ -z "$id" ]; then
         echo "Failed to create release for tag: $TAG"
-        echo "$RESPONSE"
+        echo "Details: $RESPONSE"
         return 1
     fi
 }
@@ -118,6 +120,11 @@ function release_get_id {
     declare -n id=$1
     local RESPONSE=$(curl -sH "$AUTH" "https://api.github.com/repos/$GITHUB_REPO_SLUG/releases/tags/$TAG")
     eval $(echo "$RESPONSE" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+    if [ -z "$RELEASE_ID" ]; then
+        echo "No release found for the tag $TAG"
+        echo "Details: $RESPONSE"
+        return 1
+    fi
 }
 
 #
@@ -136,9 +143,13 @@ function release_add_asset {
     # Upload asset
     if [ -s "$FILE" ]; then
         local FILENAME=$(basename $FILE)
-        local LABEL="Installation Bundle (${FILENAME#*.})"
+        if [ -z "$LABEL" ]; then
+            local __LABEL="$FILENAME"
+        else
+            local __LABEL="$LABEL ($FILENAME)"
+        fi
         echo "Uploading asset: $FILENAME"
-        local QUERY="name=$(urlencode $FILENAME)&label=$(urlencode "$LABEL")"
+        local QUERY="name=$(urlencode $FILENAME)&label=$(urlencode "$__LABEL")"
         echo "https://uploads.github.com/repos/$GITHUB_REPO_SLUG/releases/$RELEASE_ID/assets?$QUERY"
         curl \
             -X POST \
@@ -181,27 +192,29 @@ function urlencode {
 #
 
 # Read arguments and options into global variables
+_IFS="$IFS"; IFS=$'\0'
 readopts $@
+IFS="$_IFS"
 
 ## FIXME: Validate token before use
 
-# Create or find the release for the tag
-if GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" TAG="$TAG" release_create RELEASE_ID > /dev/null; then
-    echo "successfully created release id: $RELEASE_ID, tag: $TAG"
-else
-    GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" TAG="$TAG" release_get_id RELEASE_ID
-    if [ -z "$RELEASE_ID" ]; then
-        echo "unable to find release for tag: $TAG"
-        exit 1
-    fi
-    echo "retrieved existing release id: $RELEASE_ID, tag: $TAG"
+# Find the release for the tag, or create one
+if GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" TAG="$TAG" release_get_id RELEASE_ID; then
+    echo "RELEASE_ID=$RELEASE_ID"
+    echo
+elif GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" TAG="$TAG" release_create RELEASE_ID; then
+    echo "RELEASE_ID=$RELEASE_ID"
+    echo
 fi
-echo
+if [ -z "$RELEASE_ID" ]; then
+    echo "unable to create or find release for tag: $TAG"
+    exit 1
+fi
 
 # Upload assets in the dist folder
 for FILE in $DISTPATH/*; do
     if [ -f "$FILE" ]; then
-        GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" RELEASE_ID="$RELEASE_ID" FILE="$FILE" release_add_asset
+        GITHUB_API_TOKEN="$GITHUB_API_TOKEN" GITHUB_REPO_SLUG="$GITHUB_REPO_SLUG" RELEASE_ID="$RELEASE_ID" FILE="$FILE" LABEL="$LABEL" release_add_asset
     else
         echo "not a file. skipped: $FILE"
     fi
