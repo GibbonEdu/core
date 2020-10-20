601776eb9cc1cf36ea97b98c6c31e4014352a470
Ray Clark <raynichc@users.noreply.github.com>
2020-10-20 23:58:00 +1100

    Messenger: OOified cannedReponse_manage Table

--- modules/Messenger/cannedResponse_manage.php
+++ modules/Messenger/cannedResponse_manage.php
@@ -17,6 +17,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+use Gibbon\Tables\DataTable;
+use Gibbon\Services\Format;
+
 $page->breadcrumbs->add(__('Manage Canned Responses'));
 
 if (isActionAccessible($guid, $connection2, '/modules/Messenger/cannedResponse_manage.php') == false) {
@@ -39,46 +42,25 @@ if (isActionAccessible($guid, $connection2, '/modules/Messenger/cannedResponse_m
         echo "<div class='error'>".$e->getMessage().'</div>';
     }
 
-    echo "<div class='linkTop'>";
-    echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module']."/cannedResponse_manage_add.php'>".__('Add')."<img style='margin-left: 5px' title='".__('Add')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/page_new.png'/></a>";
-    echo '</div>';
+    $moduleName = $gibbon->session->get('module');
 
-    if ($result->rowCount() < 1) {
-        echo "<div class='error'>";
-        echo __('There are no records to display.');
-        echo '</div>';
-    } else {
-        echo "<table cellspacing='0' style='width: 100%'>";
-        echo "<tr class='head'>";
-        echo '<th>';
-        echo __('Subject');
-        echo '</th>';
-        echo "<th style='width:110px'>";
-        echo __('Action');
-        echo '</th>';
-        echo '</tr>';
+    $table = DataTable::create('cannedResponses');
 
-        $count = 0;
-        $rowNum = 'odd';
-        while ($row = $result->fetch()) {
-            if ($count % 2 == 0) {
-                $rowNum = 'even';
-            } else {
-                $rowNum = 'odd';
-            }
-            ++$count;
+    $table->addHeaderAction('add', __('Add'))
+        ->displayLabel()
+        ->setURL('/modules/' . $moduleName . '/cannedResponse_manage_add.php');
 
-            //COLOR ROW BY STATUS!
-            echo "<tr class=$rowNum>";
-            echo '<td>';
-            echo $row['subject'];
-            echo '</td>';
-            echo '<td>';
-            echo "<a href='".$_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.$_SESSION[$guid]['module'].'/cannedResponse_manage_edit.php&gibbonMessengerCannedResponseID='.$row['gibbonMessengerCannedResponseID']."'><img title='".__('Edit')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/config.png'/></a> ";
-            echo "<a class='thickbox' href='".$_SESSION[$guid]['absoluteURL'].'/fullscreen.php?q=/modules/'.$_SESSION[$guid]['module'].'/cannedResponse_manage_delete.php&gibbonMessengerCannedResponseID='.$row['gibbonMessengerCannedResponseID']."&width=650&height=135'><img title='".__('Delete')."' src='./themes/".$_SESSION[$guid]['gibbonThemeName']."/img/garbage.png'/></a>";
-            echo '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    }
+    $table->addColumn('subject', __('Subject'));
+
+    $table->addActionColumn()
+        ->addParam('gibbonMessengerCannedResponseID')
+        ->format(function ($cannedResponse, $actions) use ($moduleName) {
+            $actions->addAction('edit', __('Edit'))
+                ->setURL('/modules/' . $moduleName . '/cannedResponse_manage_edit.php');
+
+            $actions->addAction('delete', __('Delete'))
+                ->setURL('/modules/' . $moduleName . '/cannedResponse_manage_delete.php');
+        });
+
+    echo $table->render($result->toDataSet());
 }
