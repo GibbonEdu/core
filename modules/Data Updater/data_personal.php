f5f962c2ab5f45635f46f267af8e2887fdef3a9c
Sandra Kuipers <sandra@skuipers.com>
2020-11-02 12:32:29 +0800

    Data Updater: fix passport expiry fields not properly hiding

--- modules/Data Updater/data_personal.php
+++ modules/Data Updater/data_personal.php
@@ -543,7 +543,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Data Updater/data_personal
                         $row->addLabel('citizenship1Passport', __('Citizenship 1 Passport Number'));
                         $row->addTextField('citizenship1Passport')->maxLength(30);
 
-                    $row = $form->addRow();
+                    $row = $form->addRow()->onlyIf($isVisible('citizenship1PassportExpiry'));
                         $row->addLabel('citizenship1PassportExpiry', __('Citizenship 1 Passport Expiry Date'));
                         $row->addDate('citizenship1PassportExpiry');
 
@@ -559,7 +559,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Data Updater/data_personal
                         $row->addLabel('citizenship2Passport', __('Citizenship 2 Passport Number'));
                         $row->addTextField('citizenship2Passport')->maxLength(30);
 
-                    $row = $form->addRow();
+                    $row = $form->addRow()->onlyIf($isVisible('citizenship1PassportExpiry'));
                         $row->addLabel('citizenship2PassportExpiry', __('Citizenship 2 Passport Expiry Date'));
                         $row->addDate('citizenship2PassportExpiry');
 
