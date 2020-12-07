0b7bb466ac96464333efc170e11c5e74c87d3ad6
Sandra Kuipers <sandra@skuipers.com>
2020-12-07 09:16:47 +0800

    Timetable Admin: #1182 post-merge tweaks

--- modules/Timetable Admin/tt_edit_day_edit.php
+++ modules/Timetable Admin/tt_edit_day_edit.php
@@ -75,15 +75,11 @@ if (isActionAccessible($guid, $connection2, '/modules/Timetable Admin/tt_edit_da
 
             $row = $form->addRow();
                 $row->addLabel('color', __('Header Background Colour'))->description(__('Click to select a color.'));
-                $row->addColor("color")
-                        ->addClass('pl-2')
-                        ->setValue($rowSQL['color']);
+                $row->addColor("color");
 
             $row = $form->addRow();
                 $row->addLabel('fontColor', __('Header Font Colour'))->description(__('Click to select a color.'));
-                $row->addColor("fontColor")
-                        ->addClass('pl-2')
-                        ->setValue($rowSQL['fontColor']);
+                $row->addColor("fontColor");
 
             $row = $form->addRow();
                 $row->addLabel('columnName', __('Timetable Column'));
