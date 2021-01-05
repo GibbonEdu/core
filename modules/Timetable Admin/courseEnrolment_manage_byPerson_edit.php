6f3e173454d2112998c165b1a89bdda1bebfbe13
Sandra Kuipers <sandra@skuipers.com>
2021-01-05 08:42:39 +0800

    Timetable Admin: preserve All Users option when clicking on timetable

--- modules/Timetable Admin/courseEnrolment_manage_byPerson_edit.php
+++ modules/Timetable Admin/courseEnrolment_manage_byPerson_edit.php
@@ -214,7 +214,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Timetable Admin/courseEnro
             $gibbonTTID = isset($_GET['gibbonTTID'])? $_GET['gibbonTTID'] : null;
             $ttDate = isset($_POST['ttDate'])? dateConvertToTimestamp(dateConvert($guid, $_POST['ttDate'])) : null;
 
-            $tt = renderTT($guid, $connection2, $gibbonPersonID, $gibbonTTID, false, $ttDate, '/modules/Timetable Admin/courseEnrolment_manage_byPerson_edit.php', "&gibbonPersonID=$gibbonPersonID&gibbonSchoolYearID=$gibbonSchoolYearID&type=$type#tt", 'full', true);
+            $tt = renderTT($guid, $connection2, $gibbonPersonID, $gibbonTTID, false, $ttDate, '/modules/Timetable Admin/courseEnrolment_manage_byPerson_edit.php', "&gibbonPersonID=$gibbonPersonID&gibbonSchoolYearID=$gibbonSchoolYearID&type=$type&allUsers=$allUsers#tt", 'full', true);
             if ($tt != false) {
                 echo $tt;
             } else {
