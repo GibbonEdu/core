7bcf5eb67112cefcbfcf1d91eedbe169afef3e3d
Sandra Kuipers <sandra@skuipers.com>
2021-01-20 08:33:29 +0800

    System: update Force Password Redirect logic

--- preferencesPasswordProcess.php
+++ preferencesPasswordProcess.php
@@ -32,10 +32,11 @@ $forceReset = $gibbon->session->get('passwordForceReset');
 
 if ($forceReset != 'Y') {
     $forceReset = 'N';
-    $URL = $gibbon->session->get('absoluteURL')."/index.php?q=preferences.php&forceReset=N";
+    $URLSuccess = $gibbon->session->get('absoluteURL')."/index.php?q=preferences.php&forceReset=N";
 } else {
-    $URL = $gibbon->session->get('absoluteURL')."/index.php?forceReset=Y";
+    $URLSuccess = $gibbon->session->get('absoluteURL')."/index.php?forceReset=Y";
 }
+$URL = $gibbon->session->get('absoluteURL')."/index.php?q=preferences.php&forceReset=".$forceReset;
 
 //Check passwords are not blank
 if ($password == '' or $passwordNew == '' or $passwordConfirm == '') {
@@ -87,7 +88,7 @@ if ($password == '' or $passwordNew == '' or $passwordConfirm == '') {
                             $result = $connection2->prepare($sql);
                             $result->execute($data);
                         } catch (PDOException $e) {
-                            $URL = $gibbon->session->get('absoluteURL')."/index.php?q=preferences.php&return=errora";
+                            $URL .= '&return=errora';
                             header("Location: {$URL}");
                             exit();
                         }
@@ -95,16 +96,16 @@ if ($password == '' or $passwordNew == '' or $passwordConfirm == '') {
                         $gibbon->session->set('passwordStrongSalt', $salt);
                         $gibbon->session->set('passwordStrong', $passwordStrong);
                         $gibbon->session->set('pageLoads', null);
-                        $URL .= '&return=successa';
-                        header("Location: {$URL}");
+                        $URLSuccess .= '&return=successa';
+                        header("Location: {$URLSuccess}");
                         exit() ;
                     }
 
                     $gibbon->session->set('passwordStrongSalt', $salt);
                     $gibbon->session->set('passwordStrong', $passwordStrong);
                     $gibbon->session->set('pageLoads', null);
-                    $URL .= '&return=success0';
-                    header("Location: {$URL}");
+                    $URLSuccess .= '&return=success0';
+                    header("Location: {$URLSuccess}");
                 }
             }
         }
