3c497e0785870401b703d1672a5ac3c55bba47f5
hmerrettICHK <74997253+hmerrettICHK@users.noreply.github.com>
2021-01-27 14:54:36 +0800

    Attendance: null coalesce refactoring (#1189)
    * Refactor - Attendance
    
    * Null coalesce inside function
    
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>
    Co-authored-by: Sandra Kuipers <sandra@skuipers.com>

--- modules/Attendance/attendance_take_byPersonProcess.php
+++ modules/Attendance/attendance_take_byPersonProcess.php
@@ -26,7 +26,7 @@ require __DIR__ . '/../../gibbon.php';
 require_once __DIR__ . '/moduleFunctions.php';
 
 $gibbonPersonID = $_GET['gibbonPersonID'];
-$currentDate = $_POST['currentDate'];
+$currentDate = $_POST['currentDate'] ?? '';
 $today = date('Y-m-d');
 $URL = $_SESSION[$guid]['absoluteURL'].'/index.php?q=/modules/'.getModuleName($_POST['address'])."/attendance_take_byPerson.php&gibbonPersonID=$gibbonPersonID&currentDate=".dateConvertBack($guid, $currentDate);
 
@@ -70,9 +70,9 @@ if (isActionAccessible($guid, $connection2, '/modules/Attendance/attendance_take
                     $attendance = new AttendanceView($gibbon, $pdo);
 
                     $fail = false;
-                    $type = $_POST['type'];
-                    $reason = $_POST['reason'];
-                    $comment = $_POST['comment'];
+                    $type = $_POST['type'] ?? '';
+                    $reason = $_POST['reason'] ?? '';
+                    $comment = $_POST['comment'] ?? '';
 
                     $attendanceCode = $attendance->getAttendanceCodeByType($type);
                     $direction = $attendanceCode['direction'];
