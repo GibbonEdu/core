d498609412f8b8dcef5563222e70ed4588dca343
Sandra Kuipers <sandra@skuipers.com>
2021-01-29 16:27:31 +0800

    System: clear theme cache when switching, refactor notifications and minor links (#1235)
    * System: add non-core themes to .gitignore
    
    * System: fix cache not clearing when switching themes
    
    * System: refactor the Sidebar template to accept an array of items
    
    * System: refactor the notifications tray and notifications ajax reload
    
    * System: refactor minor links to use an array rather than raw html
    
    * System: update the resources/build submodule
    
    * System: remove debug from notification ajax
    
    * System: changelog update
    
    * Messenger: removed the pop-up Messenger bubbles
    
    * Theme: reset theme.min.css changes
    
    * School Admin: remove the Message Bubble settings
    
    * System: fix sidebar for notifications

--- index_notification_ajax.php
+++ index_notification_ajax.php
@@ -17,66 +17,28 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-//Gibbon system-wide includes
+use Gibbon\Domain\System\NotificationGateway;
+
+// Gibbon system-wide includes
 include './gibbon.php';
 
-$output = '';
+$result = ['count' => 0, 'alarm' => false];
 
-$themeName = $gibbon->session->get('gibbonThemeName') ?? 'Default';
-
-if (!isset($_SESSION[$guid]) or !$gibbon->session->exists('gibbonPersonID')) {
-    $output .= "<a class='inactive' title='".__('Notifications')."' href='#'><img class='minorLinkIcon' style='margin-left: 2px; opacity: 0.2; vertical-align: -75%' src='./themes/Default/img/notifications.png'></a>";
-} else {
-    //CHECK FOR SYSTEM ALARM
-    if ($gibbon->session->exists('gibbonRoleIDCurrentCategory')) {
-        if ($gibbon->session->get('gibbonRoleIDCurrentCategory') == 'Staff') {
-            $alarm = getSettingByScope($connection2, 'System', 'alarm');
-            if ($alarm == 'General' or $alarm == 'Lockdown' or $alarm == 'Custom') {
-                $type = 'general';
-                if ($alarm == 'Lockdown') {
-                    $type = 'lockdown';
-                } elseif ($alarm == 'Custom') {
-                    $type = 'custom';
-                }
-                $output .= "<script>
-					if ($('#TB_window').is(':visible')==true && $('#TB_window').hasClass('alarm') == false) {
-						$(\"#TB_window\").remove();
-                        $(\"body\").append(\"<div id='TB_window'></div>\");
-					}
-					if ($('#TB_window').is(':visible')==false) {
-						var url = '".$gibbon->session->get('absoluteURL').'/index_notification_ajax_alarm.php?type='.$type."&KeepThis=true&TB_iframe=true&width=1000&height=500';
-						tb_show('', url);
-						$('#TB_window').addClass('alarm') ;
-					}
-				</script>";
-            } else {
-                $output .= "<script>
-					if ($('#TB_window').is(':visible')==true && $('#TB_window').hasClass('alarm') ) {
-						tb_remove();
-					}
-				</script>";
-            }
-        }
+if ($gibbon->session->has('gibbonPersonID')) {
+    // Check for system alarm
+    if ($gibbon->session->get('gibbonRoleIDCurrentCategory') == 'Staff') {
+        $alarm = getSettingByScope($connection2, 'System', 'alarm');
+        $result['alarm'] = $alarm == 'General' || $alarm == 'Lockdown' || $alarm == 'Custom'
+            ? strtolower($alarm)
+            : false;
     }
 
-    //GET & SHOW NOTIFICATIONS
-    try {
-        $dataNotifications = array('gibbonPersonID' => $gibbon->session->get('gibbonPersonID'), 'gibbonPersonID2' => $gibbon->session->get('gibbonPersonID'));
-        $sqlNotifications = "(SELECT gibbonNotification.*, gibbonModule.name AS source FROM gibbonNotification JOIN gibbonModule ON (gibbonNotification.gibbonModuleID=gibbonModule.gibbonModuleID) WHERE gibbonPersonID=:gibbonPersonID AND status='New')
-		UNION
-		(SELECT gibbonNotification.*, 'System' AS source FROM gibbonNotification WHERE gibbonModuleID IS NULL AND gibbonPersonID=:gibbonPersonID2 AND status='New')
-		ORDER BY timestamp DESC, source, text";
-        $resultNotifications = $connection2->prepare($sqlNotifications);
-        $resultNotifications->execute($dataNotifications);
-    } catch (PDOException $e) {
-        $return .= "<div class='error'>".$e->getMessage().'</div>';
-    }
+    // Get notification count for the current user
+    $notificationGateway = $container->get(NotificationGateway::class);
+    $criteria = $notificationGateway->newQueryCriteria();
+    $notifications = $notificationGateway->queryNotificationsByPerson($criteria, $gibbon->session->get('gibbonPersonID'), 'New');
 
-    if ($resultNotifications->rowCount() > 0) {
-        $output .= "<a class='inline-block relative mr-4' title='".__('Notifications')."' href='./index.php?q=notifications.php'><span class='badge -mr-2 right-0'>".$resultNotifications->rowCount()."</span><img style='margin-left: 2px; vertical-align: -75%' src='./themes/".$themeName."/img/notifications.png'></a>";
-    } else {
-        $output .= "<a class='inactive inline-block relative mr-4' title='".__('Notifications')."' href='".$gibbon->session->get('absoluteURL')."/index.php?q=notifications.php'><img class='minorLinkIcon' style='margin-left: 2px; opacity: 0.2; vertical-align: -75%' src='".$gibbon->session->get('absoluteURL').'/themes/'.$gibbon->session->get('gibbonThemeName')."/img/notifications.png'></a>";
-    }
+    $result['count'] = $notifications->count();
 }
 
-echo $output;
+echo json_encode($result);
