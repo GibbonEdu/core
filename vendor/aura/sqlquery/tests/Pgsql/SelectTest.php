eb82e6410b4aa0623a549631525bd22637153db7
Koala Yeung <koalay@gmail.com>
2021-03-12 13:43:20 +0800

    System: replace "/vendor" with automatic installation bundle in release (GitHub Actions) (#1209)
    * github-actions: automatic bundle release
    
    * Attach an additional release bundle to all releases.
    
    * Remove and ignore the /vendor folder

--- vendor/aura/sqlquery/tests/Pgsql/SelectTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aura\SqlQuery\Pgsql;
-
-use Aura\SqlQuery\Common;
-
-class SelectTest extends Common\SelectTest
-{
-    protected $db_type = 'pgsql';
-}
--- vendor/aura/sqlquery/tests/Sqlsrv/DeleteTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Aura\SqlQuery\Sqlsrv;
-
-use Aura\SqlQuery\Common;
-
-class DeleteTest extends Common\DeleteTest
-{
-    protected $db_type = 'sqlsrv';
-}
