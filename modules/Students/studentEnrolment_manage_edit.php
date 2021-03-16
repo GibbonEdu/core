0f0580c4746c3fa6007ac9fd26a9b25410844d72
Sandra Kuipers <sandra@skuipers.com>
2021-03-16 12:10:09 +0800

    Students: move the Student Enrolment select to a gateway, refactor a bit

--- modules/Students/studentEnrolment_manage_edit.php
+++ modules/Students/studentEnrolment_manage_edit.php
@@ -18,8 +18,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
-use Gibbon\Forms\DatabaseFormFactory;
 use Gibbon\Services\Format;
+use Gibbon\Forms\DatabaseFormFactory;
+use Gibbon\Domain\School\SchoolYearGateway;
+use Gibbon\Domain\Students\StudentGateway;
+use Gibbon\Domain\Timetable\CourseSyncGateway;
 
 if (isActionAccessible($guid, $connection2, '/modules/Students/studentEnrolment_manage_edit.php') == false) {
     // Access denied
@@ -39,100 +42,96 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/studentEnrolment_
         $page->addError(__('You have not specified one or more required parameters.'));
     } else {
         
-            $data = array('gibbonSchoolYearID' => $gibbonSchoolYearID, 'gibbonStudentEnrolmentID' => $gibbonStudentEnrolmentID);
-            $sql = 'SELECT gibbonRollGroup.gibbonRollGroupID, gibbonYearGroup.gibbonYearGroupID,gibbonStudentEnrolmentID, surname, preferredName, gibbonYearGroup.nameShort AS yearGroup, gibbonRollGroup.nameShort AS rollGroup, dateStart, dateEnd, gibbonPerson.gibbonPersonID, rollOrder FROM gibbonPerson, gibbonStudentEnrolment, gibbonYearGroup, gibbonRollGroup WHERE (gibbonPerson.gibbonPersonID=gibbonStudentEnrolment.gibbonPersonID) AND (gibbonStudentEnrolment.gibbonYearGroupID=gibbonYearGroup.gibbonYearGroupID) AND (gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID) AND gibbonRollGroup.gibbonSchoolYearID=:gibbonSchoolYearID AND gibbonStudentEnrolmentID=:gibbonStudentEnrolmentID ORDER BY surname, preferredName';
-            $result = $connection2->prepare($sql);
-            $result->execute($data);
-
-        if ($result->rowCount() != 1) {
+        $studentGateway = $container->get(StudentGateway::class);
+        $enrollment = $studentGateway->getByID($gibbonStudentEnrolmentID);
+        if (empty($enrollment)) {
             $page->addError(__('The specified record cannot be found.'));
-        } else {
-            //Let's go!
-            $values = $result->fetch();
-
-            if ($search != '') {
-                echo "<div class='linkTop'>";
-                echo "<a href='".$_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Students/studentEnrolment_manage.php&gibbonSchoolYearID=$gibbonSchoolYearID&search=$search'>".__('Back to Search Results').'</a>';
-                echo '</div>';
-            }
-
-            $form = Form::create('studentEnrolmentAdd', $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/studentEnrolment_manage_editProcess.php?gibbonSchoolYearID=$gibbonSchoolYearID&search=$search");
-            $form->setFactory(DatabaseFormFactory::create($pdo));
-
-            $form->addHiddenValue('address', $_SESSION[$guid]['address']);
-            $form->addHiddenValue('gibbonStudentEnrolmentID', $gibbonStudentEnrolmentID);
-            $form->addHiddenValue('gibbonPersonID', $values['gibbonPersonID']);
-            $form->addHiddenValue('gibbonRollGroupIDOriginal', $values['gibbonRollGroupID']);
-
-            $data = array('gibbonSchoolYearID' => $gibbonSchoolYearID);
-            $sql = 'SELECT name FROM gibbonSchoolYear WHERE gibbonSchoolYearID=:gibbonSchoolYearID';
-            $result = $pdo->executeQuery($data, $sql);
-
-            $schoolYearName = ($result->rowCount() == 1)? $result->fetchColumn(0) : $_SESSION[$guid]['gibbonSchoolYearName'];
-
-            $row = $form->addRow();
-                $row->addLabel('yearName', __('School Year'));
-                $row->addTextField('yearName')->readOnly()->maxLength(20)->setValue($schoolYearName);
-
-            $row = $form->addRow();
-                $row->addLabel('studentName', __('Student'));
-                $row->addTextField('studentName')->readOnly()->setValue(Format::name('', $values['preferredName'], $values['surname'], 'Student', true));
-
-            $row = $form->addRow();
-                $row->addLabel('gibbonYearGroupID', __('Year Group'));
-                $row->addSelectYearGroup('gibbonYearGroupID')->required();
-
-            $row = $form->addRow();
-                $row->addLabel('gibbonRollGroupID', __('Roll Group'));
-                $row->addSelectRollGroup('gibbonRollGroupID', $gibbonSchoolYearID)->required();
-
-            $row = $form->addRow();
-                $row->addLabel('rollOrder', __('Roll Order'));
-                $row->addNumber('rollOrder')->maxLength(2);
-
-            // Check to see if any class mappings exists -- otherwise this feature is inactive, hide it
-            $sql = "SELECT COUNT(*) FROM gibbonCourseClassMap";
-            $resultClassMap = $pdo->executeQuery(array(), $sql);
-            $classMapCount = ($resultClassMap->rowCount() > 0)? $resultClassMap->fetchColumn(0) : 0;
-
-            if ($classMapCount > 0) {
-                $autoEnrolDefault = getSettingByScope($connection2, 'Timetable Admin', 'autoEnrolCourses');
-                $row = $form->addRow();
-                    $row->addLabel('autoEnrolStudent', __('Auto-Enrol Courses?'))
-                        ->description(__('Should this student be automatically enrolled in courses for their Roll Group?'))
-                        ->description(__('This will replace any auto-enrolled courses if the student Roll Group has changed.'));
-                    $row->addYesNo('autoEnrolStudent')->selected($autoEnrolDefault);
-            }
-
-            $schoolHistory = '';
-
-            if ($values['dateStart'] != '') {
-                $schoolHistory .= '<li><u>'.__('Start Date').'</u>: '.dateConvertBack($guid, $values['dateStart']).'</li>';
-            }
-
-            $dataSelect = array('gibbonPersonID' => $values['gibbonPersonID']);
-            $sqlSelect = 'SELECT gibbonRollGroup.name AS rollGroup, gibbonSchoolYear.name AS schoolYear FROM gibbonStudentEnrolment JOIN gibbonRollGroup ON (gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID) JOIN gibbonSchoolYear ON (gibbonStudentEnrolment.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID) WHERE gibbonPersonID=:gibbonPersonID ORDER BY gibbonStudentEnrolment.gibbonSchoolYearID';
-            $resultSelect = $pdo->executeQuery($dataSelect, $sqlSelect);
-
-            while ($resultSelect && $rowSelect = $resultSelect->fetch()) {
-                $schoolHistory .= '<li><u>'.$rowSelect['schoolYear'].'</u>: '.$rowSelect['rollGroup'].'</li>';
-            }
-
-            if ($values['dateEnd'] != '') {
-                $schoolHistory .= '<li><u>'.__('End Date').'</u>: '.dateConvertBack($guid, $values['dateEnd']).'</li>';
-            }
-
-            $row = $form->addRow();
-                $row->addLabel('schoolHistory', __('School History'));
-                $row->addContent('<ul class="list-none w-full sm:max-w-xs text-xs m-0">'.$schoolHistory.'</ul>');
-
-            $row = $form->addRow();
-                $row->addFooter();
-                $row->addSubmit();
-
-            $form->loadAllValuesFrom($values);
-
-            echo $form->getOutput();
+            return;
         }
+
+        $values = $container->get(StudentGateway::class)->selectActiveStudentByPerson($gibbonSchoolYearID, $enrollment['gibbonPersonID'])->fetch();
+        if (empty($values)) {
+            $page->addError(__('The specified record cannot be found.'));
+            return;
+        }
+
+        if ($search != '') {
+            echo "<div class='linkTop'>";
+            echo "<a href='".$_SESSION[$guid]['absoluteURL']."/index.php?q=/modules/Students/studentEnrolment_manage.php&gibbonSchoolYearID=$gibbonSchoolYearID&search=$search'>".__('Back to Search Results').'</a>';
+            echo '</div>';
+        }
+
+        $form = Form::create('studentEnrolmentAdd', $_SESSION[$guid]['absoluteURL'].'/modules/'.$_SESSION[$guid]['module']."/studentEnrolment_manage_editProcess.php?gibbonSchoolYearID=$gibbonSchoolYearID&search=$search");
+        $form->setFactory(DatabaseFormFactory::create($pdo));
+
+        $form->addHiddenValue('address', $_SESSION[$guid]['address']);
+        $form->addHiddenValue('gibbonStudentEnrolmentID', $gibbonStudentEnrolmentID);
+        $form->addHiddenValue('gibbonPersonID', $values['gibbonPersonID']);
+        $form->addHiddenValue('gibbonRollGroupIDOriginal', $values['gibbonRollGroupID']);
+
+        $schoolYear = $container->get(SchoolYearGateway::class)->getByID($gibbonSchoolYearID, ['name']);
+        $schoolYearName = $schoolYear['name'] ?? $_SESSION[$guid]['gibbonSchoolYearName'];
+
+        $row = $form->addRow();
+            $row->addLabel('yearName', __('School Year'));
+            $row->addTextField('yearName')->readOnly()->maxLength(20)->setValue($schoolYearName);
+
+        $row = $form->addRow();
+            $row->addLabel('studentName', __('Student'));
+            $row->addTextField('studentName')->readOnly()->setValue(Format::name('', $values['preferredName'], $values['surname'], 'Student', true));
+
+        $row = $form->addRow();
+            $row->addLabel('gibbonYearGroupID', __('Year Group'));
+            $row->addSelectYearGroup('gibbonYearGroupID')->required();
+
+        $row = $form->addRow();
+            $row->addLabel('gibbonRollGroupID', __('Roll Group'));
+            $row->addSelectRollGroup('gibbonRollGroupID', $gibbonSchoolYearID)->required();
+
+        $row = $form->addRow();
+            $row->addLabel('rollOrder', __('Roll Order'));
+            $row->addNumber('rollOrder')->maxLength(2)->setValue($enrollment['rollOrder']);
+
+        // Check to see if any class mappings exists -- otherwise this feature is inactive, hide it
+        $classMapCount = $container->get(CourseSyncGateway::class)->countAll();
+        if ($classMapCount > 0) {
+            $autoEnrolDefault = getSettingByScope($connection2, 'Timetable Admin', 'autoEnrolCourses');
+            $row = $form->addRow();
+                $row->addLabel('autoEnrolStudent', __('Auto-Enrol Courses?'))
+                    ->description(__('Should this student be automatically enrolled in courses for their Roll Group?'))
+                    ->description(__('This will replace any auto-enrolled courses if the student Roll Group has changed.'));
+                $row->addYesNo('autoEnrolStudent')->selected($autoEnrolDefault);
+        }
+
+        $schoolHistory = '';
+
+        if ($values['dateStart'] != '') {
+            $schoolHistory .= '<li><u>'.__('Start Date').'</u>: '.Format::date($values['dateStart']).'</li>';
+        }
+
+        $dataSelect = array('gibbonPersonID' => $values['gibbonPersonID']);
+        $sqlSelect = 'SELECT gibbonRollGroup.name AS rollGroup, gibbonSchoolYear.name AS schoolYear FROM gibbonStudentEnrolment JOIN gibbonRollGroup ON (gibbonStudentEnrolment.gibbonRollGroupID=gibbonRollGroup.gibbonRollGroupID) JOIN gibbonSchoolYear ON (gibbonStudentEnrolment.gibbonSchoolYearID=gibbonSchoolYear.gibbonSchoolYearID) WHERE gibbonPersonID=:gibbonPersonID ORDER BY gibbonStudentEnrolment.gibbonSchoolYearID';
+        $resultSelect = $pdo->executeQuery($dataSelect, $sqlSelect);
+
+        while ($resultSelect && $rowSelect = $resultSelect->fetch()) {
+            $schoolHistory .= '<li><u>'.$rowSelect['schoolYear'].'</u>: '.$rowSelect['rollGroup'].'</li>';
+        }
+
+        if ($values['dateEnd'] != '') {
+            $schoolHistory .= '<li><u>'.__('End Date').'</u>: '.Format::date($values['dateEnd']).'</li>';
+        }
+
+        $row = $form->addRow();
+            $row->addLabel('schoolHistory', __('School History'));
+            $row->addContent('<ul class="list-none w-full sm:max-w-xs text-xs m-0">'.$schoolHistory.'</ul>');
+
+        $row = $form->addRow();
+            $row->addFooter();
+            $row->addSubmit();
+
+        $form->loadAllValuesFrom($values);
+
+        echo $form->getOutput();
+        
     }
 }
