05db863ad2b330614b593dea669aab6059d535be
Sandra Kuipers <sandra@skuipers.com>
2021-03-22 09:46:19 +0800

    Forms: update subheadings to use the Heading class

--- src/Forms/FormFactory.php
+++ src/Forms/FormFactory.php
@@ -90,9 +90,14 @@ class FormFactory implements FormFactoryInterface
         return new Layout\Label($for, $label);
     }
 
-    public function createHeading($content = '')
+    public function createHeading($content = '', $tag = null)
     {
-        return new Layout\Heading($content);
+        return new Layout\Heading($content, $tag);
+    }
+
+    public function createSubheading($content, $tag = 'h4')
+    {
+        return new Layout\Heading($content, $tag = 'h4');
     }
 
     public function createContent($content = '')
@@ -244,12 +249,6 @@ class FormFactory implements FormFactoryInterface
     }
 
     /* PRE-DEFINED LAYOUT --------------------------- */
-
-    public function createSubheading($content, $tag = 'h4')
-    {
-        $content = sprintf('<%1$s class="m-0 p-0">%2$s</%1$s>', $tag, $content);
-        return $this->createContent($content);
-    }
 
     public function createAlert($content, $level = 'warning')
     {
