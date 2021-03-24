513a4f127e3ec4959eb25b58c1d94aec8761d2c0
Sandra Kuipers <sandra@skuipers.com>
2021-03-24 10:17:06 +0800

    System: update the colour value for Red in the TinyMCE color map

--- resources/assets/js/setup.js
+++ resources/assets/js/setup.js
@@ -77,7 +77,31 @@ $(document).ready(function(){
         browser_spellcheck: true,
         convert_urls: false,
         relative_urls: false,
-        default_link_target: "_blank"
+        default_link_target: "_blank",
+        color_map: [
+            "#BFEDD2", "Light Green", 
+            "#FBEEB8", "Light Yellow", 
+            "#F8CAC6", "Light Red", 
+            "#ECCAFA", "Light Purple", 
+            "#C2E0F4", "Light Blue", 
+            "#2DC26B", "Green", 
+            "#F1C40F", "Yellow", 
+            "#FF0000", "Red", 
+            "#B96AD9", "Purple", 
+            "#3598DB", "Blue", 
+            "#169179", "Dark Turquoise", 
+            "#E67E23", "Orange", 
+            "#BA372A", "Dark Red", 
+            "#843FA1", "Dark Purple", 
+            "#236FA1", "Dark Blue", 
+            "#ECF0F1", "Light Gray", 
+            "#CED4D9", "Medium Gray", 
+            "#95A5A6", "Gray", 
+            "#7E8C8D", "Dark Gray", 
+            "#34495E", "Navy Blue", 
+            "#000000", "Black", 
+            "#ffffff", "White", 
+        ],
     });
 
     // Initialize sessionTimeout
