fcbb6a8061540a4788569312cb2f58d5ca205963
Sandra Kuipers <sandra@skuipers.com>
2021-04-12 10:05:58 +0800

    Update student_view.php

--- modules/Students/student_view.php
+++ modules/Students/student_view.php
@@ -27,7 +27,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Students/student_view.php'
     $page->addError(__('You do not have access to this action.'));
 } else {
     //Get action with highest precendence
-    $highestAction = getHighestGroupedAction($gibbon->session->get('guid'), $_GET['q'], $connection2);
+    $highestAction = getHighestGroupedAction($guid, $_GET['q'], $connection2);
     if ($highestAction == false) {
         $page->addError(__('The highest grouped action cannot be determined.'));
     } else {
