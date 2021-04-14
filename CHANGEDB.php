cc4e5dc3965c093ad2b9b8a29ea4c3fafd3fff6f
Sandra Kuipers <sandra@skuipers.com>
2021-04-14 19:29:30 +0800

    System: enable linking pages to documentation through a help button (#1299)
    * System: enable linking pages to documentation through a help button
    
    * System: add help button to old default theme

--- CHANGEDB.php
+++ CHANGEDB.php
@@ -804,4 +804,6 @@ ALTER TABLE `gibbonStaff` ADD `fields` TEXT NULL AFTER `biographicalGroupingPrio
 ALTER TABLE `gibbonCourse` ADD `fields` TEXT NULL AFTER `orderBy`;end
 ALTER TABLE `gibbonCourseClass` ADD `fields` TEXT NULL AFTER `gibbonScaleIDTarget`;end
 ALTER TABLE `gibbonStaffApplicationForm` ADD `staffFields` TEXT NULL AFTER `fields`;end
+ALTER TABLE `gibbonAction` ADD `helpURL` VARCHAR(255) NULL AFTER `description`;end
+UPDATE `gibbonAction` SET helpURL='administrators/getting-started/getting-started-with-gibbon/#years-days-times' WHERE `category`='Years, Days & Times' AND `gibbonModuleID`=(SELECT gibbonModuleID FROM gibbonModule WHERE name='School Admin');end
 ";
