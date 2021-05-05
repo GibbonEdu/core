2af9fa137e7ebbcd6add99bc23e0937fa1ddf6ab
Sandra Kuipers <sandra@skuipers.com>
2021-05-05 13:44:47 +0800

    Reports: add pagination to the Proof Reading page

--- modules/Reports/reporting_proofreadProcess.php
+++ modules/Reports/reporting_proofreadProcess.php
@@ -25,8 +25,9 @@ require_once '../../gibbon.php';
 $mode = $_POST['mode'] ?? '';
 $gibbonPersonID = $_POST['gibbonPersonID'] ?? '';
 $gibbonRollGroupID = $_POST['gibbonRollGroupID'] ?? '';
+$page = $_POST['page'] ?? '';
 
-$URL = $gibbon->session->get('absoluteURL').'/index.php?q=/modules/Reports/reporting_proofread.php&mode='.$mode.'&gibbonPersonID='.$gibbonPersonID.'&gibbonRollGroupID='.$gibbonRollGroupID;
+$URL = $gibbon->session->get('absoluteURL').'/index.php?q=/modules/Reports/reporting_proofread.php&mode='.$mode.'&gibbonPersonID='.$gibbonPersonID.'&gibbonRollGroupID='.$gibbonRollGroupID.'&page='.$page;
 
 if (!empty($_POST['override'])) {
     $URL .= '&override='.$_POST['override'];
