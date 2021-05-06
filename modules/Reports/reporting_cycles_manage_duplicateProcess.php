9b19a84a3e9258e790ac03c69c6b0649d8d2b856
Sandra Kuipers <sandra@skuipers.com>
2021-05-06 14:28:15 +0800

    Fix typo

--- modules/Reports/reporting_cycles_manage_duplicateProcess.php
+++ modules/Reports/reporting_cycles_manage_duplicateProcess.php
@@ -107,7 +107,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Reports/reporting_cycles_m
 
                 foreach ($criteria as $criteriaData) {
                     // Grab the form group ID by name if it's in a different school year
-                    formif (!empty($criteriaData['gibbonFormGroupID']) && $data['gibbonSchoolYearID'] != $values['gibbonSchoolYearID']) {
+                    if (!empty($criteriaData['gibbonFormGroupID']) && $data['gibbonSchoolYearID'] != $values['gibbonSchoolYearID']) {
                         $formGroupSource = $formGroupGateway->getByID($criteriaData['gibbonFormGroupID']);
                         $formGroupDestination = $formGroupGateway->selectBy([
                             'gibbonSchoolYearID' => $data['gibbonSchoolYearID'], 
