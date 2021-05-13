9241f78360284a309da3ef4531185c6e79bc7270
Harry Merrett <hmerrett@ichk.edu.hk>
2021-05-13 13:22:56 +0800

    Attendance - final session refactoring fix

--- modules/Attendance/attendance_take_byPerson.php
+++ modules/Attendance/attendance_take_byPerson.php
@@ -127,12 +127,12 @@ if (isActionAccessible($guid, $connection2, '/modules/Attendance/attendance_take
                     });
 
                 $table->addColumn('direction', __('Attendance'))
-                    ->format(function ($log) use ($guid) {
+                    ->format(function ($log) use ($session) {
                         if (empty($log['direction'])) return Format::small(__('Not Taken'));
 
                         $output = '<b>'.__($log['direction']).'</b> ('.__($log['type']). (!empty($log['reason'])? ', '.__($log['reason']) : '') .')';
                         if (!empty($log['comment'])) {
-                            $output .= '&nbsp;<img title="'.$log['comment'].'" src="./themes/'.$_SESSION[$guid]['gibbonThemeName'].'/img/messageWall.png" width=16 height=16/>';
+                            $output .= '&nbsp;<img title="'.$log['comment'].'" src="./themes/'.$session->get'gibbonThemeName').'/img/messageWall.png" width=16 height=16/>';
                         }
 
                         return $output;
