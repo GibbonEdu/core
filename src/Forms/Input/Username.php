d037b488544d8695f82613b63eddeecb8a9687f0
Sandra Kuipers <sandra@skuipers.com>
2021-05-20 09:57:44 +0800

    System: update public registration form to use the Username class

--- src/Forms/Input/Username.php
+++ src/Forms/Input/Username.php
@@ -58,7 +58,7 @@ class Username extends TextField
     protected function getElement()
     {
         $this->maxLength(20)
-            ->uniqueField('./publicRegistrationCheck.php', ['currentUsername' => $this->getValue()])
+            ->uniqueField('./publicRegistrationCheck.php', ['fieldName' => 'username', 'currentUsername' => $this->getValue()])
             ->addValidation('Validate.Format', 'pattern: /^[a-zA-Z\u00C0-\u024F\u1E00-\u1EFF\u3040-\u309F\u3400-\u4DBF\u4E00-\u9FFF\u2B740–\u2B81F0-9_\-\.]*$/u, failureMessage: "'.__('Must be alphanumeric').'"');
 
         return parent::getElement();
