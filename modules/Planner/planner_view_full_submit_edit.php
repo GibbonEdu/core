96fd94f5b13dba0b4fb81bce42bd0379cd10891e
Sandra Kuipers <sandra@skuipers.com>
2021-06-08 13:41:55 +0800

    Planner: fix namespace-related PHP error

--- modules/Planner/planner_view_full_submit_edit.php
+++ modules/Planner/planner_view_full_submit_edit.php
@@ -19,7 +19,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 use Gibbon\Forms\Form;
 use Gibbon\Services\Format;
-use Gibbon\Services\Format;
 
 //Module includes
 require_once __DIR__ . '/moduleFunctions.php';
