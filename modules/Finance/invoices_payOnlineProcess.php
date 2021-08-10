2e7e14ec597326aaf3d784ea8fa31b2c48fbe3fb
Sandra Kuipers <sandra@skuipers.com>
2021-08-10 21:43:43 +0800

    Finance: implement Payment class in Online Invoice Payment

--- modules/Finance/invoices_payOnlineProcess.php
+++ modules/Finance/invoices_payOnlineProcess.php
@@ -18,89 +18,87 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Contracts\Comms\Mailer;
+use Gibbon\Contracts\Services\Payment;
 
 include '../../gibbon.php';
 
 include './moduleFunctions.php';
 
-$URL = $session->get('absoluteURL').'/index.php?q=/modules/Finance/invoices_payOnline.php';
+$gibbonFinanceInvoiceID = $_REQUEST['gibbonFinanceInvoiceID'] ?? '';
+$key = $_REQUEST['key'] ?? '';
 
-$paid = null;
-if (isset($_GET['paid'])) {
-    $paid = $_GET['paid'] ?? '';
-}
+$URL = $session->get('absoluteURL')."/index.php?q=/modules/Finance/invoices_payOnline.php&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key";
+$URLPayment = $session->get('absoluteURL')."/modules/Finance/invoices_payOnlineProcess.php?gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key";
 
-if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
-    //Get variables
-    $gibbonFinanceInvoiceID = $_POST['gibbonFinanceInvoiceID'] ?? '';
-    $key = $_POST['key'] ?? '';
+$payment = $container->get(Payment::class);
+$payment->setReturnURL($URLPayment);
+$payment->setCancelURL($URLPayment);
+$payment->setForeignTable('gibbonFinanceInvoice', $gibbonFinanceInvoiceID);
 
+if (!$payment->incomingPayment()) {
+    // No incoming payment, let's send a request
 
-    //Check variables
-    if ($gibbonFinanceInvoiceID == '' or $key == '') {
+    // Check variables
+    if (empty($gibbonFinanceInvoiceID)|| empty($key)) {
         $URL .= '&return=error1';
         header("Location: {$URL}");
-    } else {
-        //Check for record
-        $keyReadFail = false;
+        exit;
+    }
+
+    //Check for record
+    $keyReadFail = false;
+    try {
+        $dataKeyRead = array('gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID, 'key' => $key);
+        $sqlKeyRead = "SELECT * FROM gibbonFinanceInvoice WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID AND `key`=:key AND status='Issued'";
+        $resultKeyRead = $connection2->prepare($sqlKeyRead);
+        $resultKeyRead->execute($dataKeyRead);
+    } catch (PDOException $e) {
+        $URL .= '&return=error2';
+        header("Location: {$URL}");
+        exit();
+    }
+
+    if ($resultKeyRead->rowCount() != 1) { //If not exists, report error
+        $URL .= '&return=error2';
+        header("Location: {$URL}");
+        exit();
+    } else {    //If exists check confirmed
+        $rowKeyRead = $resultKeyRead->fetch();
+
+        //Get value of the invoice.
+        $feeOK = true;
         try {
-            $dataKeyRead = array('gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID, 'key' => $key);
-            $sqlKeyRead = "SELECT * FROM gibbonFinanceInvoice WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID AND `key`=:key AND status='Issued'";
-            $resultKeyRead = $connection2->prepare($sqlKeyRead);
-            $resultKeyRead->execute($dataKeyRead);
+            $dataFees['gibbonFinanceInvoiceID'] = $gibbonFinanceInvoiceID;
+            $sqlFees = 'SELECT gibbonFinanceInvoiceFee.gibbonFinanceInvoiceFeeID, gibbonFinanceInvoiceFee.feeType, gibbonFinanceFeeCategory.name AS category, gibbonFinanceInvoiceFee.name AS name, gibbonFinanceInvoiceFee.fee, gibbonFinanceInvoiceFee.description AS description, NULL AS gibbonFinanceFeeID, gibbonFinanceInvoiceFee.gibbonFinanceFeeCategoryID AS gibbonFinanceFeeCategoryID, sequenceNumber FROM gibbonFinanceInvoiceFee JOIN gibbonFinanceFeeCategory ON (gibbonFinanceInvoiceFee.gibbonFinanceFeeCategoryID=gibbonFinanceFeeCategory.gibbonFinanceFeeCategoryID) WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID ORDER BY sequenceNumber';
+            $resultFees = $connection2->prepare($sqlFees);
+            $resultFees->execute($dataFees);
         } catch (PDOException $e) {
+            $feeOK = false;
             $URL .= '&return=error2';
             header("Location: {$URL}");
             exit();
         }
 
-        if ($resultKeyRead->rowCount() != 1) { //If not exists, report error
-            $URL .= '&return=error2';
-            header("Location: {$URL}");
-            exit();
-        } else {    //If exists check confirmed
-            $rowKeyRead = $resultKeyRead->fetch();
-
-            //Get value of the invoice.
-            $feeOK = true;
-            try {
-                $dataFees['gibbonFinanceInvoiceID'] = $gibbonFinanceInvoiceID;
-                $sqlFees = 'SELECT gibbonFinanceInvoiceFee.gibbonFinanceInvoiceFeeID, gibbonFinanceInvoiceFee.feeType, gibbonFinanceFeeCategory.name AS category, gibbonFinanceInvoiceFee.name AS name, gibbonFinanceInvoiceFee.fee, gibbonFinanceInvoiceFee.description AS description, NULL AS gibbonFinanceFeeID, gibbonFinanceInvoiceFee.gibbonFinanceFeeCategoryID AS gibbonFinanceFeeCategoryID, sequenceNumber FROM gibbonFinanceInvoiceFee JOIN gibbonFinanceFeeCategory ON (gibbonFinanceInvoiceFee.gibbonFinanceFeeCategoryID=gibbonFinanceFeeCategory.gibbonFinanceFeeCategoryID) WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID ORDER BY sequenceNumber';
-                $resultFees = $connection2->prepare($sqlFees);
-                $resultFees->execute($dataFees);
-            } catch (PDOException $e) {
-                $feeOK = false;
-                $URL .= '&return=error2';
-                header("Location: {$URL}");
-                exit();
+        if ($feeOK == true) {
+            $feeTotal = 0;
+            while ($rowFees = $resultFees->fetch()) {
+                $feeTotal += $rowFees['fee'];
             }
 
-            if ($feeOK == true) {
-                $feeTotal = 0;
-                while ($rowFees = $resultFees->fetch()) {
-                    $feeTotal += $rowFees['fee'];
-                }
+            if ($payment->isEnabled() and $feeTotal > 0) {
+                $financeOnlinePaymentEnabled = getSettingByScope($connection2, 'Finance', 'financeOnlinePaymentEnabled');
+                $financeOnlinePaymentThreshold = getSettingByScope($connection2, 'Finance', 'financeOnlinePaymentThreshold');
+                if ($financeOnlinePaymentEnabled == 'Y') {
+                    if ($financeOnlinePaymentThreshold == '' or $financeOnlinePaymentThreshold >= $feeTotal) {
+                        // Let's make a payment
+                        $return = $payment->requestPayment($feeTotal, __('Invoice Number') .' '.$gibbonFinanceInvoiceID);
 
-                $currency = getSettingByScope($connection2, 'System', 'currency');
-                $enablePayments = getSettingByScope($connection2, 'System', 'enablePayments');
-                $paymentAPIUsername = getSettingByScope($connection2, 'System', 'paymentAPIUsername');
-                $paymentAPIPassword = getSettingByScope($connection2, 'System', 'paymentAPIPassword');
-                $paymentAPISignature = getSettingByScope($connection2, 'System', 'paymentAPISignature');
-
-                if ($enablePayments == 'Y' and $paymentAPIUsername != '' and $paymentAPIPassword != '' and $paymentAPISignature != '' and $feeTotal > 0) {
-                    $financeOnlinePaymentEnabled = getSettingByScope($connection2, 'Finance', 'financeOnlinePaymentEnabled');
-                    $financeOnlinePaymentThreshold = getSettingByScope($connection2, 'Finance', 'financeOnlinePaymentThreshold');
-                    if ($financeOnlinePaymentEnabled == 'Y') {
-                        if ($financeOnlinePaymentThreshold == '' or $financeOnlinePaymentThreshold >= $feeTotal) {
-                            //Let's call for the payment to be done!
-                            $session->set('gatewayCurrencyNoSupportReturnURL', $session->get('absoluteURL').'/index.php?q=/modules/Finance/invoices_payOnline.php&return=error3');
-                            $URL = $session->get('absoluteURL')."/lib/paypal/expresscheckout.php?Payment_Amount=$feeTotal&return=".urlencode("modules/Finance/invoices_payOnlineProcess.php?return=success1&paid=Y&feeTotal=$feeTotal&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key").'&fail='.urlencode("modules/Finance/invoices_payOnlineProcess?return=success2&paid=N&feeTotal=$feeTotal&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key");
-                            header("Location: {$URL}");
-                        } else {
-                            $URL .= '&return=error2';
-                            header("Location: {$URL}");
-                            exit();
+                        if (!empty($return)) {
+                            $URL .= '&return='.$return;
+                            header("Location: " . $URL);
+                            exit;
                         }
+
                     } else {
                         $URL .= '&return=error2';
                         header("Location: {$URL}");
@@ -111,44 +109,28 @@ if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
                     header("Location: {$URL}");
                     exit();
                 }
+            } else {
+                $URL .= '&return=error2';
+                header("Location: {$URL}");
+                exit();
             }
         }
+        
     }
-} else { //IF PAID IS Y WE ARE JUST RETURNING TO FINALISE PAYMENT AND RECORD OF PAYMENT, SO LET'S DO IT.
-    //Get returned paypal tokens, ids, etc
-    $paymentMade = 'N';
-    if ($_GET['return'] == 'success1') {
-        $paymentMade = 'Y';
-    }
-    $paymentToken = null;
-    if (isset($_GET['token'])) {
-        $paymentToken = $_GET['token'] ?? '';
-    }
-    $paymentPayerID = null;
-    if (isset($_GET['PayerID'])) {
-        $paymentPayerID = $_GET['PayerID'] ?? '';
-    }
-    $feeTotal = null;
-    if (isset($_GET['feeTotal'])) {
-        $feeTotal = $_GET['feeTotal'] ?? '';
-    }
-    $gibbonFinanceInvoiceID = '';
-    if (isset($_GET['gibbonFinanceInvoiceID'])) {
-        $gibbonFinanceInvoiceID = $_GET['gibbonFinanceInvoiceID'] ?? '';
-    }
-    $key = '';
-    if (isset($_GET['key'])) {
-        $key = $_GET['key'] ?? '';
-    }
+} else { 
+    // Handle incoming payment
+    
+    $gibbonFinanceInvoiceID = $_GET['gibbonFinanceInvoiceID'] ?? '';
+    $key = $_GET['key'] ?? '';
 
     $gibbonFinanceInvoiceeID = '';
     $invoiceTo = '';
     $gibbonSchoolYearID = '';
 
-        $dataKeyRead = array('gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID, 'key' => $key);
-        $sqlKeyRead = 'SELECT * FROM gibbonFinanceInvoice WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID AND `key`=:key';
-        $resultKeyRead = $connection2->prepare($sqlKeyRead);
-        $resultKeyRead->execute($dataKeyRead);
+    $dataKeyRead = array('gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID, 'key' => $key);
+    $sqlKeyRead = 'SELECT gibbonFinanceInvoice.gibbonFinanceInvoiceeID, gibbonFinanceInvoice.invoiceTo, gibbonFinanceInvoice.gibbonSchoolYearID FROM gibbonFinanceInvoice WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID AND `key`=:key';
+    $resultKeyRead = $connection2->prepare($sqlKeyRead);
+    $resultKeyRead->execute($dataKeyRead);
     if ($resultKeyRead->rowCount() == 1) {
         $rowKeyRead = $resultKeyRead->fetch();
         $gibbonFinanceInvoiceeID = $rowKeyRead['gibbonFinanceInvoiceeID'];
@@ -157,38 +139,26 @@ if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
     }
 
     //Check return values to see if we can proceed
-    if ($paymentToken == '' or $feeTotal == '' or $gibbonFinanceInvoiceID == '' or $key == '' or $gibbonFinanceInvoiceeID == '' or $invoiceTo = '' or $gibbonSchoolYearID == '') {
-        //Success $URL.="&addReturn=success2&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key" ;
+    if ($gibbonFinanceInvoiceID == '' or $key == '' or $gibbonFinanceInvoiceeID == '' or $invoiceTo = '' or $gibbonSchoolYearID == '') {
+        $URL .= '&return=warning3';
         header("Location: {$URL}");
         exit();
     } else {
         //PROCEED AND FINALISE PAYMENT
-        require '../../lib/paypal/paypalfunctions.php';
-
-        //Ask paypal to finalise the payment
-        $confirmPayment = confirmPayment($guid, $feeTotal, $paymentToken, $paymentPayerID);
-
-        $ACK = $confirmPayment['ACK'];
-        $paymentTransactionID = $confirmPayment['PAYMENTINFO_0_TRANSACTIONID'];
-        $paymentReceiptID = $confirmPayment['PAYMENTINFO_0_RECEIPTID'];
+        $return = $payment->confirmPayment();
+        $result = $payment->getPaymentResult();
+        $gibbonPaymentID = $result['gibbonPaymentID'];
 
         //Payment was successful. Yeah!
-        if ($ACK == 'Success') {
+        if ($result['success']) {
             $updateFail = false;
 
-            //Save payment details to gibbonPayment
-            $gibbonPaymentID = setPaymentLog($connection2, $guid, 'gibbonFinanceInvoice', $gibbonFinanceInvoiceID, 'Online', 'Complete', $feeTotal, 'Paypal', 'Success', $paymentToken, $paymentPayerID, $paymentTransactionID, $paymentReceiptID);
-
-            //Link gibbonPayment record to gibbonApplicationForm, and make note that payment made
-            if ($gibbonPaymentID != '') {
-                try {
-                    $data = array('paidDate' => date('Y-m-d'), 'paidAmount' => $feeTotal, 'gibbonPaymentID' => $gibbonPaymentID, 'gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID);
-                    $sql = "UPDATE gibbonFinanceInvoice SET status='Paid', paidDate=:paidDate, paidAmount=:paidAmount, gibbonPaymentID=:gibbonPaymentID WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID";
-                    $result = $connection2->prepare($sql);
-                    $result->execute($data);
-                } catch (PDOException $e) {
-                    $updateFail = true;
-                }
+            //Link gibbonPayment record to gibbonFinanceInvoice, and make note that payment made
+            if (!empty($gibbonPaymentID)) {
+                $data = array('paidDate' => date('Y-m-d'), 'paidAmount' => $result['amount'], 'gibbonPaymentID' => $gibbonPaymentID, 'gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID);
+                $sql = "UPDATE gibbonFinanceInvoice SET status='Paid', paidDate=:paidDate, paidAmount=:paidAmount, gibbonPaymentID=:gibbonPaymentID WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID";
+                $result = $connection2->prepare($sql);
+                $result->execute($data);
             } else {
                 $updateFail = true;
             }
@@ -205,10 +175,10 @@ if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
             $emailsCount = 0;
             if ($invoiceTo == 'Company') {
 
-                    $dataCompany = array('gibbonFinanceInvoiceeID' => $gibbonFinanceInvoiceeID);
-                    $sqlCompany = 'SELECT * FROM gibbonFinanceInvoicee WHERE gibbonFinanceInvoiceeID=:gibbonFinanceInvoiceeID';
-                    $resultCompany = $connection2->prepare($sqlCompany);
-                    $resultCompany->execute($dataCompany);
+                $dataCompany = array('gibbonFinanceInvoiceeID' => $gibbonFinanceInvoiceeID);
+                $sqlCompany = 'SELECT * FROM gibbonFinanceInvoicee WHERE gibbonFinanceInvoiceeID=:gibbonFinanceInvoiceeID';
+                $resultCompany = $connection2->prepare($sqlCompany);
+                $resultCompany->execute($dataCompany);
                 if ($resultCompany->rowCount() != 1) {
                 } else {
                     $rowCompany = $resultCompany->fetch();
@@ -265,10 +235,10 @@ if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
             if (count($emails) > 0) {
                 //Get receipt number
 
-                    $dataPayments = array('foreignTable' => 'gibbonFinanceInvoice', 'foreignTableID' => $gibbonFinanceInvoiceID);
-                    $sqlPayments = 'SELECT gibbonPayment.*, surname, preferredName FROM gibbonPayment JOIN gibbonPerson ON (gibbonPayment.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE foreignTable=:foreignTable AND foreignTableID=:foreignTableID ORDER BY timestamp, gibbonPaymentID';
-                    $resultPayments = $connection2->prepare($sqlPayments);
-                    $resultPayments->execute($dataPayments);
+                $dataPayments = array('foreignTable' => 'gibbonFinanceInvoice', 'foreignTableID' => $gibbonFinanceInvoiceID);
+                $sqlPayments = 'SELECT gibbonPayment.*, surname, preferredName FROM gibbonPayment JOIN gibbonPerson ON (gibbonPayment.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE foreignTable=:foreignTable AND foreignTableID=:foreignTableID ORDER BY timestamp, gibbonPaymentID';
+                $resultPayments = $connection2->prepare($sqlPayments);
+                $resultPayments->execute($dataPayments);
                 $receiptCount = $resultPayments->rowCount();
 
                 //Prep message
@@ -297,21 +267,19 @@ if ($paid != 'Y') { //IF PAID IS NOT Y, LET'S REDIRECT TO MAKE PAYMENT
             $URL .= "&return=success1&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key";
             header("Location: {$URL}");
         } else {
+            if ($return == 'warning3') {
+                $URL .= "&return=warning3&gibbonFinanceInvoiceID=$gibbonFinanceInvoiceID&key=$key";
+                header("Location: {$URL}");
+                exit;
+            }
             $updateFail = false;
 
-            //Save payment details to gibbonPayment
-            $gibbonPaymentID = setPaymentLog($connection2, $guid, 'gibbonFinanceInvoice', $gibbonFinanceInvoiceID, 'Online', 'Failure', $feeTotal, 'Paypal', 'Failure', $paymentToken, $paymentPayerID, $paymentTransactionID, $paymentReceiptID);
-
             //Link gibbonPayment record to gibbonApplicationForm, and make note that payment made
-            if ($gibbonPaymentID != '') {
-                try {
-                    $data = array('gibbonPaymentID' => $gibbonPaymentID, 'gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID);
-                    $sql = 'UPDATE gibbonFinanceInvoice gibbonPaymentID=:gibbonPaymentID WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID';
-                    $result = $connection2->prepare($sql);
-                    $result->execute($data);
-                } catch (PDOException $e) {
-                    $updateFail = true;
-                }
+            if (!empty($gibbonPaymentID)) {
+                $data = array('gibbonPaymentID' => $gibbonPaymentID, 'gibbonFinanceInvoiceID' => $gibbonFinanceInvoiceID);
+                $sql = 'UPDATE gibbonFinanceInvoice gibbonPaymentID=:gibbonPaymentID WHERE gibbonFinanceInvoiceID=:gibbonFinanceInvoiceID';
+                $result = $connection2->prepare($sql);
+                $result->execute($data);
             } else {
                 $updateFail = true;
             }
