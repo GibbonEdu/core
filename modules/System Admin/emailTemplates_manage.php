e2d46d3e0306f6cf9ded221a20cb9d294d60c9b4
Sandra Kuipers <sandra@skuipers.com>
2021-08-26 08:20:14 +0800

    System Admin: post-merge namespace fixes for #1405

--- modules/System Admin/emailTemplates_manage.php
+++ modules/System Admin/emailTemplates_manage.php
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use Gibbon\Forms\Form;
 use Gibbon\Services\Format;
 use Gibbon\Tables\DataTable;
 use Gibbon\Domain\System\EmailTemplateGateway;
--- modules/System Admin/emailTemplates_manage_duplicateProcess.php
+++ modules/System Admin/emailTemplates_manage_duplicateProcess.php
@@ -17,9 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use Gibbon\Contracts\Comms\Mailer;
 use Gibbon\Domain\System\EmailTemplateGateway;
-use Gibbon\Comms\EmailTemplate;
 
 require_once '../../gibbon.php';
 
