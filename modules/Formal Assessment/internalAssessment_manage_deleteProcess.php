585e03ec4729ded50cc2b3e0d2efbceecb3fef89
hmerrettICHK <74997253+hmerrettICHK@users.noreply.github.com>
2021-09-13 15:20:12 +0800

    Formal Assessment, Individual Needs, Library: null coalesce refactoring (#1417)
    * Timetable - updating module functions to add red/blue week colours in View Timetable by Facility
    
    * Null coalesce second pass - Formal Assessment, Individual Needs, Library
    
    * Revert changes to timetable
    
    * Formal Assessment, Individual Needs, Library: additional null coalesce and URL work
    
    * Library: null coalesce fix
    
    * Library: null coalesce fix
    
    * Update library_lending_item_renewProcess.php
    
    Co-authored-by: Wren Merrett <wmerrett@ichk.edu.hk>
    Co-authored-by: Sandra Kuipers <sandra@skuipers.com>

--- modules/Formal Assessment/internalAssessment_manage_deleteProcess.php
+++ modules/Formal Assessment/internalAssessment_manage_deleteProcess.php
@@ -21,8 +21,9 @@ include '../../gibbon.php';
 
 $gibbonCourseClassID = $_POST['gibbonCourseClassID'] ?? '';
 $gibbonInternalAssessmentColumnID = $_GET['gibbonInternalAssessmentColumnID'] ?? '';
-$URL = $session->get('absoluteURL').'/index.php?q=/modules/'.getModuleName($_POST['address'])."/internalAssessment_manage_delete.php&gibbonInternalAssessmentColumnID=$gibbonInternalAssessmentColumnID&gibbonCourseClassID=$gibbonCourseClassID";
-$URLDelete = $session->get('absoluteURL').'/index.php?q=/modules/'.getModuleName($_POST['address'])."/internalAssessment_manage.php&gibbonCourseClassID=$gibbonCourseClassID";
+$address = $_POST['address'] ?? '';
+$URL = $session->get('absoluteURL').'/index.php?q=/modules/'.getModuleName($address)."/internalAssessment_manage_delete.php&gibbonInternalAssessmentColumnID=$gibbonInternalAssessmentColumnID&gibbonCourseClassID=$gibbonCourseClassID";
+$URLDelete = $session->get('absoluteURL').'/index.php?q=/modules/'.getModuleName($address)."/internalAssessment_manage.php&gibbonCourseClassID=$gibbonCourseClassID";
 
 if (isActionAccessible($guid, $connection2, '/modules/Formal Assessment/internalAssessment_manage_delete.php') == false) {
     $URL .= '&return=error0';
