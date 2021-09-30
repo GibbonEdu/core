3ac003c5acdab5beb5fd5c3ccf5eda37ce3c55cd
Ross Parker <>
2021-09-30 17:58:25 +0800

    System: added ability to run CLI scripts remotely, with secure key - WIP

--- cli/schoolAdmin_parentDailyEmailSummary.php
+++ cli/schoolAdmin_parentDailyEmailSummary.php
@@ -33,7 +33,10 @@ getSystemSettings($guid, $connection2);
 setCurrentSchoolYear($guid, $connection2);
 Format::setupFromSession($container->get('session'));
 
-if (!isCommandLineInterface()) {
+$remoteCLIKey = getSettingByScope($connection2, 'System Admin', 'remoteCLIKey');
+$remoteCLIKeyInput = $_GET['remoteCLIKey'] ?? null;
+
+if (!isCommandLineInterface() OR (!empty($remoteCLIKey) AND $remoteCLIKey != $remoteCLIKeyInput)) {
     echo __('This script cannot be run from a browser, only via CLI.');
     return;
 }
