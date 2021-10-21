c48385e7739bddaa1ca4ad909009843aa000d3ab
Sandra Kuipers <sandra@skuipers.com>
2021-10-21 15:26:48 +0800

    Behaviour: update behaviour letters to use email templates, rename related settings (#1479)
    * System Admin: add Email Templates for negative behaviour letters
    
    * School Admin: remove settings for Behaviour Letter templates
    
    * School Admin: rename existing Behaviour Letter settings to add Negative keywords

--- tests/acceptance/School Admin/BehaviourSettingsCept.php
+++ tests/acceptance/School Admin/BehaviourSettingsCept.php
@@ -17,13 +17,10 @@ $newFormValues = array(
     'negativeDescriptors'           => 'Negative1,Negative2,Negative3',
     'enableLevels'                  => 'Y',
     'levels'                        => 'Level1,Level2,Level3',
-    'enableBehaviourLetters'        => 'Y',
-    'behaviourLettersLetter1Count'  => '4',
-    'behaviourLettersLetter1Text'   => 'Letter Test 1',
-    'behaviourLettersLetter2Count'  => '8',
-    'behaviourLettersLetter2Text'   => 'Letter Test 2',
-    'behaviourLettersLetter3Count'  => '12',
-    'behaviourLettersLetter3Text'   => 'Letter Test 3',
+    'enableNegativeBehaviourLetters'        => 'Y',
+    'behaviourLettersNegativeLetter1Count'  => '4',
+    'behaviourLettersNegativeLetter2Count'  => '8',
+    'behaviourLettersNegativeLetter3Count'  => '12',
     'policyLink'                    => 'http://test.test',
 );
 
@@ -39,7 +36,7 @@ $I->seeInFormFields('#content form', $newFormValues);
 $resetFormValues = $originalFormValues;
 $resetFormValues['enableDescriptors'] = 'Y';
 $resetFormValues['enableLevels'] = 'Y';
-$resetFormValues['enableBehaviourLetters'] = 'Y';
+$resetFormValues['enableNegativeBehaviourLetters'] = 'Y';
 $I->submitForm('#content form', $resetFormValues, 'Submit');
 
 // Restore Original Settings -----------------------------------
