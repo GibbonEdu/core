850c423796dad6c0e91aaf89ba728933583862e9
Sandra Kuipers <sandra@skuipers.com>
2021-10-27 13:57:02 +0800

    Update messenger_post.php

--- modules/Messenger/messenger_post.php
+++ modules/Messenger/messenger_post.php
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use Gibbon\Domain\System\SettingGateway;
 use Gibbon\Forms\Form;
 use Gibbon\Contracts\Comms\SMS;
 use Gibbon\Services\Format;
