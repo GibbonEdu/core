ff0f3d2f15a1eb70704975708ef26eebf3706b81
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-02 14:41:19 +0800

    Delete tests directory

--- tests/acceptance/WelcomeCept.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('ensure the frontpage works');
-$I->amOnPage('/');
-$I->see('Welcome', 'h2');
-$I->see('Powered by Gibbon');
--- tests/config/data/invalid.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "This is a mock php file for uploads testing.";
-?>
