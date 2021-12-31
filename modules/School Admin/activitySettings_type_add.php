9264d01fca10249ae217f9404618da84f055aa87
Sandra Kuipers <sandra@skuipers.com>
2021-12-31 13:24:07 +0800

    School Admin: fix missing SessionGateway in Add Activity Type

--- modules/School Admin/activitySettings_type_add.php
+++ modules/School Admin/activitySettings_type_add.php
@@ -18,6 +18,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 use Gibbon\Forms\Form;
+use Gibbon\Domain\System\SettingGateway;
 
 if (isActionAccessible($guid, $connection2, '/modules/School Admin/activitySettings_type_add.php') == false) {
     // Access denied
