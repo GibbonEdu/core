27c60983c05e323432678e14c2bd742e0b9cc508
Sandra Kuipers <sandra@skuipers.com>
2022-01-20 14:10:41 +0800

    Planner: fix posting chat on the lesson planner

--- modules/Planner/planner_view_full_postProcess.php
+++ modules/Planner/planner_view_full_postProcess.php
@@ -64,7 +64,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Planner/planner_view_full.
                 $row = $result->fetch();
 
                 //INSERT
-                $replyTo = $_POST['replyTo'] ?? null;
+                $replyTo = !empty($_POST['replyTo']) ? $_POST['replyTo'] : null;
                 $comment = $_POST['comment'] ?? '';
                 
                 try {
