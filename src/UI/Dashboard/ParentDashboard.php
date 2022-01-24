4a735c66187d43a512746512c14ab70faefe6d36
Sandra Kuipers <sandra@skuipers.com>
2022-01-24 08:51:58 +0800

    System: sanitise dashboard input and tab selection

--- src/UI/Dashboard/ParentDashboard.php
+++ src/UI/Dashboard/ParentDashboard.php
@@ -845,12 +845,9 @@ class ParentDashboard implements OutputableInterface, ContainerAwareInterface
             $return .= '</div>';
         }
 
+        $defaultTab = preg_replace('/[^0-9]/', '', $_GET['tab'] ?? 0);
 
-        $defaultTab = 0;
-        if (isset($_GET['tab'])) {
-            $defaultTab = $_GET['tab'];
-        }
-        else if (!is_null($parentDashboardDefaultTabCount)) {
+        if (!isset($_GET['tab']) && !is_null($parentDashboardDefaultTabCount)) {
             $defaultTab = $parentDashboardDefaultTabCount-1;
         }
         $return .= "<script type='text/javascript'>";
