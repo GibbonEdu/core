cdeb32201ba3c3d3dba3890f78420da38f8eae0b
Ross Parker <>
2022-03-10 07:28:02 +0800

    System: fixed text field autocomplete issue caused by use of " in existing values

--- src/Forms/Input/TextField.php
+++ src/Forms/Input/TextField.php
@@ -117,18 +117,18 @@ class TextField extends Input
         return false;
     }
 
-    
+
     /**
      * Gets the HTML output for this form element.
      * @return  string
      */
     protected function getElement()
     {
-        
+
         $output = '<input type="text" '.$this->getAttributeString().'>';
-        
+
         if (!empty($this->autocomplete)) {
-            $source = implode(',', array_map(function ($str) { return sprintf('"%s"', $str); }, $this->autocomplete));
+            $source = implode(',', array_map(function ($str) { return sprintf('"%s"', addslashes($str)); }, $this->autocomplete));
             $output .= '<script type="text/javascript">';
             $output .= '$("#'.$this->getID().'").autocomplete({source: ['.$source.']});';
             $output .= '</script>';
