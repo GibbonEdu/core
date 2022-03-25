9fe9170b3131dd89699066dc49406a1472a800fc
Sandra Kuipers <sandra@skuipers.com>
2022-03-25 11:17:05 +0800

    System: start v23.0.02

--- version.php
+++ version.php
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$version = '23.0.01';
+$version = '23.0.02';
 
 /**
  * System Requirements
