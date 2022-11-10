769ffaebc26cd9a11cbff503e6dbaa65be0b0958
Sandra Kuipers <sandra@skuipers.com>
2022-11-10 10:39:51 +0800

    Staff: fix PHP notice in staff absence pages

--- modules/Staff/src/Tables/AbsenceFormats.php
+++ modules/Staff/src/Tables/AbsenceFormats.php
@@ -57,7 +57,7 @@ class AbsenceFormats
     {
         $output = Format::dateRangeReadable($absence['dateStart'], $absence['dateEnd']);
         if ($absence['allDay'] == 'Y' || $absence['days'] > 1) {
-            $output .= $absence['value'] != $absence['days']
+            $output .= !empty($absence['value']) && $absence['value'] != $absence['days']
                 ? '<br/>'.Format::small(__('{total} Total (across {count} Days)', ['total' => round($absence['value'], 1), 'count' => $absence['days']]))
                 : '<br/>'.Format::small(__n('{count} Day', '{count} Days', $absence['days']));
         } else {
