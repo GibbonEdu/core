2bf344c74b0f5e582493c8ba64d7bc1b750cafd3
Sandra Kuipers <sandra@skuipers.com>
2023-03-20 09:31:40 +0800

    Attendance: update the last attendance string in Form Groups not Registered

--- modules/Attendance/report_formGroupsNotRegistered_byDate.php
+++ modules/Attendance/report_formGroupsNotRegistered_byDate.php
@@ -275,7 +275,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Attendance/report_formGrou
                     $finalLog = current($logAll);
                     $person = $container->get(UserGateway::class)->getByID($finalLog['gibbonPersonIDTaker'], ['preferredName', 'surname']);
 
-                    echo Format::alert(__('Form group attendance was last taken for {formGroup} by {person} at {time}', [
+                    echo Format::alert(__('The last Form Group attendance recorded was {formGroup}, first taken by {person} at {time}', [
                         'formGroup' => $finalLog['nameShort'],
                         'person' => Format::name('', $person['preferredName'], $person['surname'], 'Staff', false, true),
                         'time' => Format::time($finalLog['timestampTaken']),
