16638b849220dd24ed1e536b44b76e222ae0f6c0
Sandra Kuipers <sandra@skuipers.com>
2023-05-09 11:26:04 +0800

    Rubrics: fixed include file path in visualize save script

--- modules/Rubrics/rubrics_visualise_saveAjax.php
+++ modules/Rubrics/rubrics_visualise_saveAjax.php
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-require_once "../../../gibbon.php";
+require_once "../../gibbon.php";
 
 $img = $_POST['img'] ?? null;
 $imgPath = $_POST['path'] ?? null;
