aa7c96ff646be33466a81cd9ddb540a53ff5eb6a
Ross Parker <>
2023-09-22 13:13:12 +0800

    System: transferred copyright from Ross Parker to the Gibbon Foundation

--- tests/unit/View/PageTest.php
+++ tests/unit/View/PageTest.php
@@ -1,7 +1,9 @@
 <?php
 /*
-Gibbon, Flexible & Open School System
-Copyright (C) 2010, Ross Parker
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
