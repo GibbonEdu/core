bee125255be0d76c590fcc397451ac863471fb27
Sandra Kuipers <sandra@skuipers.com>
2023-09-26 14:51:42 +0800

    System: fix missing line breaks in code error alerts

--- src/View/Page.php
+++ src/View/Page.php
@@ -308,7 +308,7 @@ class Page extends View
      */
     public function addAlert(string $text, string $context = 'message')
     {
-        $this->alerts[$context][] = strip_tags($text, '<a><b><i><u><strong><br>');
+        $this->alerts[$context][] = strip_tags($text, '<a><b><i><u><strong><br><ul><ol><li>');
     }
 
     /**
