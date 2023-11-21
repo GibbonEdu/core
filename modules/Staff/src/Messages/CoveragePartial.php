8eea6f2ae97a1381769eb6fc370956d442a14f93
Koala Yeung <koalay@gmail.com>
2023-11-21 13:40:27 +0800

    refactor: dateIntlReadable refactor 12
    Refactor with unit test verification
    * modules/Staff/src/Messages/CoveragePartial.php

--- modules/Staff/src/Messages/CoveragePartial.php
+++ modules/Staff/src/Messages/CoveragePartial.php
@@ -34,7 +34,7 @@ class CoveragePartial extends Message
         $this->coverage = $coverage;
 
         $this->uncoveredDates = array_map(function ($date) {
-            return Format::dateReadable($date, '%b %e');
+            return Format::dateIntlReadable($date, 'MMM d');
         }, $uncoveredDates);
     }
 
