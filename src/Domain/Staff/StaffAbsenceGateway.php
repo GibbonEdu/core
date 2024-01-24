f8cd583327d124488b11a489ee4d92800d48b3fa
Sandra Kuipers <sandra@skuipers.com>
2024-01-24 14:11:08 +0800

    Update src/Domain/Staff/StaffAbsenceGateway.php

--- src/Domain/Staff/StaffAbsenceGateway.php
+++ src/Domain/Staff/StaffAbsenceGateway.php
@@ -173,7 +173,6 @@ class StaffAbsenceGateway extends QueryableGateway implements ScrubbableGateway
             ->leftJoin('gibbonPerson AS coverage', 'gibbonStaffCoverage.gibbonPersonIDCoverage=coverage.gibbonPersonID')
             ->where('gibbonStaffAbsenceDate.date BETWEEN :dateStart AND :dateEnd')
             ->where("gibbonStaffAbsence.status = 'Approved'")
-            //->where("gibbonPerson.status = 'Full'")
             ->bindValue('dateStart', $dateStart)
             ->bindValue('dateEnd', $dateEnd)
             ->groupBy(['gibbonStaffAbsence.gibbonStaffAbsenceID', 'gibbonStaffCoverageDate.gibbonStaffCoverageDateID']);
