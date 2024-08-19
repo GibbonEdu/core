6037e296ca8a4ff0f86035db0566a64f5da61e31
Sandra Kuipers <skuipers@ichk.edu.hk>
2024-08-19 11:06:38 +0800

    Activities: string updates

--- modules/Activities/explore_category.php
+++ modules/Activities/explore_category.php
@@ -54,7 +54,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Activities/explore_categor
     }
 
     if ($category['viewable'] != 'Y' && !$canViewInactive) {
-        $page->addMessage(__m('This event is not viewable at this time. Please return to the Events page to explore a different event.'));
+        $page->addMessage(__m('This activity is not viewable at this time. Please return to the categories page to explore a different activity.'));
         return;
     }
 
