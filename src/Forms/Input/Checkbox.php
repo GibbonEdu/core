405b9b1e4f3c1f2a27a219855d3d4ecd0a26eac8
Sandra Kuipers <skuipers@ichk.edu.hk>
2024-10-01 11:33:37 +0800

    System: begin removing baked-in UI styles

--- src/Forms/Input/Checkbox.php
+++ src/Forms/Input/Checkbox.php
@@ -285,7 +285,7 @@ class Checkbox extends Input
 
                     if ($value != 'on') $this->setValue($value);
 
-                    $this->addClass('w-4 h-4');
+                    $this->addClass('h-4 w-4 rounded border-gray-400 text-blue-500 focus:ring-blue-600');
 
                     if ($this->inline) {
                         $output .= '<input type="checkbox" '.$this->getAttributeString().'>&nbsp;';
