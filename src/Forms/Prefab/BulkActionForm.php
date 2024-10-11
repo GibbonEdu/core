e5fb94ec0feeb2ba1c1af05aedfa269833911600
Sandra Kuipers <skuipers@ichk.edu.hk>
2024-10-11 20:23:52 +0800

    Forms: remove the outdated FormRenderer class

--- src/Forms/Prefab/BulkActionForm.php
+++ src/Forms/Prefab/BulkActionForm.php
@@ -22,8 +22,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 namespace Gibbon\Forms\Prefab;
 
 use Gibbon\Forms\Form;
-use Gibbon\Forms\FormFactory;
-use Gibbon\Forms\FormRenderer;
 use Gibbon\Forms\View\FormBlankView;
 
 /**
