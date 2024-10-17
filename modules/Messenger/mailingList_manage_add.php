46759194414bae64b7cf217a26072d3942c2ddb7
Ross Parker <>
2024-10-17 14:55:22 +0800

    Messenger: added new Mailing List feature

---
+++ modules/Messenger/mailingList_manage_add.php
@@ -0,0 +1,60 @@
+<?php
+/*
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+use Gibbon\Forms\Form;
+
+$page->breadcrumbs
+    ->add(__('Manage Mailing Lists'), 'mailingList_manage.php')
+    ->add(__('Add Recipient'));
+
+if (isActionAccessible($guid, $connection2, '/modules/Messenger/mailingList_manage_add.php') == false) {
+    // Access denied
+    $page->addError(__('You do not have access to this action.'));
+} else {
+    //Proceed!
+    $editLink = '';
+    if (isset($_GET['editID'])) {
+        $editLink = $session->get('absoluteURL').'/index.php?q=/modules/Messenger/mailingList_manage_edit.php&gibbonMessengerMailingListID='.$_GET['editID'];
+    }
+    $page->return->setEditLink($editLink);
+	
+	$form = Form::create('mailingList', $session->get('absoluteURL').'/modules/'.$session->get('module').'/mailingList_manage_addProcess.php');
+                
+	$form->addHiddenValue('address', $session->get('address'));
+
+	$row = $form->addRow();
+		$row->addLabel('surname', __('Surname'));
+		$row->addTextField('surname')->required()->maxLength(60);
+
+	$row = $form->addRow();
+		$row->addLabel('preferredName', __('Preferred Name'));
+		$row->addTextField('preferredName')->required()->maxLength(60);
+
+	$row = $form->addRow();
+		$row->addLabel('email', __('Email'))->description(__('Must be unique.'));
+		$row->addEmail('email')->required()->maxLength(75);
+
+	$row = $form->addRow();
+		$row->addFooter();
+		$row->addSubmit();
+
+	echo $form->getOutput();
+}
