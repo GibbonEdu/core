ad0fafd6f8f6ae6c22f34d9c8102f4f41d3a56d7
Sandra Kuipers <skuipers@ichk.edu.hk>
2024-10-31 12:00:04 +0800

    Messenger: replace mb_str_pad (only 8.3+) with str_pad

--- modules/Messenger/messenger_sendProcess.php
+++ modules/Messenger/messenger_sendProcess.php
@@ -68,7 +68,7 @@ if (isActionAccessible($guid, $connection2, "/modules/Messenger/messenger_post.p
     // Check which recipients who have been manually unchecked
     $recipientList = $_POST['gibbonMessengerReceiptID'] ?? []; 
     for ($i = 0; $i < count($recipientList); $i++) {
-        $recipientList[$i] = mb_str_pad($recipientList[$i], 14, "0", STR_PAD_LEFT);
+        $recipientList[$i] = str_pad($recipientList[$i], 14, "0", STR_PAD_LEFT);
     }
     $recipients = $messengerReceiptGateway->selectMessageRecipientList($gibbonMessengerID)->fetchAll();
     $unselected = array_diff(array_column($recipients, 'gibbonMessengerReceiptID'), $recipientList);
