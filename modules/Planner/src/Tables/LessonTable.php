c4b0c921ca069ead4432c0d321cef55370ec5673
Sandra Kuipers <skuipers@ichk.edu.hk>
2024-11-26 10:15:33 +0800

    Planner: fixed planner actions to return to Year Overview page if they originated there

--- modules/Planner/src/Tables/LessonTable.php
+++ modules/Planner/src/Tables/LessonTable.php
@@ -167,6 +167,7 @@ class LessonTable
             ->addParam('timeEnd')
             ->addParam('search', $gibbonPersonID)
             ->addParam('viewBy', $viewBy == 'date' ? 'date' : 'class')
+            ->addParam('subView', $viewBy == 'year' ? 'year' : 'lesson')
             ->format(function ($values, $actions) use ($editAccess, $highestAction, $viewBy) {
                 $fullEditAccess = $editAccess && ($highestAction == 'Lesson Planner_viewEditAllClasses' || !empty($values['isTeacher']));
 
