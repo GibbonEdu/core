7e94054fee08da322fa33c31ca8c8790ed256f9d
Sandra Kuipers <skuipers@ichk.edu.hk>
2025-05-19 13:27:20 +0800

    Staff: enable viewing full family details for one's own family only

--- modules/Staff/staff_view_details.php
+++ modules/Staff/staff_view_details.php
@@ -405,7 +405,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Staff/staff_view_details.p
 
                         echo $page->fetchFromTemplate('profile/family.twig.html', [
                             'families' => $families,
-                            'fullDetails' => false,
+                            'fullDetails' => $gibbonPersonID == $session->get('gibbonPersonID'),
                         ]);
                     } elseif ($subpage == 'Facilities') {
                         $staffFacilityGateway = $container->get(StaffFacilityGateway::class);
