19cfb6345ef28f4395ec5ef5c2d3cda6c66f54ff
Ali Alam <aalam@ichk.edu.hk>
2026-01-14 11:15:17 +0800

    Staff: fixed the issue to allow editing coverage request

--- modules/Staff/coverage_manage_editProcess.php
+++ modules/Staff/coverage_manage_editProcess.php
@@ -49,8 +49,7 @@ if (isActionAccessible($guid, $connection2, '/modules/Staff/coverage_manage_edit
     }
 
     $data = [
-        'reason'      => $_POST['reason'],
-        'notesStatus' => $_POST['notesStatus'],
+        'notesStatus' => $_POST['notesStatus'] ?? '',
     ];
 
     // Update the coverage
