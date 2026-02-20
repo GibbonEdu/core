f83f8d116bb58c36269d0cd8bcae4f7143e3cb30
Sandra Kuipers <skuipers@ichk.edu.hk>
2026-02-20 16:46:35 +0800

    Tests: add tests for Markbook

---
+++ tests/acceptance/Markbook/AddMultipleMarkbookColumnsCept.php
@@ -0,0 +1,52 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('add multiple markbook columns across classes');
+$I->loginAsAdmin();
+
+// Navigate to Markbook Edit page
+$I->amOnModulePage('Markbook', 'markbook_view.php');
+$I->seeBreadcrumb('View Markbook');
+
+$I->selectFromDropdown('gibbonCourseClassID', 2);
+$I->click('Go', '#searchForm');
+
+$gibbonCourseClassID = $I->grabValueFromURL('gibbonCourseClassID');
+
+// Add Multiple Columns ------------------------------------------------
+$I->amOnModulePage('Markbook', 'markbook_edit_addMulti.php', array('gibbonCourseClassID' => $gibbonCourseClassID));
+$I->seeBreadcrumb('Add Multiple Columns');
+
+// Select multiple classes (including current class)
+$I->selectFromDropdown('gibbonCourseClassIDMulti', 1);
+$I->selectFromDropdown('gibbonCourseClassIDMulti', 2);
+
+$formValues = array(
+    'name'                     => 'Multi Test Column',
+    'description'              => 'This is a multi-class test column.',
+    'type'                     => 'Homework',
+    'attainment'               => 'N',
+    'effort'                   => 'N',
+    'comment'                  => 'N',
+    'uploadedResponse'         => 'N',
+    'viewableStudents'         => 'N',
+    'viewableParents'          => 'N',
+);
+
+$I->submitForm('#content form', $formValues, 'Submit');
+$I->seeSuccessMessage();
+
+// Verify column was created by navigating back to markbook edit
+$I->amOnModulePage('Markbook', 'markbook_edit.php', array('gibbonCourseClassID' => $gibbonCourseClassID));
+$I->see('Multi Test Column');
+
+// Get the column ID for cleanup
+$gibbonMarkbookColumnID = $I->grabFromDatabase('gibbonMarkbookColumn', 'gibbonMarkbookColumnID', array('name' => 'Multi Test Column', 'gibbonCourseClassID' => $gibbonCourseClassID));
+
+// Clean up - Delete the created column
+$I->amOnModulePage('Markbook', 'markbook_edit_delete.php', array(
+    'gibbonCourseClassID' => $gibbonCourseClassID,
+    'gibbonMarkbookColumnID' => $gibbonMarkbookColumnID
+));
+
+$I->click('Delete');
+$I->seeSuccessMessage();
---
+++ tests/acceptance/Markbook/ManageWeightingsCept.php
@@ -0,0 +1,83 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('manage markbook weightings');
+$I->loginAsAdmin();
+
+// Enable Column Weighting Setting ---------------------------------
+$I->amOnModulePage('School Admin', 'markbookSettings.php');
+$originalMarkbookSettings = $I->grabAllFormValues();
+
+$newMarkbookSettings = array_replace($originalMarkbookSettings, array(
+    'enableColumnWeighting' => 'Y',
+));
+
+$I->submitForm('#content form', $newMarkbookSettings, 'Submit');
+$I->seeSuccessMessage();
+
+// Navigate to Markbook View
+$I->amOnModulePage('Markbook', 'markbook_view.php');
+$I->seeBreadcrumb('View Markbook');
+
+$I->selectFromDropdown('gibbonCourseClassID', 2);
+$I->click('Go', '#searchForm');
+
+$gibbonCourseClassID = $I->grabValueFromURL('gibbonCourseClassID');
+
+// Navigate to Manage Weightings
+$I->amOnModulePage('Markbook', 'weighting_manage.php', array('gibbonCourseClassID' => $gibbonCourseClassID));
+$I->see('Weightings');
+
+// Add Weighting ------------------------------------------------
+$I->clickNavigation('Add');
+$I->see('Add Weighting');
+
+$formValues = array(
+    'type'                     => 'Test Weighting',
+    'description'              => 'This is a test weighting.',
+    'weighting'                => '50.00',
+    'calculate'                => 'term',
+    'reportable'               => 'Y',
+);
+
+$I->submitForm('#content form', $formValues, 'Submit');
+$I->seeSuccessMessage();
+
+$gibbonMarkbookWeightID = $I->grabEditIDFromURL();
+
+// Edit ------------------------------------------------
+$I->amOnModulePage('Markbook', 'weighting_manage_edit.php', array(
+    'gibbonMarkbookWeightID' => $gibbonMarkbookWeightID,
+    'gibbonCourseClassID' => $gibbonCourseClassID
+));
+$I->see('Edit Weighting');
+
+$I->seeInFormFields('#content form', array(
+    'description' => 'This is a test weighting.',
+    'calculate' => 'term',
+));
+
+$editFormValues = array(
+    'type'                     => 'Updated Weighting',
+    'description'              => 'This is an updated weighting.',
+    'weighting'                => '60',
+    'calculate'                => 'year',
+    'reportable'               => 'N',
+);
+
+$I->submitForm('#content form', $editFormValues, 'Submit');
+$I->seeSuccessMessage();
+
+// Delete ------------------------------------------------
+$I->amOnModulePage('Markbook', 'weighting_manage_delete.php', array(
+    'gibbonMarkbookWeightID' => $gibbonMarkbookWeightID,
+    'gibbonCourseClassID' => $gibbonCourseClassID
+));
+
+$I->click('Delete');
+$I->seeSuccessMessage();
+
+// Restore Original Settings -----------------------------------
+$I->amOnModulePage('School Admin', 'markbookSettings.php');
+$I->submitForm('#content form', $originalMarkbookSettings, 'Submit');
+$I->seeSuccessMessage();
+
