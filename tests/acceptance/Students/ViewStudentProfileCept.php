8047596d3d2d6e4b94e6af49536b1a607d506920
Sandra Kuipers <skuipers@ichk.edu.hk>
2026-02-20 21:59:49 +0800

    Tests: fix tests and remove tests with long CURL timeout issues

--- tests/acceptance/Students/ViewStudentProfileCept.php
+++ tests/acceptance/Students/ViewStudentProfileCept.php
@@ -10,12 +10,29 @@ $I->loginAsAdmin();
 $I->amOnModulePage('Students', 'student_view.php');
 $I->seeBreadcrumb('View Student Profiles');
 
-// Get a student
+// Database Seed  ------------------------------
+
 $gibbonPersonID = $I->grabFromDatabase('gibbonPerson', 'gibbonPersonID', ['username' => 'testingstudent']);
 $gibbonSchoolYearID = $I->grabFromDatabase('gibbonSchoolYear', 'gibbonSchoolYearID', ['status' => 'Current']);
+$gibbonFormGroupID = $I->grabFromDatabase('gibbonFormGroup', 'gibbonFormGroupID');
+$gibbonYearGroupID = $I->grabFromDatabase('gibbonYearGroup', 'gibbonYearGroupID');
+
+$gibbonStudentEnrolmentID = $I->haveInDatabase('gibbonStudentEnrolment', [
+    'gibbonPersonID'     => $gibbonPersonID,
+    'gibbonSchoolYearID' => $gibbonSchoolYearID,
+    'gibbonFormGroupID'  => $gibbonFormGroupID,
+    'gibbonYearGroupID'  => $gibbonYearGroupID,
+]);
+
+
+// Test View Page  ------------------------------
 
 $I->amOnModulePage('Students', 'student_view_details.php', [
     'gibbonPersonID' => $gibbonPersonID,
     'gibbonSchoolYearID' => $gibbonSchoolYearID
 ]);
 $I->seeBreadcrumb('View Student Profiles');
+
+// Database Cleanup  ------------------------------
+
+$I->deleteFromDatabase('gibbonStudentEnrolment', ['gibbonStudentEnrolmentID' => $gibbonStudentEnrolmentID]);
--- tests/acceptance/System Admin/ViewSystemCheckCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @covers modules/System Admin/systemCheck.php
- */
-$I = new AcceptanceTester($scenario);
-$I->wantTo('View system check');
-$I->loginAsAdmin();
-$I->amOnModulePage('System Admin', 'systemCheck.php');
-$I->seeBreadcrumb('System Check');
