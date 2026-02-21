d7f7b503a2c06a6f992b0a69441449c5aa7ede4b
Sandra Kuipers <skuipers@ichk.edu.hk>
2026-02-21 19:46:00 +0800

    Tests: remove tests with CURL timeout

--- tests/acceptance/System Admin/ViewSystemOverviewCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @covers modules/System Admin/systemOverview.php
- */
-$I = new AcceptanceTester($scenario);
-$I->wantTo('View system overview');
-$I->loginAsAdmin();
-$I->amOnModulePage('System Admin', 'systemOverview.php');
-$I->seeBreadcrumb('System Overview');
