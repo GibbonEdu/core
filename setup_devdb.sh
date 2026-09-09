dcb326651abc32a13e1d0b7ea7f933ee773a1c5d
Peter Li <peterli852@proton.me>
2026-09-09 18:40:15 +0800

    Add step to add absolutePath value in gibbonSetting table

--- setup_devdb.sh
+++ setup_devdb.sh
@@ -73,6 +73,15 @@ else
   exit 1
 fi
 
+log "Updating absolutePath value in gibbonSetting table"
+docker compose exec -T -e MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" db \
+  mysql --init-command="SET SESSION sql_mode='';" -uroot "${MYSQL_DATABASE}" <<'SQL'
+UPDATE gibbonSetting
+  SET value = '/var/www/html'
+  WHERE name = 'absolutePath'
+SQL
+log "OK: absolutePath value is /var/www/html"
+
 log "Updating installType to Development in gibbonSetting table"
 docker compose exec -T -e MYSQL_PWD="${MYSQL_ROOT_PASSWORD}" db \
   mysql --init-command="SET SESSION sql_mode='';" -uroot "${MYSQL_DATABASE}" <<'SQL'
